51640087be3b07ce8646c926d0df8ecd0a0b60fa
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-03-18 22:33:21 +0900

    2-2-2-type-conversionプロジェクトの追加

---
+++ part1/ch02/2-2-2-type-conversion/src/type_conversion.cpp
@@ -0,0 +1,25 @@
+/*
+ * type_conversion.cpp
+ *
+ *  Created on: 2016/03/18
+ *      Author: sasaki
+ */
+// 2016.03.18 add
+#include <iostream>
+
+void some_function()
+{
+	double d = 2.2;
+	int i = 7;
+	d = d + i;
+	i = d + i; // no warning
+
+	// 2016.03.18 add
+	std::cout << " d = " << d << ", i = " << i << "\n";
+}
+int main()
+{
+	some_function();
+}
+
+
