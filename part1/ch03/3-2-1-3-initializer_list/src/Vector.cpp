b1715fe0ce1f09a39b6b02c0b27506f9688aab38
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-03-23 21:39:45 +0900

    3-2-1-3-initializer_listプロジェクトの追加

---
+++ part1/ch03/3-2-1-3-initializer_list/src/Vector.cpp
@@ -0,0 +1,60 @@
+/*
+ * Vector.cpp
+ *
+ *  Created on: 2016/03/21
+ *      Author: sasaki
+ */
+
+#include <initializer_list>
+//#include <algorithm>
+#include "Vector.h"
+
+Vector::Vector(std::initializer_list<double> lst)
+	:elem{ new double[lst.size()] }, sz{ static_cast<int>(lst.size()) }, space{ 0 }
+{
+//	std::copy(lst.begin(), lst.end(), elem);
+	double *p = elem;
+	for (auto it = lst.begin(); it != lst.end(); ++it, ++p)
+		*p = *it;
+}
+
+// 2016.03.21 add copy constructor
+Vector::Vector(const Vector&src)
+	:elem{ new double[src.sz] }, sz{ src.sz }, space{0}
+{
+	for (int i = 0; i < sz; ++i) {
+		elem[i] = src.elem[i];
+	}
+}
+
+Vector& Vector::operator=(const Vector& src)
+{
+	if (this == &src) return *this;
+
+	double *newalloc = new double[src.sz];
+	sz = src.sz;
+	space = 0;
+	delete[] elem;
+	elem = newalloc;
+	for (int i = 0; i < sz; ++i) {
+		elem[i] = src.elem[i];
+	}
+}
+
+void Vector::push_back(double x)
+{
+	if (space == 0) {
+		int newsize = (sz == 0) ? 8 : sz * 2;
+		space = newsize - sz;
+		double *newalloc = new double[newsize];
+		for (int i = 0; i < sz; ++i) {
+			newalloc[i] = elem[i];
+		}
+		delete[] elem;
+		elem = newalloc;
+	}
+
+	elem[sz++] = x;
+	--space;
+}
+
---
+++ part1/ch03/3-2-1-3-initializer_list/src/Vector.h
@@ -0,0 +1,28 @@
+/*
+ * Vector.h
+ *
+ *  Created on: 2016/03/21
+ *      Author: sasaki
+ */
+
+
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+class Vector {
+private:
+	double*	elem;
+	int		sz;
+	int		space;
+public:
+	Vector() :elem{ nullptr }, sz{ 0 }, space{ 0 } { }
+	Vector(std::initializer_list<double> lst);
+	Vector(const Vector&src);
+	~Vector() { delete[] elem; }
+	Vector& operator=(const Vector& src);
+	double& operator[](int i) { return elem[i]; }
+	int size() const { return sz; }
+	void push_back(double);
+};
+
+#endif /* VECTOR_H_ */
