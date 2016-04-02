ba9c01c17a171915dfb69cce4303efd3c8b373cb
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-02 18:27:40 +0900

    4-4-3-mapプロジェクトの追加

---
+++ part1/ch04/4-4-3-map/src/map.cpp
@@ -0,0 +1,32 @@
+/*
+ * map.cpp
+ *
+ *  Created on: 2016/04/02
+ *      Author: sasaki
+ */
+
+#include <map>
+#include <iostream>
+using namespace std;
+
+map<string, int> phone_book = {
+	{"David Hume", 123456},
+	{"Karl Popper", 234567},
+	{"Bertrand Arthur William Russell", 345678}
+};
+
+int get_number(const string& s)
+{
+	return phone_book[s];
+}
+
+int main()
+{
+	int x;
+
+	x = get_number("Karl Popper");
+	cout << "get_number(\"Karl Popper\") returns " << x << '\n';
+}
+
+
+
