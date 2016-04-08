b6557d5726fc879cfcfd985b4d728d03a8e3d62e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-08 21:28:50 +0900

    5-6-5-limitsプロジェクトの追加

---
+++ part1/ch05/5-6-5-limits/src/limits.cpp
@@ -0,0 +1,19 @@
+/*
+ * limits.cpp
+ *
+ *  Created on: 2016/04/08
+ *      Author: sasaki
+ */
+
+
+#include <limits>
+#include <iostream>
+using namespace std;
+
+static_assert(numeric_limits<char>::is_signed, "unsigned characters!");
+static_assert(1000000 < numeric_limits<int>::max(), "small ints!");
+
+int main()
+{
+	cout << "compile ok!\n";
+}
