d4f7e41fddce9a0e707577a3fa414641d6b9b351
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-13 19:57:47 +0900

    7-2-2-nullptrプロジェクトの追加

---
+++ part2/ch07/7-2-2-nullptr/src/nullptr.cpp
@@ -0,0 +1,18 @@
+/*
+ * nullptr.cpp
+ *
+ *  Created on: 2016/04/13
+ *      Author: sasaki
+ */
+
+#include <cstdlib>
+
+int* pi = nullptr;
+double* pd = nullptr;
+//int i = nullptr;
+	// cannot convert 'std::nullptr_t' to 'int' in initialization
+
+int* x = 0;
+
+int* p = NULL;
+
