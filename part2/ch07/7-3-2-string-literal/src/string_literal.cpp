179f2a8915cecbfa0306a85530fe6896691b5a5c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-14 20:22:32 +0900

    7-3-2-string-literalプロジェクトの追加

---
+++ part2/ch07/7-3-2-string-literal/src/string_literal.cpp
@@ -0,0 +1,54 @@
+/*
+ * string_literal.cpp
+ *
+ *  Created on: 2016/04/14
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+void f()
+{
+	char* p = "Plato";
+		// ISO C++ forbids converting a string constant to 'char*' [-Wwrite-strings]
+	//p[4] = 'e';	// system down
+
+	cout << "p = " << p << '\n';
+}
+
+void f2()
+{
+	char p[] = "Zeno";
+	p[0] = 'R';
+	cout << "p = " << p << '\n';
+}
+
+const char* error_message(int i)
+{
+	return "range_error";
+}
+
+const char* p = "Heraclitus";
+const char* q = "Heraclitus";
+
+void g()
+{
+	if (p == q) cout << "one!\n";
+}
+
+int main()
+{
+	cout << sizeof("Bohr") << '\n';
+
+	f();
+	f2();
+	cout << "error_message(0) = " << error_message(0) << '\n';
+	g();
+	cout << "beep at end of message\a\n";
+	char alpha[] = "abcdefghijklmlopqrstuvwxyz"
+					"ABCDEFGHIJKLMLOPQRSTUVWXYZ";
+	cout << alpha << '\n';
+}
+
+
