70038a631b2b42d4f5048fbc5e697c8674cd9f3a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-24 11:15:15 +0900

    9-5-4-do-statementプロジェクトの追加

---
+++ part2/ch09/9-5-4-do-statement/src/do_statement.cpp
@@ -0,0 +1,30 @@
+/*
+ * do_statement.cpp
+ *
+ *  Created on: 2016/04/24
+ *      Author: sasaki
+ */
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void print_backwards(char a[], int i)
+{
+	cout << '{';
+	do {
+		cout << a[--i];
+	} while (i);
+	cout << '}';
+}
+
+int main()
+{
+	char s1[] = "this is a string";
+	print_backwards(s1, strlen(s1));
+
+	//char s2[] = "";
+	//print_backwards(s2, strlen(s2));
+		// crash!!!
+}
+
