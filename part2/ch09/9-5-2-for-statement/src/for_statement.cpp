d6d9a9b8037062bc4cc8410f1c9da62fe1edca71
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-24 10:04:38 +0900

    9-5-2-for-statementプロジェクトの追加

---
+++ part2/ch09/9-5-2-for-statement/src/for_statement.cpp
@@ -0,0 +1,65 @@
+/*
+ * for_statement.cpp
+ *
+ *  Created on: 2016/04/24
+ *      Author: sasaki
+ */
+
+#include <iostream>
+#include <vector>
+#include "unistd.h"
+using namespace std;
+
+void f(int v[], int max)
+{
+	for (int i = 0; i!=max; ++i)
+		v[i] = i*i;
+}
+
+void f2(int v[], int max)
+{
+	int i;
+	while (i!=max) {
+		v[i] = i*i;
+		++i;
+	}
+}
+
+void input(vector<string>& v)
+{
+	for (string s; cin >> s; )
+		v.push_back(s);
+}
+
+template<typename C> void print(C &c)
+{
+	cout << '[';
+	for (auto p = begin(c); p!=end(c); ++p)
+		cout << ' ' << *p;
+	cout << " ]\n";
+}
+
+int main()
+{
+	const int max = 5;
+
+	int	v[max];
+	f(v, max);
+	print(v);
+
+	int v2[max];
+	f2(v2, max);
+	print(v2);
+
+	vector<string> vs;
+	input(vs);
+	print(vs);
+
+	// forever
+	cout << "loop forever...\n";
+	for (;;) {
+		sleep(1);
+	}
+}
+
+
