84fee188b99e83e9aa331e55cb33fc0b0433b6db
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-25 22:06:41 +0900

    10-3-1-calculate-resultプロジェクトの追加

---
+++ part2/ch10/10-3-1-calculate-result/src/calculate_result.cpp
@@ -0,0 +1,38 @@
+/*
+ * calculate_result.cpp
+ *
+ *  Created on: 2016/04/25
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+void f(int x, int y)
+{
+	int j = x = y;
+	int* p = &++x;
+	//int* q = &(x++);
+		// lvalue required as unary '&' operand
+	int* p2 = &(x>y?x:y);
+	//int& r = (x<y)?x:1;
+		// invalid initialization of non-const reference of type 'int&' from an rvalue of type 'int'
+
+	cout << "j == " << j << '\n';
+	cout << "p == " << p << '\n';
+	cout << "p2 == " << p2 << '\n';
+}
+
+void f()
+{
+	int i = 1;
+	while (i < i) ++i;
+	cout << "i has becom negative!" << i << '\n';
+}
+
+int main()
+{
+	f(1, 2);
+	f();
+}
+
