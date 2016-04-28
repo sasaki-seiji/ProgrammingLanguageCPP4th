f5cfd6c1cb160c79f044433a0db52e5a76293434
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-29 08:30:18 +0900

    5-2-3-pointer-and-reference-conversionプロジェクトの追加

---
+++ part2/ch10/10-5-2-3-pointer-and-reference-conversion/src/pointer_and_reference_conversion.cpp
@@ -0,0 +1,24 @@
+/*
+ * pointer_and_reference_conversion.cpp
+ *
+ *  Created on: 2016/04/29
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+int* p = (1+2)*(2*(1-1));
+
+int i;
+void* vpi = &i;
+void f(){ }
+//void* vpf = f;
+	// invalid conversion from 'void (*)()' to 'void*' [-fpermissive]
+
+int main()
+{
+	cout << "p: " << p << '\n';
+	cout << "vpi: " << vpi << '\n';
+}
+
