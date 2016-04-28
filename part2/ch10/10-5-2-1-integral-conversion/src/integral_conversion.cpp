e04333e9ecadb44f7300d6c223ea3af48a0956d0
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-29 08:29:08 +0900

    10-5-2-integral-conversionプロジェクトの追加

---
+++ part2/ch10/10-5-2-1-integral-conversion/src/integral_conversion.cpp
@@ -0,0 +1,22 @@
+/*
+ * integral_conversion.cpp
+ *
+ *  Created on: 2016/04/29
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+unsigned char uc = 1023;
+	// large integer implicitly truncated to unsigned type [-Woverflow]
+
+signed char sc = 1023;
+	// overflow in implicit constant conversion [-Woverflow]
+
+int main()
+{
+	cout << "int{uc}: " << int{uc} << '\n';
+	cout << "int{sc}: " << int{sc} << '\n';
+}
+
