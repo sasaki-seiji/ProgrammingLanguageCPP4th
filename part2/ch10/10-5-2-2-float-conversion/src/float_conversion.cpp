a5fd82a76de8a14ed85c36d8af23d683daf74bd2
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-29 08:29:41 +0900

    10-5-2-2-float-conversionプロジェクトの追加

---
+++ part2/ch10/10-5-2-2-float-conversion/src/float_conversion.cpp
@@ -0,0 +1,31 @@
+/*
+ * float_conversion.cpp
+ *
+ *  Created on: 2016/04/29
+ *      Author: sasaki
+ */
+
+#include <cfloat>
+#include <limits>
+#include <iostream>
+using namespace std;
+
+float f = FLT_MAX;
+double d = f;
+double d2 = DBL_MAX;
+float f2 = d2;
+long double ld = d2;
+long double ld2 = numeric_limits<long double>::max();
+double d3 = ld2;
+
+int main()
+{
+	cout << "f(FLT_MAX): " << f << '\n';
+	cout << "d(=f): " << d << '\n';
+	cout << "d2(DBL_MAX): " << d2 << '\n';
+	cout << "f2(=d2): " << f2 << '\n';
+	cout << "ld(=d2): " << ld << '\n';
+	cout << "ld2(<long double>::max(): " << ld2 << '\n';
+	cout << "d3(=ld2): " << d3 << '\n';
+}
+
