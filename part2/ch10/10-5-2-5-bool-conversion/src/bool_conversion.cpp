fcaedb9feb49b08fdd98228e76034a55eaaaee2b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-29 08:30:56 +0900

    10-5-2-5-bool-conversionプロジェクトの追加

---
+++ part2/ch10/10-5-2-5-bool-conversion/src/bool_conversion.cpp
@@ -0,0 +1,45 @@
+/*
+ * bool_conversion.cpp
+ *
+ *  Created on: 2016/04/29
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+void f(int* p, int i)
+{
+	bool is_not_zero = p;
+	bool b2 = i;
+
+	cout << "is_not_zero: " << is_not_zero << '\n';
+	cout << "b2: " << b2 << '\n';
+}
+
+void do_something(int x) { cout << "do_something(" << x << ")\n"; }
+void fi(int i) { cout << "fi(" << i << ")\n"; }
+void fb(bool b) { cout << "fb(" << b << ")\n"; }
+
+void ff(int*p, int* q)
+{
+	if (p) do_something(*p);
+	if (q!=nullptr) do_something(*q);
+
+	//fi(p);
+		// Invalid arguments 'Candidates are:void fi(int)'
+		// invalid conversion from 'int*' to 'int' [-fpermissive]
+
+	fb(q);
+}
+
+int main()
+{
+	int	x=1, y=2;
+
+	f(&x, 10);
+	f(nullptr, 0);
+
+	ff(&x, &y);
+}
+
