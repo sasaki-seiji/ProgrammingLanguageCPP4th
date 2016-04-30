de3c6aeeff4dfd64fa816cb5c9b027d27a647c36
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-04-30 21:33:23 +0900

    11-2-4-1-nothrow-newプロジェクトの追加

---
+++ part2/ch11/11-2-4-1-nothrow-new/src/nothrow_new.cpp
@@ -0,0 +1,31 @@
+/*
+ * nothrow_new.cpp
+ *
+ *  Created on: 2016/04/30
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+void f(int n)
+{
+	int* p = new(nothrow) int[n];
+	if (p==nullptr) {
+		cerr << "can't allocate memory\n";
+	}
+
+	cout << "p: " << p << '\n';
+	operator delete(p, nothrow);
+}
+
+int main()
+{
+	f(1000);
+	f(1000000);
+	f(10000000);
+	f(100000000);
+	f(200000000);
+	f(400000000);
+	f(800000000);
+}
