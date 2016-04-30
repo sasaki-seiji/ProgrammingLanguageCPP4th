f0aa8c1b25db5bed9c17c16998afc32bc6eef9ab
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-05-01 07:10:55 +0900

    11-3-initializer-listプロジェクトの追加

---
+++ part2/ch11/11-3-initializer-list/src/initializer_list.cpp
@@ -0,0 +1,49 @@
+/*
+ * initializer_list.cpp
+ *
+ *  Created on: 2016/05/01
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+struct S { int a, b; };
+struct SS { double a, b; };
+
+void f(S);
+
+void g(S);
+void g(SS);
+
+void h()
+{
+	f({1,2});
+	//g({1,2});
+		// 	'g' is ambiguous 'Candidates are:void g(SS)	void g(S)'
+		// call of overloaded 'g(<brace-enclosed initializer list>)' is ambiguous
+
+	g(S{1,2});
+	g(SS{1,2});
+}
+
+int main()
+{
+	h();
+}
+
+// undefs
+void f(S s)
+{
+	cout << "f(S)\n";
+}
+
+void g(S s)
+{
+	cout << "g(S)\n";
+}
+
+void g(SS s)
+{
+	cout << "g(SS)\n";
+}
