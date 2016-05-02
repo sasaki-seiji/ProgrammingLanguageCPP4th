2b06c387b4116dd2ac6498e864ac12b55f6cd7f6
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-05-02 16:26:20 +0900

    11-4-1-lambda-implementation-modelプロジェクトの追加

---
+++ part2/ch11/11-4-1-lambda-implementation-model/src/lambda_implementation_model.cpp
@@ -0,0 +1,43 @@
+/*
+ * lambda_implementation_model.cpp
+ *
+ *  Created on: 2016/05/02
+ *      Author: sasaki
+ */
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+void print_modulo(const vector<int>& v, ostream& os, int m)
+{
+	for_each(begin(v), end(v),
+			[&os,m](int x){ if (x%m==0) os << x << '\n'; }
+	);
+}
+
+class Modulo_print {
+	ostream& os;
+	int m;
+public:
+	Modulo_print(ostream& s, int mm) :os(s), m(mm) {}
+	void operator()(int x) const
+		{ if (x % m == 0) os << x << '\n'; }
+};
+
+void print_modulo2(const vector<int>& v, ostream& os, int m)
+{
+	for_each(begin(v), end(v), Modulo_print{os,m});
+}
+
+int main()
+{
+	vector<int> v;
+	for (int i = 0; i < 100; ++i)
+		v.push_back(i);
+
+	print_modulo(v, cout, 17);
+	print_modulo(v, cout, 19);
+}
+
