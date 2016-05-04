a7b06250b444136ee1795b1f22ee3e449cae07e6
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-05-04 18:10:55 +0900

    11-5-2-named-castプロジェクトの追加

---
+++ part2/ch11/11-5-2-named-cast/src/named_cast.cpp
@@ -0,0 +1,63 @@
+/*
+ * named_cast.cpp
+ *
+ *  Created on: 2016/05/04
+ *      Author: sasaki
+ */
+
+
+#include <iostream>
+using namespace std;
+
+struct IO_device {
+	unsigned short cmd;
+	unsigned short stat;
+};
+
+IO_device* d1 = reinterpret_cast<IO_device*>(0xff00);
+
+void* my_allocator(size_t);
+
+void f()
+{
+	int* p = static_cast<int*>(my_allocator(100));
+	cout << "p: " << p << '\n';
+}
+
+void g()
+{
+	char x = 'a';
+	//int* p1 = &x;
+		// cannot convert 'char*' to 'int*' in initialization
+	//int* p2 = static_cast<int*>(&x);
+		// invalid static_cast from type 'char*' to type 'int*'
+	int* p3 = reinterpret_cast<int*>(&x);
+	cout << "p3: " << p3 << '\n';
+	*p3 = 12345679; // dangerous
+}
+
+struct B { };
+struct D : B { };
+
+void h()
+{
+	B* pb = new D;
+	//D* pd = pb;
+		// invalid conversion from 'B*' to 'D*' [-fpermissive]
+	D* pd2 = static_cast<D*>(pb);
+	cout << "pd2: " << pd2 << '\n';
+}
+
+int main()
+{
+	cout << "d1: " << d1 << '\n';
+	f();
+	g();
+	h();
+}
+
+// undefs
+void* my_allocator(size_t sz)
+{
+	return operator new(sz);
+}
