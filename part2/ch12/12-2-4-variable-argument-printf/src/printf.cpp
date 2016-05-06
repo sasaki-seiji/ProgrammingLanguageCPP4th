7c6b1f82113dae2c17d02fe764edd55283979674
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-05-07 07:48:27 +0900

    12-2-4-variable-argument-printfプロジェクトの追加

---
+++ part2/ch12/12-2-4-variable-argument-printf/src/printf.cpp
@@ -0,0 +1,21 @@
+/*
+ * printf.cpp
+ *
+ *  Created on: 2016/05/07
+ *      Author: sasaki
+ */
+
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	printf("Hello, world!\n");
+	const char *first_name = "seiji", *second_name = "sasaki";
+	printf("My name is %s %s\n", first_name, second_name);
+	printf("%d + %d = %d\n", 2, 3, 5);
+
+	//printf("My name is %s %s\n", 2);	// crash!!!
+		// format '%s' expects a matching 'char*' argument [-Wformat=]
+		// format '%s' expects argument of type 'char*', but argument 2 has type 'int' [-Wformat=]
+}
