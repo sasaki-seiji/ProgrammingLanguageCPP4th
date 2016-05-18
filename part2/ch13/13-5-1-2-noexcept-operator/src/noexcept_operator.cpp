7487d6dc209ba6b2831d2e60478cf46b6a59dec9
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-05-18 20:26:45 +0900

    13-5-1-2-noexcept-operatorプロジェクトの追加

---
+++ part2/ch13/13-5-1-2-noexcept-operator/src/noexcept_operator.cpp
@@ -0,0 +1,61 @@
+/*
+ * noexcept_operator.cpp
+ *
+ *  Created on: 2016/05/18
+ *      Author: sasaki
+ */
+
+#include <type_traits>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template<typename T>
+constexpr bool Is_pod()
+{
+	return std::is_pod<T>::value;
+}
+
+template<typename T>
+void my_fct(T& x) noexcept(Is_pod<T>())
+{
+	T tmp {x};
+	cout << "my_fct(): " << tmp << '\n';
+}
+
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+	os << "[ ";
+	for (auto& x : v)
+		os << x << ", ";
+	os << "]";
+	return os;
+}
+
+template<typename T>
+void f(T& x)
+{
+	cout << "f(" << x << ")\n";
+}
+
+template<typename T>
+void call_f(vector<T>& v) noexcept(noexcept(f(v[0])))
+{
+	for (auto x : v)
+		f(x);
+}
+
+
+int main()
+{
+	double	x = 12.3;
+	vector<int> v {1,2,3};
+
+	my_fct(x);
+	my_fct(v);
+
+	call_f(v);
+	vector<string> vs {"this", "is", "string", "vector"};
+	call_f(vs);
+}
