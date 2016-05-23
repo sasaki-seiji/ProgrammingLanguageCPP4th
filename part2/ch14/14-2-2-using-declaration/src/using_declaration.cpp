592cba32982591dcccba58818031e5fc39cad93b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-05-23 20:20:15 +0900

    14-2-2-using-declarationプロジェクトの追加

---
+++ part2/ch14/14-2-2-using-declaration/src/using_declaration.cpp
@@ -0,0 +1,74 @@
+/*
+ * using_declaration.cpp
+ *
+ *  Created on: 2016/05/23
+ *      Author: sasaki
+ */
+
+#include <string>
+#include <vector>
+#include <sstream>
+
+std::vector<std::string> split(const std::string& s)
+{
+	std::vector<std::string> res;
+	std::istringstream iss(s);
+	for (std::string buf; iss >> buf; )
+		res.push_back(buf);
+	return res;
+}
+
+using std::string;
+
+std::vector<string> split2(const string& s)
+{
+	std::vector<string> res;
+	std::istringstream iss(s);
+	for (string buf; iss >> buf; )
+		res.push_back(buf);
+	return res;
+}
+
+namespace N {
+	void f(int);
+	void f(string);
+}
+
+void g()
+{
+	using N::f;
+	f(789);
+	f("Bruce");
+}
+
+#include <iostream>
+template<typename T>
+std::ostream& operator<<(std::ostream&os, const std::vector<T>& v)
+{
+	std::cout << "[";
+	for (auto& x : v)
+		std::cout << x << ",";
+	std::cout << "]";
+	return os;
+}
+int main()
+{
+	std::vector<std::string> v = split("this is a pen");
+	std::cout << v << '\n';
+
+	std::vector<string> v2 = split2("this is another pen");
+	std::cout << v2 << '\n';
+
+	g();
+}
+
+// undefs
+void N::f(int i)
+{
+	std::cout << "N::f(" << i << ")\n";
+}
+void N::f(string s)
+{
+	std::cout << "N::f(\"" << s << "\")\n";
+}
+
