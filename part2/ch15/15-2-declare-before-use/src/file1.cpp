9dfd8f2b41646bb6e17505f0401f490e4fd33722
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-07-09 15:36:20 +0900

    15-2-declare-before-useの追加

---
+++ part2/ch15/15-2-declare-before-use/src/file1.cpp
@@ -0,0 +1,22 @@
+/*
+ * file1.cpp
+ *
+ *  Created on: 2016/07/09
+ *      Author: sasaki
+ */
+
+
+//int g() { return f()+7; } // undefined reference to `c'
+//int f() { return x; }	// 'x' was not declared in this scope
+int x;
+
+int f() { return x; }	// 'x' was not declared in this scope
+int g() { return f()+7; } // undefined reference to `c'
+
+// add main
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << g() << '\n';
+}
