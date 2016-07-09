ff4674a9a6d1903e9e360a71db235068ff7488d3
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-07-09 16:50:08 +0900

    15-2-internal-linkageの追加

---
+++ part2/ch15/15-2-internal-linkage/src/file1.cpp
@@ -0,0 +1,16 @@
+/*
+ * file1.cpp
+ *
+ *  Created on: 2016/07/09
+ *      Author: sasaki
+ */
+
+
+// internal linkage
+static int x1 = 1;
+const char x2 = 'a';
+
+// external linkage
+int y1 = 1;
+extern const char y2 = 'a';
+
---
+++ part2/ch15/15-2-internal-linkage/src/file2.cpp
@@ -0,0 +1,19 @@
+/*
+ * file2.cpp
+ *
+ *  Created on: 2016/07/09
+ *      Author: sasaki
+ */
+
+extern int y1;
+extern const char y2;
+
+// add main
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "y1 : " << y1 << '\n';
+	cout << "y2 : " << y2 << '\n';
+}
+
