db7e003e5e6b9d95232271a45e27aa62c278c5c5
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-07-10 22:56:07 +0900

    15-2-5-extern-cの追加

---
+++ part2/ch15/15-2-5-extern-c/src/c_impl.c
@@ -0,0 +1,19 @@
+/*
+ * c_impl.c
+ *
+ *  Created on: 2016/07/10
+ *      Author: sasaki
+ */
+
+/* implement by c*/
+
+#include <stdio.h>
+
+int f()
+{
+	printf("f()\n");
+}
+
+int g2 = 100;
+int g3 = 123;
+
---
+++ part2/ch15/15-2-5-extern-c/src/extern_c.cpp
@@ -0,0 +1,51 @@
+/*
+ * extern_c.cpp
+ *
+ *  Created on: 2016/07/10
+ *      Author: sasaki
+ */
+
+extern "C" int f();
+
+int g()
+{
+	//return f(1);
+		// error: too many arguments to function 'int f()'
+	return f();
+}
+
+extern "C" {
+	int g1;
+	extern int g2;
+}
+
+extern "C" int g3;
+extern "C" { int g4; }
+
+#include <cstdio>
+
+void f2()
+{
+	std::printf("Hello, ");
+	printf("world!\n");
+}
+
+
+// add main
+#include <iostream>
+using namespace std;
+int main()
+{
+	g();
+
+	g1 = 10;
+	cout << "g1: " << g1 << '\n';
+	cout << "g2: " << g2 << '\n';
+
+	cout << "g3: " << g3 << '\n';
+	g4 = 3;
+	cout << "g4: " << g4 << '\n';
+
+	f2();
+}
+
