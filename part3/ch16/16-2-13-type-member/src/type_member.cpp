c7b5ada4d26fc0461b54b2532e6ad4eb6a2800d4
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-07-18 20:07:49 +0900

    Nodeタイプメンバをpublicに変更

--- part3/ch16/16-2-13-type-member/src/type_member.cpp
+++ part3/ch16/16-2-13-type-member/src/type_member.cpp
@@ -5,8 +5,12 @@
  *      Author: sasaki
  */
 
+#include <iostream>
+using namespace std;
+
 template<typename T>
 class Tree {
+public: // 2016.07.18 change to public from private
 	using value_type = T;
 	enum Policy {rb, splay, treeps };
 	class Node {
@@ -15,18 +19,31 @@ class Tree {
 		value_type value;
 	public:
 		void f(Tree*);
+
+		// 2016.07.18 add
+		Node(value_type v, Node* r=nullptr, Node* l=nullptr)
+		: right{r}, left{l}, value{v} { }
 	};
+
+private:
 	Node* top;
+
 public:
 	void g(Node*);
+
+	// 2016.07.18 add
+	Tree(Node* t = nullptr) : top{t} { }
 };
 
 template<typename T>
 void Tree<T>::Node::f(Tree* p)
 {
-	top = right;
+	//top = right;
+		// invalid use of non-static data member 'Tree<int>::top'
 	p->top = right;
 	value_type v = left->value;
+
+	cout << "in Tree<T>::Node::f(Tree*) - v: " << v << '\n';
 }
 
 template<typename T>
@@ -34,7 +51,8 @@ void Tree<T>::g(Node* p)
 {
 	//value_type val = right->value;
 		//  'right' was not declared in this scope
-	value_type v = p->right->value;
+	//value_type v = p->right->value;
+		// 'Tree<int>::value_type Tree<int>::Node::value' is private
 	p->f(this);
 }
 
@@ -42,7 +60,12 @@ void Tree<T>::g(Node* p)
 // add main
 int main()
 {
-	Tree<int> tree;
+	Tree<int>::Node node_r {10};
+	Tree<int>::Node node_l {20};
+	Tree<int>::Node node_top {30, &node_r, &node_l};
+	Tree<int> tree {&node_top};
+
+	tree.g(&node_top);
 }
 
 
