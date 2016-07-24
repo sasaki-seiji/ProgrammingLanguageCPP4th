1277123acc2df2d04489da7b4148fb0f0ba75b5b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-07-24 18:35:26 +0900

    17-3-1-initialize-without-constructorの追加

---
+++ part3/ch17/17-3-1-initialize-without-constructor/src/initialize_without_constructor.cpp
@@ -0,0 +1,115 @@
+/*
+ * initialize_without_constructor.cpp
+ *
+ *  Created on: 2016/07/24
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+int a {1};
+char* p {nullptr};
+
+
+struct Work {
+	string author;
+	string name;
+	int year;
+};
+
+Work s9 { "Beethoven",
+		"Symphony No. 9 in D minor, Op. 125: Choral",
+		1824
+};
+
+Work currently_playing { s9 };
+Work none { };
+
+Work alpha;
+
+void f()
+{
+	Work beta;
+
+	cout << "alpha.author: " << alpha.author << '\n'
+			<< "alpha.name: " << alpha.name << '\n'
+			<< "alpha.year: " << alpha.year << '\n';
+
+	cout << "beta.author: " << beta.author << '\n'
+			<< "beta.name: " << beta.name << '\n'
+			<< "beta.year: " << beta.year << '\n';
+}
+
+struct Buf {
+	int count;
+	char buf[16*1024];
+};
+
+Buf buf0;
+
+void g()
+{
+	Buf buf1;
+	Buf buf2 {};
+	int* p1 = new int;
+	int* p2 = new int{};
+	int* p3 = new int{7};
+
+	cout << "buf0.count: " << buf0.count <<
+			", int(buf0.buf[0]): " << int(buf0.buf[0]) << '\n';
+	cout << "buf1.count: " << buf1.count <<
+			", int(buf1.buf[0]): " << int(buf1.buf[0]) << '\n';
+	cout << "buf2.count: " << buf2.count <<
+			", int(buf2.buf[0]): " << int(buf2.buf[0]) << '\n';
+
+	cout << "*p1: " << *p1 << '\n';
+	cout << "*p2: " << *p2 << '\n';
+	cout << "*p3: " << *p3 << '\n';
+}
+
+template<typename T>
+class Checked_pointer {
+//private:
+	// error: no matching function for call to 'Checked_pointer<int>::Checked_pointer(<brace-enclosed initializer list>)'
+public:
+	T* p;
+public:
+	T& operator*();
+};
+
+Checked_pointer<int> cp {new int{7}};
+
+// add undef func
+
+template<typename T>
+inline T& Checked_pointer<T>::operator*()
+{
+	if (!p) throw runtime_error("Checked_pointer::operator*()");
+	return *p;
+}
+
+// add main
+
+int main()
+{
+	cout << "a: " << a << '\n';
+	cout << "static_cast<void*>(p): " << static_cast<void*>(p) << '\n';
+
+	cout << "s9.author: " << s9.author << '\n'
+			<< "s9.name: " << s9.name << '\n'
+			<< "s9.year: " << s9.year << '\n';
+	cout << "currently_playing.author: " << currently_playing.author << '\n'
+			<< "currently_playing.name: " << currently_playing.name << '\n'
+			<< "currently_playing.year: " << currently_playing.year << '\n';
+	cout << "none.author: " << none.author << '\n'
+			<< "none.name: " << none.name << '\n'
+			<< "none.year: " << none.year << '\n';
+
+	f();
+	g();
+
+	cout << "*cp: " << *cp << '\n';
+}
+
+
