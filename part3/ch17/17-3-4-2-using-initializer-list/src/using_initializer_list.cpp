3e93f10c99239ced4688f492bd806b8c62a485cb
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-07-27 20:26:08 +0900

    17-3-4-2-using-initializer-listの追加

---
+++ part3/ch17/17-3-4-2-using-initializer-list/src/using_initializer_list.cpp
@@ -0,0 +1,84 @@
+/*
+ * using_initializer_list.cpp
+ *
+ *  Created on: 2016/07/27
+ *      Author: sasaki
+ */
+
+#include <initializer_list>
+#include <iostream>
+#include <memory>
+using namespace std;
+
+
+void f(initializer_list<int> args)
+{
+	for (int i = 0; i != args.size(); ++i)
+		cout << args.begin()[i] << "\n";
+}
+
+void f2(initializer_list<int> args)
+{
+	for (auto p = args.begin(); p != args.end(); ++p)
+		cout << *p << "\n";
+}
+
+void f3(initializer_list<int> args)
+{
+	for (auto x : args)
+		cout << x << "\n";
+}
+
+int fx(std::initializer_list<int> x, int val)
+{
+	//*x.begin() = val;
+		// error: assignment of read-only location '* x.std::initializer_list<_E>::begin<int>()'
+	return *x.begin();
+}
+
+void g()
+{
+	for (int i=0; i!=10; ++i)
+		cout << fx({1,2,3}, i) << '\n';
+}
+
+
+template<typename E>
+class Vector {
+public:
+	Vector(initializer_list<E> s);
+	~Vector() { allocator<E>().deallocate(elem, sz); }
+	void print(ostream& os);
+private:
+	void reserve(int n) { elem = allocator<E>().allocate(n); }
+	int sz;
+	E* elem;
+};
+
+template<typename E>
+Vector<E>::Vector(initializer_list<E> s) :sz{s.size()}
+{
+	reserve(sz);
+	uninitialized_copy(s.begin(), s.end(), elem);
+}
+
+template<typename E>
+void Vector<E>::print(ostream& os)
+{
+	os << "[";
+	for (int i=0; i<sz; ++i)
+		os << elem[i] << ',';
+	os << "]\n";
+}
+// add main
+
+int main()
+{
+	f({1,2,3,4});
+	f2({9,8,7});
+	f3({11,22,33});
+	g();
+
+	Vector<int> v = {1,2,3,4};
+	v.print(cout);
+}
