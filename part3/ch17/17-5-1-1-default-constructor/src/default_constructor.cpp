640ca4fce124221d3bc589159a31c3d96112f2a0
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-07-31 08:08:35 +0900

    7-5-1-1-default-constructorの追加

---
+++ part3/ch17/17-5-1-1-default-constructor/src/default_constructor.cpp
@@ -0,0 +1,83 @@
+/*
+ * default_constructor.cpp
+ *
+ *  Created on: 2016/07/31
+ *      Author: sasaki
+ */
+
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class X {
+	string s;
+	string s2;
+	vector<string> v;
+
+public:
+	X(const X& a)
+		:s{a.s}, v{a.v}
+	{
+
+	}
+
+	X(const string& si, const string& si2, const vector<string>& vi)
+		: s{si}, s2{si2}, v{vi}
+	{
+	}
+
+	void print(ostream& os);
+};
+
+ostream& operator<<(ostream& os, const vector<string>& v)
+{
+	os << '[';
+	for (auto& x : v)
+		os << x << ',';
+	os << ']';
+	return os;
+}
+
+void X::print(ostream& os)
+{
+	os << "X:s{" << s << "},s2{" << s2 << "},v{" << v << "}\n";
+}
+
+
+class Y {
+	string s;
+	string s2;
+	vector<string> v;
+
+public:
+	Y(const string& si, const string& si2, const vector<string>& vi)
+		: s{si}, s2{si2}, v{vi}
+	{
+	}
+
+	void print(ostream& os);
+};
+
+void Y::print(ostream& os)
+{
+	os << "Y:s{" << s << "},s2{" << s2 << "},v{" << v << "}\n";
+}
+
+
+// add main
+
+int main()
+{
+	X x1{"aaa", "xyz", {"123", "abc", "ABC"}};
+	x1.print(cout) ;
+
+	X x2{x1};
+	x2.print(cout);
+
+	Y y1{"aaa", "xyz", {"123", "abc", "ABC"}};
+	y1.print(cout) ;
+
+	Y y2{y1};
+	y2.print(cout);
+}
