81d6dd7a1acb905543172e71378003288f70007f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-08-01 19:20:19 +0900

    17-6-2-default-operatorの追加

---
+++ part3/ch17/17-6-2-default-operator/src/default_operator.cpp
@@ -0,0 +1,38 @@
+/*
+ * default_operator.cpp
+ *
+ *  Created on: 2016/08/01
+ *      Author: sasaki
+ */
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+struct S {
+	string a;
+	int b;
+};
+
+S f(S arg)
+{
+	S s0 {};
+	cout << "s0.a: " << s0.a << ", s0.b: " << s0.b << '\n';
+
+	S s1 {s0};
+	cout << "s1.a: " << s1.a << ", s1.b: " << s1.b << '\n';
+
+	s1 = arg;
+	cout << "s1.a: " << s1.a << ", s1.b: " << s1.b << '\n';
+
+	return s1;
+}
+
+// add main
+
+int main()
+{
+	S s = f(S{"string", 10});
+	cout << "s.a: " << s.a << ", s.b: " << s.b << '\n';
+
+}
