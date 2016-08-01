6e956d5324a6a2fa0796b5921a27381197870dbe
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-08-01 19:45:39 +0900

    17-6-3-2-maintain-invarientの追加

---
+++ part3/ch17/17-6-3-2-maintain-invarient/src/maintain_invarient.cpp
@@ -0,0 +1,52 @@
+/*
+ * maintain_invarient.cpp
+ *
+ *  Created on: 2016/08/01
+ *      Author: sasaki
+ */
+
+#include <vector>
+#include <iostream>
+using namespace std;
+
+struct Z {
+	vector<int> elem;
+	int my_favorite;
+	int* largest;
+};
+
+
+// add util
+
+ostream& operator<<(ostream& os, const vector<int>& v)
+{
+	os << "[ ";
+	for (auto x : v)
+		os << x << ", ";
+	os << "]";
+	return os;
+}
+
+void print(const Z& z)
+{
+	cout << ".elem: " << z.elem << " .my_favorite: " << z.my_favorite
+			<< " .largest: " << z.largest << '\n';
+}
+
+// add main
+
+int main()
+{
+	Z v0;
+	cout << "v0: "; print(v0);
+
+	Z val {{1,2,3},1,&val.elem[2]};
+	cout << "val: "; print(val);
+
+	Z v2 = val;
+	cout << "v2: "; print(v2);
+
+	Z v3 = move(val);
+	cout << "v3: "; print(v3);
+	cout << "val: "; print(val);
+}
