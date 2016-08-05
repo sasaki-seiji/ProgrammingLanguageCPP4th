6d55e343d5a0786687c494fdb53726514d7829b9
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-08-05 20:20:37 +0900

    17-6-4-delete-of-functionの追加

---
+++ part3/ch17/17-6-4-delete-of-function/src/delete_of_function.cpp
@@ -0,0 +1,102 @@
+/*
+ * delete_of_function.cpp
+ *
+ *  Created on: 2016/08/05
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+	Base() { };
+
+	Base& operator=(const Base&) = delete;
+	Base(const Base&) = delete;
+	Base& operator=(Base&&) = delete;
+	Base(Base&&) = delete;
+};
+
+Base x1;
+//Base x2 {x1};
+	// error: use of deleted function 'Base::Base(const Base&)'
+
+
+template<typename T>
+T* clone(T* p)
+{
+	return new T{*p};
+}
+
+struct Foo {
+	Foo() { cout << "Foo::Foo()\n"; }
+	Foo(const Foo&) { cout << "Foo::Foo(const Foo&)\n"; }
+};
+struct Shape {
+	Shape() { cout << "Shape::Shape()\n"; }
+	Shape(const Shape&) { cout << "Shape::Shape(const Shape&)\n"; }
+};
+Foo* clone(Foo*) = delete;
+
+void f(Shape* ps, Foo* pf)
+{
+	Shape* ps2 = clone(ps);
+	//Foo* pf2 = clone(pf);
+		// error: use of deleted function 'Foo* clone(Foo*)'
+}
+
+
+struct Z {
+	Z(double);
+	Z(int) = delete;
+};
+
+void f()
+{
+	//Z z1 {1};
+		// error: use of deleted function 'Z::Z(int)'
+	Z z2 {1.0};
+}
+
+
+class Not_on_stack {
+public:
+	~Not_on_stack() = delete;
+};
+
+class Not_on_free_store {
+public:
+	void* operator new(size_t) = delete;
+};
+
+void g()
+{
+	//Not_on_stack v1;
+		// error: use of deleted function 'Not_on_stack::~Not_on_stack()'
+	Not_on_free_store v2;
+
+	Not_on_stack* p1 = new Not_on_stack;
+	//Not_on_free_store* p2 = new Not_on_free_store;
+		// error: use of deleted function 'static void* Not_on_free_store::operator new(size_t)'
+}
+
+// add undef func
+
+Z::Z(double b)
+{
+	cout << "Z::Z(double: " << b << ")\n";
+}
+
+// add main
+
+int main()
+{
+	Shape shape;
+	Foo	foo;
+	f(&shape, &foo);
+
+	f();
+
+	g();
+}
