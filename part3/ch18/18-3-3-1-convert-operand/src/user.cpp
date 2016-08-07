77f28248b41bbfef02b01539ab58f4aae71e4bef
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-08-07 17:22:26 +0900

    18-3-3-1-convert-operandの追加

---
+++ part3/ch18/18-3-3-1-convert-operand/src/user.cpp
@@ -0,0 +1,50 @@
+/*
+ * user.cpp
+ *
+ *  Created on: 2016/08/07
+ *      Author: sasaki
+ */
+
+
+#include "complex.h"
+#include <iostream>
+using namespace std;
+
+void f(complex x, complex y)
+{
+	cout << "f(complex, complex):\n";
+	cout << "x: " << x << '\n';
+	cout << "y: " << y << '\n';
+
+	cout << "x==y: " << (x==y) << '\n';
+	cout << "x==3: " << (x==3) << '\n';
+	cout << "3==y: " << (3==y) << '\n';
+}
+
+
+void g(complex z)
+{
+	3+z;
+	//3.operator+=(z);
+		// error: unable to find numeric literal operator 'operator""operator'
+	//3+=z;
+		// error: no match for 'operator+=' (operand types are 'int' and 'complex')
+}
+
+// add main
+
+int main()
+{
+	complex x1{3,0}, y1{3, 0}, x2{1,2}, y2{-1,-3};
+	f(x1, y1);
+	f(x2, y2);
+
+	g(x2);
+
+
+	complex x {4,5};
+	complex z {sqrt(x)+={1,2}};
+
+	cout << "x: " << x << '\n';
+	cout << "z: " << z << '\n';
+}
