7cc262f4218401e3bfa87f9aa9b663adc64c8404
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-08-27 10:59:16 +0900

    20-5-3-using-declaration-and-access-control

---
+++ part3/ch20/20-5-3-using-declaration-and-access-control/src/using_declaration_and_access_control.cpp
@@ -0,0 +1,47 @@
+/*
+ * using_declaration_and_access_control.cpp
+ *
+ *  Created on: 2016/08/27
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+class B {
+private:
+	int a;
+protected:
+	int b;
+public:
+	int c;
+};
+
+class D : public B {
+public:
+	//using B::a;
+		// error: 'int B::a' is private
+	using B::b;
+};
+
+class BB : private B {
+public:
+	using B::b;
+//	using B::c;
+};
+
+// add main
+
+int main()
+{
+	D d;
+	d.b = 23;
+	cout << "d.b: " << d.b << '\n';
+
+	BB bb;
+	bb.b = 34;
+	//bb.c = 45;
+		// error: 'int B::c' is inaccessible
+	cout << "bb.b: " << bb.b << '\n';
+}
+
