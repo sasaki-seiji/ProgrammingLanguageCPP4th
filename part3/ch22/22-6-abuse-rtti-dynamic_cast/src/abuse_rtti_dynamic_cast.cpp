cebcd81e1b9f3a5106f9c0b8d9c67fe15341a65c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-09-05 22:17:35 +0900

    22-6-abuse-rtti-dynamic_castの追加

---
+++ part3/ch22/22-6-abuse-rtti-dynamic_cast/src/abuse_rtti_dynamic_cast.cpp
@@ -0,0 +1,62 @@
+/*
+ * abuse_rtti_dynamic_cast.cpp
+ *
+ *  Created on: 2016/09/05
+ *      Author: sasaki
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Object {
+public:
+	virtual ~Object() { }
+};
+
+class Container : public Object {
+public:
+	void put(Object* p) { v.push_back(p); }
+	Object* get() { Object *p = v.back(); v.pop_back(); return p; }
+private:
+	vector<Object*> v;
+};
+
+class Ship : public Object { };
+
+Ship* f(Ship* ps, Container* c)
+{
+	c->put(ps);
+
+	Object* p = c->get();
+	if (Ship* q = dynamic_cast<Ship*>(p)) {
+		return q;
+	}
+	else {
+		cout << "not ship object\n";
+	}
+	return nullptr;
+}
+
+Ship* f(Ship* ps, vector<Ship*>& c)
+{
+	c.push_back(ps);
+
+	Ship* p = c.back();
+	c.pop_back();
+	return p;
+}
+
+// add main
+
+int main()
+{
+	Container c;
+	Ship s;
+	Ship* p = f(&s, &c);
+	cout << "f(&s,Container*) == &s ? " << (p == &s) << '\n';
+
+	vector<Ship*> v;
+	Ship* q = f(&s, v);
+	cout << "f(&s, vector<Ship*>&) == &s ? " << (q == &s) << '\n';
+}
