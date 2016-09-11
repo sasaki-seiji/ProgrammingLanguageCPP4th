802a435d66953c662f729e5e0627fe635fd28e6d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-09-11 09:39:03 +0900

    23-4-3-member-aliasの追加

---
+++ part3/ch23/23-4-3-member-alias/src/user.cpp
@@ -0,0 +1,30 @@
+/*
+ * user.cpp
+ *
+ *  Created on: 2016/09/11
+ *      Author: sasaki
+ */
+
+
+#include "Vector.h"
+#include <iostream>
+using namespace std;
+
+template<typename T>
+T sum(Vector<T>& v)
+{
+	typename Vector<T>::value_type ans = T{};
+
+	for (typename Vector<T>::iterator it = v.begin(); it!=v.end(); ++it)
+		ans += *it;
+
+	return ans;
+}
+
+int main()
+{
+	Vector<int> vi{1,1,2,3,5,8,13};
+
+	int ans = sum(vi);
+	cout << "sum(vi) returns " << ans << '\n';
+}
