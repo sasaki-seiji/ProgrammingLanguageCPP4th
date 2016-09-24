c6f22bc6dfc11cd03b03c946943b94e127e9caa5
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-09-24 14:47:47 +0900

    26-3-3-point-of-instantiation-tempfuncの追加

---
+++ part3/ch26/26-3-3-point-of-instantiation-tempfunc/src/point_of_instantiation_tempfunc.cpp
@@ -0,0 +1,62 @@
+/*
+ * point_of_instantiation_tempfunc.cpp
+ *
+ *  Created on: 2016/09/24
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+// point of instantiation
+
+void g(int);
+
+template<typename T>
+void f(T a)
+{
+	g(a);
+}
+
+void h(int i)
+{
+	extern void g(double);
+	f(i);
+}
+
+// indirect recursive call
+
+void g2(int);
+
+template<typename T>
+void f2(T a)
+{
+	g2(a);
+	if (a>1) h2(T(a-1));
+}
+
+enum Count { one=1, two, three };
+void h2(Count i)
+{
+	f2(i);
+}
+
+// add undef
+
+void g(int i)
+{
+	cout << "g(int: " << i << ")\n";
+}
+
+void g2(int i)
+{
+	cout << "g2(int: " << i << ")\n";
+}
+
+// add main
+
+int main()
+{
+	h(12);
+	h2(two);
+}
