bd988e934382bc49340d92a6f1ed8687daf1351f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-09-24 16:07:29 +0900

    26-3-5-tempalte-and-namespaceの追加

---
+++ part3/ch26/26-3-5-template-and-namespace/src/template_and_namespace.cpp
@@ -0,0 +1,39 @@
+/*
+ * template_and_namespace.cpp
+ *
+ *  Created on: 2016/09/24
+ *      Author: sasaki
+ */
+
+#include <iostream>
+using namespace std;
+
+namespace N {
+	class A { };
+	char f(A) { cout << "N::f(A)\n"; return 'A'; }
+}
+
+char f(int i) { cout << "::f(int:" << i << ")\n"; return 'i'; }
+
+template<typename T>
+char g(T t)
+{
+	return f(t);
+}
+
+char f(double d) { cout << "::f(double:" << d << ")\n"; return d; }
+
+char c1 = g(N::A());
+char c2 = g(2);
+char c3 = g(2.1);
+
+
+// add main
+
+int main()
+{
+	cout << "c1: " << c1 << '\n';
+	cout << "c2: " << c2 << '\n';
+	cout << "c3: " << c3 << '\n';
+}
+
