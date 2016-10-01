bff3bfcc785fafc74498c9d3a52a74a00ff7f732
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-10-01 09:53:08 +0900

    27-4-1-associated-balancer-typeの追加

---
+++ part3/ch27/27-4-1-associated-balancer-type/src/balancedtree.h
@@ -0,0 +1,66 @@
+/*
+ * balancedtree.h
+ *
+ *  Created on: 2016/10/01
+ *      Author: sasaki
+ */
+
+#ifndef BALANCEDTREE_H_
+#define BALANCEDTREE_H_
+
+
+#include <ostream>
+
+template<typename N>
+struct Node_base : N::balance_type {
+	N* left_child;
+	N* right_child;
+
+	Node_base(): left_child{nullptr}, right_child{nullptr} { }
+
+	void add_left(N& n)
+	{
+		if (left_child==nullptr)
+			left_child = &n;
+		else if (this->compare(n, *left_child))
+			left_child->add_left(n);
+		else
+			left_child->add_right(n);
+	}
+
+	void add_right(N& n)
+	{
+		if (right_child==nullptr)
+			right_child = &n;
+		else if (this->compare(n, *right_child))
+			right_child->add_left(n);
+		else
+			right_child->add_right(n);
+	}
+};
+
+template<typename Val, typename Balance>
+struct Search_node : Node_base<Search_node<Val,Balance>> {
+	using balance_type = Balance;
+
+	Val v;
+	Search_node(Val vv) : v{vv} { }
+
+	void insert(Search_node& n)
+	{
+		if (this->compare(n,*this))
+			this->add_left(n);
+		else
+			this->add_right(n);
+	}
+
+	void print_tree(std::ostream& os)
+	{
+		if (this->left_child) this->left_child->print_tree(os);
+		os << ' ' << v;
+		if (this->right_child) this->right_child->print_tree(os);
+	}
+};
+
+
+#endif /* BALANCEDTREE_H_ */
---
+++ part3/ch27/27-4-1-associated-balancer-type/src/user.cpp
@@ -0,0 +1,50 @@
+/*
+ * user.cpp
+ *
+ *  Created on: 2016/10/01
+ *      Author: sasaki
+ */
+
+
+#include "balancedtree.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+
+struct Red_black_balance {
+#if 0
+	template<typename Val, typename Balance>
+	bool compare(const Search_node<Val, Balance>& a, const Search_node<Val, Balance>& b)
+	{
+		return a.v < b.v;
+	}
+#else
+	template<typename Val>
+	bool compare(const Search_node<Val, Red_black_balance>& a, const Search_node<Val, Red_black_balance>& b)
+	{
+		return a.v < b.v;
+	}
+#endif
+};
+
+template<typename T>
+using Rb_node = Search_node<T,Red_black_balance>;
+
+using My_node = Rb_node<double>;
+My_node my_root {3.5};
+
+void user(const vector<double>& v)
+{
+	for (auto x : v)
+		my_root.insert(*new My_node{x});
+
+	my_root.print_tree(cout);
+}
+
+// add main
+
+int main()
+{
+	vector<double> v {0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0};
+	user(v);
+}
