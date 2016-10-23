38cfcf5b9d0297642c8be0d03c6a27ebb628d2ff
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-10-23 14:42:57 +0900

    24-4-1-axiomの追加

---
+++ part3/ch24/24-4-1-axiom/src/axiom.cpp
@@ -0,0 +1,61 @@
+/*
+ * axiom.cpp
+ *
+ *  Created on: 2016/10/23
+ *      Author: sasaki
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+template<typename T>
+bool Copy_equality(T x)
+{
+	return T{x}==x;
+}
+
+template<typename T>
+bool Copy_assign_equality(T x, T& y)
+{
+	return (y=x, y==x);
+}
+
+// temporal impl
+template<typename T>
+bool can_destroy(T& x) { return true; }
+
+template<typename T>
+bool Move_effect(T x, T& y)
+{
+	return (x==y ? (x==T{std::move(y)}) : true) && can_destroy(y);
+}
+
+template<typename T>
+bool Move_assign_effect(T x, T& y, T& z)
+{
+	return (y==z ? (x=std::move(y), x==z) : true) && can_destroy(y);
+}
+
+
+// add main
+
+int main()
+{
+	cout << "Copy_equality(int) :" << Copy_equality(10) << '\n';
+	cout << "Copy_equality(string): " << Copy_equality(string{"abc"}) << '\n';
+
+	vector<int> vi{1,2,3},vo;
+	cout << "Copy_assign_equality(vector, vector&): " << Copy_assign_equality(vi, vo) << '\n';
+
+	string	s1{"abc"}, s2{"abc"};
+	cout << "Move_effect(string,string&): " << Move_effect(s1,s2) << '\n';
+	cout << "after moved - s2: " << s2 << '\n';
+
+	string	s3, s4{"abc"}, s5{"abc"};
+	cout << "Move_assign_effect(string,string&,string&): "
+			<< Move_assign_effect(s3,s4,s5) << '\n';
+	cout << "after moved - s4: " << s4 << '\n';
+}
+
