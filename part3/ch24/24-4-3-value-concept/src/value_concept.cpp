b3c20525d6eddb7e3a0eabde6c492cebe1c1215c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-10-24 20:21:26 +0900

    24-4-3-value-conceptの追加

---
+++ part3/ch24/24-4-3-value-concept/src/value_concept.cpp
@@ -0,0 +1,52 @@
+/*
+ * value_concept.cpp
+ *
+ *  Created on: 2016/10/24
+ *      Author: sasaki
+ */
+
+#include "concept.h"
+using namespace Estd;
+
+#include <iostream>
+using namespace std;
+
+template<int N>
+constexpr bool Small_size()
+{
+	return N<=8;
+}
+
+constexpr int stack_limit = 2048;
+
+template<typename T, int N>
+constexpr bool Stackable()
+{
+	return Regular<T>() && sizeof(T)*N <= stack_limit;
+}
+
+template<typename T, int N>
+struct Buffer {
+	T buf[N];
+};
+
+template<typename T, int N>
+void fct()
+{
+	static_assert(Stackable<T,N>(), "fct() buffer won't fit on stack");
+	Buffer<T,N> buf;
+	buf.buf[0] = T{};
+	cout << "buf.buf[0]: " << buf.buf[0] << '\n';
+}
+
+// add main
+
+int main()
+{
+	cout << "Small_size<4>(): " << Small_size<4>() << '\n';
+	cout << "Small_size<10>(): " << Small_size<10>() << '\n';
+
+	fct<int,512>();
+	//fct<int,1024>();
+		// error: static assertion failed: fct() buffer won't fit on stack
+}
