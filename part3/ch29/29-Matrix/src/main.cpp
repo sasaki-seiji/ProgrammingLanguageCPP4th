1d1baa6697a40abca64271f37cf7700ad691a605
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-11-17 20:05:35 +0900

    Matrix_ref *= scalar  の追加

--- part3/ch29/29-Matrix/src/main.cpp
+++ part3/ch29/29-Matrix/src/main.cpp
@@ -79,6 +79,7 @@ int main()
 	test_Matrix_ref_assign_scalar();
 	test_Matrix_ref_addassign_scalar();
 	test_Matrix_ref_subassign_scalar();
+	test_Matrix_ref_mulassign_scalar();
 	test_Matrix_ref_add_scalar();
 
 	// test_matrix_addition.cpp
