b419a85eb0c403c8c2d661f6aa071683d0d04d42
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-11-19 09:46:07 +0900

    Matrix_sliceのコンストラクタのテストに、次元数１，２を追加

--- part3/ch29/29-Matrix/src/test_matrix_slice.cpp
+++ part3/ch29/29-Matrix/src/test_matrix_slice.cpp
@@ -14,40 +14,80 @@ void test_default_Matrix_slice()
 {
 	cout << "--- test_default_Matrix_slice() --\n";
 
-	Matrix_slice<3> ms;
-	cout << "default Matrix_slice<3>: " << ms << endl;
+	Matrix_slice<3> ms3;
+	cout << "default Matrix_slice<3>: " << ms3 << endl;
 
+	Matrix_slice<2> ms2;
+	cout << "default Matrix_slice<2>: " << ms2 << endl;
+
+	Matrix_slice<1> ms1;
+	cout << "default Matrix_slice<1>: " << ms1 << endl;
 }
 
 void test_Matrix_slice_from_offset_extents()
 {
 	cout << "--- test_Matrix_slice_from_offset_extents() --\n";
 
-	Matrix_slice<3> ms(10, {2, 3, 4});
-	cout << "ms: " << ms << endl;
+	Matrix_slice<3> ms3(10, {2, 3, 4});
+	cout << "ms3: " << ms3 << endl;
 
-	cout << "ms(0,0,0): " << ms(0,0,0) << endl;
-	cout << "ms(1,2,3): " << ms(1,2,3) << endl;
+	cout << "ms3(0,0,0): " << ms3(0,0,0) << endl;
+	cout << "ms3(1,2,3): " << ms3(1,2,3) << endl;
+
+	Matrix_slice<2> ms2(10, {2, 3});
+	cout << "ms2: " << ms2 << endl;
+
+	cout << "ms2(0,0): " << ms2(0,0) << endl;
+	cout << "ms2(1,2): " << ms2(1,2) << endl;
+
+	Matrix_slice<1> ms1(10, {2});
+	cout << "ms1: " << ms1 << endl;
+
+	cout << "ms1(0): " << ms1(0) << endl;
+	cout << "ms1(1): " << ms1(1) << endl;
+
 }
 
 void test_Matrix_slice_from_offset_extents_strides()
 {
 	cout << "--- test_Matrix_slice_from_offset_extents_strides() --\n";
 
-	Matrix_slice<3> ms(10, {2, 3, 4}, {20, 5, 1});
-	cout << "ms: " << ms << endl;
+	Matrix_slice<3> ms3(10, {2, 3, 4}, {24, 8, 2});
+	cout << "ms3: " << ms3 << endl;
 
-	cout << "ms(0,0,0): " << ms(0,0,0) << endl;
-	cout << "ms(1,2,3): " << ms(1,2,3) << endl;
+	cout << "ms3(0,0,0): " << ms3(0,0,0) << endl;
+	cout << "ms3(1,2,3): " << ms3(1,2,3) << endl;
+
+	Matrix_slice<2> ms2(10, {3, 4}, {8, 2});
+	cout << "ms2: " << ms2 << endl;
+
+	cout << "ms2(0,0): " << ms2(0,0) << endl;
+	cout << "ms2(2,3): " << ms2(2,3) << endl;
+
+	Matrix_slice<1> ms1(10, {4}, {2});
+	cout << "ms1: " << ms1 << endl;
+
+	cout << "ms1(0): " << ms1(0) << endl;
+	cout << "ms1(3): " << ms1(3) << endl;
+
 }
 
 void test_Matrix_slice_from_variadic_extent()
 {
 	cout << "--- test_Matrix_slice_from_variadic_extent() --\n";
 
-	Matrix_slice<3> ms(2, 3, 4);
-	cout << "ms: " << ms << endl;
+	Matrix_slice<3> ms3(2, 3, 4);
+	cout << "ms3: " << ms3 << endl;
+	cout << "ms3(0,0,0): " << ms3(0,0,0) << endl;
+	cout << "ms3(1,2,3): " << ms3(1,2,3) << endl;
 
-	cout << "ms(0,0,0): " << ms(0,0,0) << endl;
-	cout << "ms(1,2,3): " << ms(1,2,3) << endl;
+	Matrix_slice<2> ms2(3, 4);
+	cout << "ms2: " << ms2 << endl;
+	cout << "ms2(0,0): " << ms2(0,0) << endl;
+	cout << "ms2(2,3): " << ms2(2,3) << endl;
+
+	Matrix_slice<1> ms1(4);
+	cout << "ms1: " << ms1 << endl;
+	cout << "ms1(0): " << ms1(0) << endl;
+	cout << "ms1(3): " << ms1(3) << endl;
 }
