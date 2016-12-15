20499a6e697d243563ab8de4c61d1e9aeefe3588
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-12-15 21:11:20 +0900

    「8.2.6:POD」の例題を詳細化
    is_podを構成する要素：
      is_standard_layout, 
      is_trivially_copyable, 
      is_trivially_default_constructible
      に分解

--- part2/ch08/8-2-6-pod/src/pod.cpp
+++ part2/ch08/8-2-6-pod/src/pod.cpp
@@ -28,15 +28,55 @@ struct S9 : S0, S1 { };
 void test_pod()
 {
 	cout << "is_pod<S0>::value ? " << is_pod<S0>::value << '\n';
+	cout << "  is_standard_layout<S0>::value ? " << is_standard_layout<S0>::value << '\n';
+	cout << "  is_trivially_copyable<S0>::value ? " << is_trivially_copyable<S0>::value << '\n';
+	cout << "  is_trivially_default_constructible<S0>::value ? " << is_trivially_default_constructible<S0>::value << '\n';
+
 	cout << "is_pod<S1>::value ? " << is_pod<S1>::value << '\n';
+	cout << "  is_standard_layout<S1>::value ? " << is_standard_layout<S1>::value << '\n';
+	cout << "  is_trivially_copyable<S1>::value ? " << is_trivially_copyable<S1>::value << '\n';
+	cout << "  is_trivially_default_constructible<S1>::value ? " << is_trivially_default_constructible<S1>::value << '\n';
+
 	cout << "is_pod<S2>::value ? " << is_pod<S2>::value << '\n';
+	cout << "  is_standard_layout<S2>::value ? " << is_standard_layout<S2>::value << '\n';
+	cout << "  is_trivially_copyable<S2>::value ? " << is_trivially_copyable<S2>::value << '\n';
+	cout << "  is_trivially_default_constructible<S2>::value ? " << is_trivially_default_constructible<S2>::value << '\n';
+
 	cout << "is_pod<S3>::value ? " << is_pod<S3>::value << '\n';
+	cout << "  is_standard_layout<S3>::value ? " << is_standard_layout<S3>::value << '\n';
+	cout << "  is_trivially_copyable<S3>::value ? " << is_trivially_copyable<S3>::value << '\n';
+	cout << "  is_trivially_default_constructible<S3>::value ? " << is_trivially_default_constructible<S3>::value << '\n';
+
 	cout << "is_pod<S4>::value ? " << is_pod<S4>::value << '\n';
+	cout << "  is_standard_layout<S4>::value ? " << is_standard_layout<S4>::value << '\n';
+	cout << "  is_trivially_copyable<S4>::value ? " << is_trivially_copyable<S4>::value << '\n';
+	cout << "  is_trivially_default_constructible<S4>::value ? " << is_trivially_default_constructible<S4>::value << '\n';
+
 	cout << "is_pod<S5>::value ? " << is_pod<S5>::value << '\n';
+	cout << "  is_standard_layout<S5>::value ? " << is_standard_layout<S5>::value << '\n';
+	cout << "  is_trivially_copyable<S5>::value ? " << is_trivially_copyable<S5>::value << '\n';
+	cout << "  is_trivially_default_constructible<S5>::value ? " << is_trivially_default_constructible<S5>::value << '\n';
+
 	cout << "is_pod<S6>::value ? " << is_pod<S6>::value << '\n';
+	cout << "  is_standard_layout<S6>::value ? " << is_standard_layout<S6>::value << '\n';
+	cout << "  is_trivially_copyable<S6>::value ? " << is_trivially_copyable<S6>::value << '\n';
+	cout << "  is_trivially_default_constructible<S6>::value ? " << is_trivially_default_constructible<S6>::value << '\n';
+
 	cout << "is_pod<S7>::value ? " << is_pod<S7>::value << '\n';
+	cout << "  is_standard_layout<S7>::value ? " << is_standard_layout<S7>::value << '\n';
+	cout << "  is_trivially_copyable<S7>::value ? " << is_trivially_copyable<S7>::value << '\n';
+	cout << "  is_trivially_default_constructible<S7>::value ? " << is_trivially_default_constructible<S7>::value << '\n';
+
 	cout << "is_pod<S8>::value ? " << is_pod<S8>::value << '\n';
+	cout << "  is_standard_layout<S8>::value ? " << is_standard_layout<S8>::value << '\n';
+	cout << "  is_trivially_copyable<S8>::value ? " << is_trivially_copyable<S8>::value << '\n';
+	cout << "  is_trivially_default_constructible<S8>::value ? " << is_trivially_default_constructible<S8>::value << '\n';
+
 	cout << "is_pod<S9>::value ? " << is_pod<S9>::value << '\n';
+	cout << "  is_standard_layout<S9>::value ? " << is_standard_layout<S9>::value << '\n';
+	cout << "  is_trivially_copyable<S9>::value ? " << is_trivially_copyable<S9>::value << '\n';
+	cout << "  is_trivially_default_constructible<S9>::value ? " << is_trivially_default_constructible<S9>::value << '\n';
+
 }
 
 template<typename T>
@@ -58,7 +98,9 @@ int main()
 	test_pod();
 
 	int	ia1[10], ia2[10];
+		// 'int' has trivial default constructor, so array do not initialize each element
 	string sa1[10], sa2[10];
+		// 'string' has non-trivial default constructor, so array initialize each element
 
 	mycopy(ia1, ia2, 10);
 	mycopy(sa1, sa2, 10);
