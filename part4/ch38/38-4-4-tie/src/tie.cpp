1e665f0c0d5ed9d4dad692bb82c12378cfcde0d2
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-12-26 23:15:56 +0900

    38-4-4-tieの追加

---
+++ part4/ch38/38-4-4-tie/src/tie.cpp
@@ -0,0 +1,18 @@
+/*
+ * tie.cpp
+ *
+ *  Created on: 2016/12/26
+ *      Author: sasaki
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Please enter a number: ";
+	int num;
+	cin >> num;
+	cout << "entered number: " << num << endl;
+}
