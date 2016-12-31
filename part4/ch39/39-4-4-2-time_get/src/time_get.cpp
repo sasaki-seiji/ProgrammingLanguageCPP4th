4e20879714798f580e63c9ff7d1ecf49bc5b15f5
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-12-31 15:37:07 +0900

    39-4-4-2-time_getの追加

---
+++ part4/ch39/39-4-4-2-time_get/src/time_get.cpp
@@ -0,0 +1,36 @@
+/*
+ * time_get.cpp
+ *
+ *  Created on: 2016/12/31
+ *      Author: sasaki
+ */
+
+#include <iostream>
+#include <locale>
+#include <sstream>
+using namespace std;
+
+#include "Date.h"
+using namespace Chrono;
+
+
+void test()
+{
+	//Date d1(3,10,1980);
+	Date d1(3,Month::oct,1980);
+	cout << d1 << '\n';
+
+	auto order = use_facet<time_get<char>>(cin.getloc()).date_order();
+	if (order == time_base::mdy)
+		cout << "month day year\n";
+	else
+		cout << "poor guess\n";
+	stringstream ss ("10/3/1980");
+	ss >> d1;
+	cout << d1 << '\n';
+}
+
+int main()
+{
+	test();
+}
