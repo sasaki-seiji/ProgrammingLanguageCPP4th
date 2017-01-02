17c515ae16c1fc0edc2533c281cbfb7d9ea0cc22
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-02 12:10:27 +0900

    テキストの整形

--- part4/ch39/39-2-1-named-locale/src/named_locale.cpp
+++ part4/ch39/39-2-1-named-locale/src/named_locale.cpp
@@ -13,19 +13,14 @@ int main()
 {
 	locale loc1;
 	cout << "loc1: " << loc1.name() << '\n';
-
 	locale loc2 {""};
 	cout << "loc2: " << loc2.name() << '\n';
-
 	locale loc3 {"C"};
 	cout << "loc3: " << loc3.name() << '\n';
-
 	locale loc4 {locale::classic()};
 	cout << "loc4: " << loc4.name() << '\n';
-
 	locale loc5 {"POSIX"};
 	cout << "loc5: " << loc5.name() << '\n';
-
 	try {
 		locale loc6 {"Danish_Denmark.1252"};
 		cout << "loc6: " << loc6.name() << '\n';
@@ -33,7 +28,6 @@ int main()
 	catch (const runtime_error& e) {
 		cerr << "locale(\"Danish_Denmark.1252\"): " << e.what() << endl;
 	}
-
 	locale loc7 {"en_US.UTF-8"};
 	cout << "loc7: " << loc7.name() << '\n';
 }
