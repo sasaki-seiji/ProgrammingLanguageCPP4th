0a74d7ff4a32f56e6cb6006ddc0ae7401510c90d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-04 20:48:03 +0900

    39-4-5-count-spacesの追加

---
+++ part4/ch39/39-4-5-count-spaces/src/count_spaces.cpp
@@ -0,0 +1,47 @@
+/*
+ * count_spaces.cpp
+ *
+ *  Created on: 2017/01/04
+ *      Author: sasaki
+ */
+
+
+#include <locale>
+#include <iostream>
+using namespace std;
+
+int count_spaces(const string& s, const locale& loc)
+{
+	const ctype<char>& ct = use_facet<ctype<char>>(loc);
+	int i = 0;
+
+	for(auto p = s.begin(); p!=s.end(); ++p)
+		if (ct.is(ctype_base::space,*p))
+			++i;
+	return i;
+}
+
+int count_spaces_puncts(const string& s, const locale& loc)
+{
+	const ctype<char>& ct = use_facet<ctype<char>>(loc);
+	int i = 0;
+
+	for(auto p = s.begin(); p!=s.end(); ++p)
+		if (ct.is(ctype_base::space|ctype_base::punct,*p))
+			++i;
+	return i;
+}
+
+
+
+int main()
+{
+	string s = "abc def\tghi$jkl#\nmno\fpqr";
+	locale loc1{};
+
+	int i1 = count_spaces(s, loc1);
+	cout << "i1: " << i1 << endl;
+
+	int i2 = count_spaces_puncts(s, loc1);
+	cout << "i2: " << i2 << endl;
+}
