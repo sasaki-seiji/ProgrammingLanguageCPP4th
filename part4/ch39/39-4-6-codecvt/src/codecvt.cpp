b7e1f2d24c5beb31f5f7b95e42555a8ba870e195
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-05 22:26:49 +0900

    内部動作の解析：alway_noconv呼び出しの確認

--- part4/ch39/39-4-6-codecvt/src/codecvt.cpp
+++ part4/ch39/39-4-6-codecvt/src/codecvt.cpp
@@ -13,6 +13,7 @@ int main()
 {
 	locale ulocale(locale("C"), new Cvt_to_upper);
 	cerr << ulocale.name() << endl;
+	cerr << use_facet<codecvt<char,char,mbstate_t>>(ulocale).always_noconv() << endl;
 
 	cin.imbue(ulocale);
 	for (char ch; cin>>ch; )
