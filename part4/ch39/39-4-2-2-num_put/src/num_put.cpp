1ca65ae0236dcb809ab312528d5932e26be47ffb
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-09 08:33:32 +0900

    文字列ストリームへの出力と、da_DKロケールへの出力を追加

--- part4/ch39/39-4-2-2-num_put/src/num_put.cpp
+++ part4/ch39/39-4-2-2-num_put/src/num_put.cpp
@@ -6,11 +6,21 @@
  */
 
 
+#include <sstream>
 #include "Double.h"
 using namespace std;
 
 int main()
 {
-	Double d {1.234};
+	Double d {12345.6789};
 	cout << d << endl;
+
+	ostringstream oss;
+	oss << d;
+	cout << oss.str() << endl;
+
+	ostringstream oss2;
+	oss2.imbue(locale("da_DK.UTF-8"));
+	oss2 << d;
+	cout << oss2.str() << endl;
 }
