9c90f14bb93343ecfd3fa1b65ebb2eec2e3fbf91
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-08 21:48:11 +0900

    39-4-2-1-format-numberから39-4-2-1-numpunctに名前を変更
    更に、各種ロケールでの数値表現をテストを追加

--- part4/ch39/39-4-2-1-numpunct/src/numpunct.cpp
+++ part4/ch39/39-4-2-1-numpunct/src/numpunct.cpp
@@ -24,8 +24,23 @@ void f()
 			<< " *** " << fixed << 1234567.8 << '\n';
 }
 
+void test_numpunct(const locale& loc)
+{
+	cout << "-- " << loc.name() << " --\n";
+	cout.imbue(loc);
+	cout << 123456789 << endl;
+	cout << defaultfloat << 12345678.9 << endl;
+	cout << fixed << 12345678.9 << endl;
+	cout << boolalpha << "true: " << true << ", false: " << false << endl;
+}
+
 int main()
 {
 	f();
+
+	test_numpunct(locale("en_US.UTF-8"));
+	test_numpunct(locale("fr_FR.UTF-8"));
+	test_numpunct(locale("da_DK"));
+	test_numpunct(locale(""));
 }
 
