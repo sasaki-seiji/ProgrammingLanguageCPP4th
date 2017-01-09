699e51b46bcdd9bbc30caf3e49dc365cfa10b311
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-09 12:35:55 +0900

    各種ロケールの出力を追加

--- part4/ch39/39-4-3-money-formatting/src/money_formatting.cpp
+++ part4/ch39/39-4-3-money-formatting/src/money_formatting.cpp
@@ -17,22 +17,27 @@ void f(long int i)
 int main()
 {
 	long long i = -123456789;
+	cout << "default:     ";
 	f(i);
 
 	locale loc_my_money {locale(), new My_money_io};
+	cout << "My_money_io: ";
 	cout.imbue(loc_my_money);
 	f(i);
 
 	locale loc_en{"en_US.UTF-8"};
+	cout << "en_US.UTF-8: ";
 	cout.imbue(loc_en);
 	f(i);
 
 	locale loc_jp{""};
+	cout << "preferred:   ";
 	cout.imbue(loc_jp);
 	f(i);
 
-	locale loc_de{"de_DE"};
-	cout.imbue(loc_de);
+	locale loc_fr{"fr_FR.UTF-8"};
+	cout << "fr_FR.UTF-8: ";
+	cout.imbue(loc_fr);
 	f(i);
 }
 
