2027afaa1e1ed624fa30f2e2e6248da244285468
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-15 13:52:10 +0900

    40-7-3-bernoulli_distributionの追加

---
+++ part4/ch40/40-7-3-bernoulli_distribution/src/bernoulli_distribution.cpp
@@ -0,0 +1,29 @@
+/*
+ * bernoulli_distribution.cpp
+ *
+ *  Created on: 2017/01/15
+ *      Author: sasaki
+ */
+
+// bernoulli_distribution
+#include <iostream>
+#include <random>
+
+int main()
+{
+  const int nrolls=10000;
+
+  std::default_random_engine generator;
+  std::bernoulli_distribution distribution(0.5);
+
+  int count=0;  // count number of trues
+
+  for (int i=0; i<nrolls; ++i) if (distribution(generator)) ++count;
+
+  std::cout << "bernoulli_distribution (0.5) x 10000:" << std::endl;
+  std::cout << "true:  " << count << std::endl;
+  std::cout << "false: " << nrolls-count << std::endl;
+
+  return 0;
+}
+
