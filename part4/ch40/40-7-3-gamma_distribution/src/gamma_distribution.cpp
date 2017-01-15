3446318367e2ae713cb0b60cd0b54f6c8d3768f2
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-15 15:17:50 +0900

    40-7-3-gamma_distributionの追加

---
+++ part4/ch40/40-7-3-gamma_distribution/src/gamma_distribution.cpp
@@ -0,0 +1,41 @@
+/*
+ * gamma_distribution.cpp
+ *
+ *  Created on: 2017/01/15
+ *      Author: sasaki
+ */
+
+// gamma_distribution
+#include <iostream>
+#include <random>
+
+int main()
+{
+  const int nrolls=10000;  // number of experiments
+  const int nstars=100;    // maximum number of stars to distribute
+
+  std::default_random_engine generator;
+  std::gamma_distribution<double> distribution(2.0,1.5);
+
+  int p[10]={};
+  int over_10 = 0;
+
+  for (int i=0; i<nrolls; ++i) {
+    double number = distribution(generator);
+    if (number<10) ++p[int(number)];
+    else ++over_10;
+  }
+
+  std::cout << "gamma_distribution (2.0,1.5):" << std::endl;
+
+  for (int i=0; i<10; ++i) {
+    std::cout << i << "-" << (i+1) << ": ";
+    std::cout << std::string(p[i]*nstars/nrolls,'*') << std::endl;
+  }
+
+  std::cout << "over 10.0 time intervals: " << over_10 << std::endl;
+
+  return 0;
+}
+
+
