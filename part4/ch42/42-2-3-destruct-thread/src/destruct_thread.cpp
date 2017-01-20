f4730fcc02eac6180bc9e58347af9a79964b6a3f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-20 20:58:16 +0900

    heartbeatモジュールを分離

--- part4/ch42/42-2-3-destruct-thread/src/destruct_thread.cpp
+++ part4/ch42/42-2-3-destruct-thread/src/destruct_thread.cpp
@@ -5,25 +5,10 @@
  *      Author: sasaki
  */
 
-
 #include <thread>
-#include <chrono>
-#include <iostream>
+#include "heartbeat.h"
 using namespace std;
-using namespace std::chrono;
 
-void output(steady_clock::time_point tp)
-{
-	cout << tp.time_since_epoch().count() << endl;
-}
-
-void heartbeat()
-{
-	while(true) {
-		output(steady_clock::now());
-		this_thread::sleep_for(chrono::seconds{1});
-	}
-}
 
 void run()
 {
