22fe942b8b96059eacad860f37b52026284dce64
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-30 19:10:43 +0900

    Linux環境に対応

---
+++ part1/ch02/2-2-1-hello-world/2-2-1-minimum/src/2-1-1-minimum.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : 2-1-1-minimum.cpp
+// Author      : sasaki seiji
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+int main() { }
+	// main function do not have to return value.
+	// if main does not return value, system receive the value
+	// indicating success.
+
