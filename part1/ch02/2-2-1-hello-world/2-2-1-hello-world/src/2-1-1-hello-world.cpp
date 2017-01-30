b1d40e8c0f62166349011c8be0c6c81d825a9fed
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-01-30 19:06:54 +0900

    Linux環境に対応

---
+++ part1/ch02/2-2-1-hello-world/2-2-1-hello-world/src/2-1-1-hello-world.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : 2-1-1-hello-world.cpp
+// Author      : sasaki seiji
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello World!\n" ;
+	return 0;
+}
