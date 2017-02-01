dd87efdc71cb0a97b56cf78f992956059ec2b257
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-01 20:53:09 +0900

    Linux環境に対応

--- part1/ch03/3-2-2-abstract-type/src/use.cpp
+++ part1/ch03/3-2-2-abstract-type/src/use.cpp
@@ -22,6 +22,7 @@ void use(Container& c)
 
 void g()
 {
+	cout << "Vector_container(10):\n";
 	Vector_container vc(10);
 	vc[0] = 1.2; vc[5] = 2.3;	// 2016.03.26 add
 	use(vc);
@@ -29,6 +30,7 @@ void g()
 
 void h()
 {
+	cout << "List_container:\n";
 	List_container lc = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 	use(lc);
 }
