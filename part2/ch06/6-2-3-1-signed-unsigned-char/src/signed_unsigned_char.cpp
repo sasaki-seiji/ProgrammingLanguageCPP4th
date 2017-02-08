910462e29463fabeb1f3f24641bb0608b53ae253
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-08 20:51:52 +0900

    Linux環境に対応

--- part2/ch06/6-2-3-1-signed-unsigned-char/src/signed_unsigned_char.cpp
+++ part2/ch06/6-2-3-1-signed-unsigned-char/src/signed_unsigned_char.cpp
@@ -16,9 +16,9 @@ void f(char c, signed char sc, unsigned char uc)
 {
 	//char* pc = &uc;
 		// invalid conversion from 'unsigned char*' to 'char*' [-fpermissive]
-	//signed char* psc = pc;
+	//signed char* psc = &c;
 		// invalid conversion from 'char*' to 'signed char*' [-fpermissive]
-	//unsigned char* puc = pc;
+	//unsigned char* puc = &c;
 		// invalid conversion from 'char*' to 'unsigned char*' [-fpermissive]
 	//psc = puc;
 		// invalid conversion from 'unsigned char*' to 'signed char*' [-fpermissive]
@@ -26,20 +26,20 @@ void f(char c, signed char sc, unsigned char uc)
 
 void g(char c, signed char sc, unsigned char uc)
 {
-	c = 255;
+	c = 255;	// no warning
 	c = sc;
-	c = uc;
-	sc = uc;
+	c = uc;		// no warning
+	sc = uc;	// no warning
 	uc = sc;
 	sc = c;
 	uc = c;
 
-	cout << "c = " << c << ", sc = " << sc << ", uc = " << uc << '\n';
+	cout << "int{c} = " << int{c} << ", int{sc} = " << int{sc} << ", int{uc} = " << int{uc} << '\n';
 }
 
 int main()
 {
-	cout << "c = " << c << ", i = " << i << '\n';
+	cout << "int{c} = " << int{c} << ", i = " << i << '\n';
 
 	g('a', 'b', 'c');
 	g('a', -100, 128);
