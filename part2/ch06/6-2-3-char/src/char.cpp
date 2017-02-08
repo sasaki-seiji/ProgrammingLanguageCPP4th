d0c6848e43e0fcc572c007a864a8f3d76526b5b5
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-08 20:39:26 +0900

    Linux環境に対応

--- part2/ch06/6-2-3-char/src/char.cpp
+++ part2/ch06/6-2-3-char/src/char.cpp
@@ -10,12 +10,16 @@ using namespace std;
 
 void inval()
 {
+	cout << "inval():\n";
+
 	for (char c; cin >> c; )
 		cout << "the value of '" << c << "' is " << int{c} << '\n';
 }
 
 void digits()
 {
+	cout << "digits():\n";
+
 	for (int i=0; i!=10; ++i)
 		cout << static_cast<char>('0'+i);
 }
