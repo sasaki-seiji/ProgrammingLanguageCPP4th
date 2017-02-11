3a8e856743fa6d65b2b9a1f02b1aac3df9f6e83e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-11 17:06:36 +0900

    Linux環境に対応

--- part2/ch06/6-5-type-alias/src/type_alias.cpp
+++ part2/ch06/6-5-type-alias/src/type_alias.cpp
@@ -5,6 +5,7 @@
  *      Author: sasaki
  */
 
+#include <typeinfo>
 #include <vector>
 #include <iostream>
 using namespace std;
@@ -22,14 +23,23 @@ class My_allocator {
 };
 
 template<typename T>
-	using vector = std::vector<T, My_allocator<T>>;
+	using Vector = std::vector<T, My_allocator<T>>;
 
 using Char = char;
-//using Uchar = unsigned Char;
-using Uchar = unsigned char;
+using Uchar = unsigned Char;	// not error
+using Uchar2 = unsigned char;
 
 int main()
 {
+	cout << "typeid(Pchar) = " << typeid(Pchar).name() << '\n';
+	cout << "typeid(PF) = " << typeid(PF).name() << '\n';
+	cout << "typeid(int32_t) = " << typeid(int32_t).name() << '\n';
+	cout << "typeid(int16_t) = " << typeid(int16_t).name() << '\n';
+	cout << "typeid(PtoF) = " << typeid(PtoF).name() << '\n';
+	cout << "typeid(Char) = " << typeid(Char).name() << '\n';
+	cout << "typeid(Uchar) = " << typeid(Uchar).name() << '\n';
+	cout << "typeid(Uchar2) = " << typeid(Uchar2).name() << '\n';
+
 	Pchar p1 = nullptr;
 	char* p3 =p1;
 
