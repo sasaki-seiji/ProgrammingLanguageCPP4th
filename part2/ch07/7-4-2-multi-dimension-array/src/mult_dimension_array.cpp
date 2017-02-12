20a8aa74093d4f384b70b8728645dcbd2a7a09f9
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-12 21:20:07 +0900

    Linux環境に対応

--- part2/ch07/7-4-2-multi-dimension-array/src/mult_dimension_array.cpp
+++ part2/ch07/7-4-2-multi-dimension-array/src/mult_dimension_array.cpp
@@ -14,7 +14,7 @@ void init_ma()
 {
 	for (int i = 0; i!=3; ++i)
 		for (int j = 0; j!=5; ++j)
-			ma[i][j] = 10*i*j;
+			ma[i][j] = 10*i+j;
 }
 
 void print_ma()
@@ -27,11 +27,11 @@ void print_ma()
 }
 
 //int bad[3,5];
-	// expected ']' before ',' token
-	// expected unqualified-id before numeric constant
+	// error: expected ']' before ',' token
+	// error: expected unqualified-id before numeric constant
 int good[3][5];
 //int ouch = good[1,4];
-	// invalid conversion from 'int*' to 'int' [-fpermissive]
+	// error: invalid conversion from 'int*' to 'int' [-fpermissive]
 int nice = good[1][4];
 
 int main()
