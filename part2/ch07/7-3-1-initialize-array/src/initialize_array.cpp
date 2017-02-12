c8ae52f2cfa76bcdd662f7afe99930ceb35386e8
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-12 18:52:23 +0900

    Linux環境に対応

--- part2/ch07/7-3-1-initialize-array/src/initialize_array.cpp
+++ part2/ch07/7-3-1-initialize-array/src/initialize_array.cpp
@@ -12,20 +12,20 @@ int v1[] = { 1, 2, 3, 4 };
 char v2[] = { 'a', 'b', 'c', 0 };
 
 //char v3[2] = { 'a', 'b', 0 };
-	// too many initializers for 'char [2]'
+	// error: too many initializers for ‘char [2]’
 
 char v4[3] = { 'a', 'b', 0 };
 
 int v5[8] = { 1, 2, 3, 4 };
 
 //int v6[8] = v5;
-	// array must be initialized with a brace-enclosed initializer
+	// error: array must be initialized with a brace-enclosed initializer
 
 int main()
 {
 	int v6[8];
 	//v6 = v5;
-		// invalid array assignment
+		// error: invalid array assignment
 
 	cout << "sizeof(v1)/sizeof(v1[0]) = " << sizeof(v1)/sizeof(v1[0]) << '\n';
 	cout << "sizeof(v2)/sizeof(v2[0]) = " << sizeof(v2)/sizeof(v2[0]) << '\n';
