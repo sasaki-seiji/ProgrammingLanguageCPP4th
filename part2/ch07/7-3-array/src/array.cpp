472c8313f68d14c72dfc2de88af2bee826bd32ed
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-12 18:45:19 +0900

    Linux環境に対応

--- part2/ch07/7-3-array/src/array.cpp
+++ part2/ch07/7-3-array/src/array.cpp
@@ -14,15 +14,20 @@ char* a[32];
 
 void f()
 {
+	cout << "-- f() --\n";
+
 	int aa[10];
 	aa[6] = 9;
 	int x = aa[99];
 
+	cout << "aa[6] = " << aa[6] << '\n';
 	cout << "x = " << x << '\n';
 }
 
 void f(int n)
 {
+	cout << "-- f(" << n << ") --\n";
+
 	int v1[n];
 	vector<int> v2(n);
 
@@ -34,6 +39,8 @@ int a1[10];
 
 void f2()
 {
+	cout << "-- f2() --\n";
+
 	int a2[20];
 	int *p = new int[40];
 
