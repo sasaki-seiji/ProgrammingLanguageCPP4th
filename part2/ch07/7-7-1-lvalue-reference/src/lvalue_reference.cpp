2741ed9a4a815f7e331d1de8966d8ce0a573f4b5
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-13 20:43:48 +0900

    単語のカウントを7-7-1-word-countプロジェクトに分離

--- part2/ch07/7-7-1-lvalue-reference/src/lvalue_reference.cpp
+++ part2/ch07/7-7-1-lvalue-reference/src/lvalue_reference.cpp
@@ -72,27 +72,6 @@ void g2()
 	cout << "x = " << x << '\n';
 }
 
-template<typename K, typename V>
-class Map {
-public:
-	V& operator[](const K& k);
-	pair<K,V>* begin() { return &elem[0]; }
-	pair<K,V>* end() { return &elem[0]+elem.size(); }
-private:
-	vector<pair<K,V>> elem;
-};
-
-template<typename K, typename V>
-V& Map<K,V>::operator[](const K& k)
-{
-	for (auto& x : elem)
-		if (k == x.first)
-			return x.second;
-
-	elem.push_back({k,V{ }});
-	return elem.back().second;
-}
-
 int main()
 {
 	cout << "::var = " << ::var << '\n';
@@ -103,12 +82,6 @@ int main()
 	g();
 	f2();
 	g2();
-
-	cout << "-- enter words:\n";
-	Map<string,int> buf;
-	for (string s; cin >> s; ) ++buf[s];
-	for (const auto& x : buf)
-		cout << x.first << ": " << x.second << '\n';
 }
 
 int temp = 3;
