f2f85be37d93fd3c57782ee739108f65e90de235
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-14 21:34:32 +0900

    Linux環境に対応

--- part2/ch07/7-7-4-pointer-and-reference/src/pointer_and_reference.cpp
+++ part2/ch07/7-7-4-pointer-and-reference/src/pointer_and_reference.cpp
@@ -11,18 +11,23 @@ using namespace std;
 
 void fp(char* p)
 {
+	cout << "\n-- fp(" << p << ") --\n";
+
 	while (*p)
 		cout << *p++;
 }
 
 void fr(char& r)
 {
+	cout << "\n-- fr(" << r << ") --\n";
+
 	while (r)
 		cout << r++;
 }
 
 void fr2(char& r)
 {
+	cout << "\n-- fr2(" << r << ") --\n";
 	char* p = &r;
 	while (*p)
 		cout << *p++;
@@ -33,6 +38,7 @@ class Proxy {
 	T& m;
 public:
 	Proxy(T& mm) :m {mm} { }
+	void print() { cout << m << endl; }
 };
 
 template<typename T>
@@ -41,27 +47,29 @@ class Handle {
 public:
 	Handle(T* mm) :m{mm} { }
 	void rebind(T* mm) { m = mm; }
+	void print() { cout << *m << endl; }
 };
 
 struct Matrix { };
 
 Matrix operator+(const Matrix& a, const Matrix& b) { return a; }
 //Matrix operator-(const Matrix*, const Matrix*);
-	// 'Matrix operator-(const Matrix*, const Matrix*)' must have an argument of class or enumerated type
+	// error: 'Matrix operator-(const Matrix*, const Matrix*)' must have an argument of class or enumerated type
 
 Matrix y, z;
 Matrix x = y+z;
 //Matrix x2 = &y-&z;
-	// conversion from 'long long int' to non-scalar type 'Matrix' requested
+	// error: conversion from 'long long int' to non-scalar type 'Matrix' requested
 
 string s1 = "College Station";
 string s2 = "Manhattan";
 
 //string& a1[] = {s1, s2};
-	// declaration of 'a1' as array of references
+	// error: declaration of 'a1' as array of references
 string* a2[] = {&s1, &s2};
 
 //vector<string&> v1 = {s1, s2};
+	// error: could not convert ‘{s1, s2}’ from ‘<brace-enclosed initializer list>’ to ‘std::vector<std::__cxx11::basic_string<char>&>’
 vector<string*> v2 = {&s1, &s2};
 
 char* ident(char* p) { return p; }
@@ -69,24 +77,27 @@ char& r { *ident(nullptr) };
 
 int main()
 {
-	char s[] = { 'a', 'b', 'c', '\0' };
+	char s[] = { 'a', 'b', 'c', 'd', '\0' };
 
-	cout << "\nfp(&s[0]):\n";
 	fp(&s[0]);
-
-	cout << "\nfr(s[0]):\n";
 	fr(s[0]);
 
-	cout << "\nfr2(s[0]):\n";
 	s[0] = 'a'; // fr(s[0]) changes s[0] !!!
 	fr2(s[0]);
 
+	cout << "\n-- proxy and handle --\n";
+
 	int	x = 10, y = 20;
 	Proxy<int> proxy {x};
-	Handle<int> handle {&x};
-	handle.rebind(&y);
+	proxy.print();
 
-	//cout << "r = " << r << '\n';
-	// crash!!!
+	Handle<int> handle {&x};
+	handle.print();
+	handle.rebind(&y);
+	handle.print();
+
+	cout << "r = " << r << '\n';
+	// MinGW windows: crash!!!
+	// Ubuntu: terminated with exit value: -1
 }
 
