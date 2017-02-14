e325219d159d61124d7ee24e6d1363dcc305fccf
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-14 20:34:00 +0900

    Linux環境に対応

--- part2/ch07/7-7-2-rvalue-reference/src/rvalue_reference.cpp
+++ part2/ch07/7-7-2-rvalue-reference/src/rvalue_reference.cpp
@@ -10,23 +10,25 @@
 using namespace std;
 
 string var {"Cambridge"};
-string f() { return ""; }
+string f() { return "string returned from f()"; }
 
 string& r1 {var};
 //string& r2 {f()};
-	// invalid initialization of non-const reference of type 'std::__cxx11::string& {aka std::__cxx11::basic_string<char>&}' from an rvalue of type 'std::__cxx11::string {aka std::__cxx11::basic_string<char>}'
+	// error: invalid initialization of non-const reference of type 'std::__cxx11::string& {aka std::__cxx11::basic_string<char>&}' from an rvalue of type 'std::__cxx11::string {aka std::__cxx11::basic_string<char>}'
 //string& r3 {"Princeton"};
-	// invalid initialization of non-const reference of type 'std::__cxx11::string& {aka std::__cxx11::basic_string<char>&}' from an rvalue of type 'std::__cxx11::string {aka std::__cxx11::basic_string<char>}'
+	// error: invalid initialization of non-const reference of type 'std::__cxx11::string& {aka std::__cxx11::basic_string<char>&}' from an rvalue of type 'std::__cxx11::string {aka std::__cxx11::basic_string<char>}'
 
-//string&& rr1 {var};
-	// cannot bind 'std::__cxx11::string {aka std::__cxx11::basic_string<char>}' lvalue to 'std::__cxx11::string&& {aka std::__cxx11::basic_string<char>&&}'
-string&& rr2 {f()};
+string&& rr1 {f()};
+//string&& rr2 {var};
+	// error: cannot bind 'std::__cxx11::string {aka std::__cxx11::basic_string<char>}' lvalue to 'std::__cxx11::string&& {aka std::__cxx11::basic_string<char>&&}'
 string&& rr3 {"Oxford"};
 
 const string& cr1 {"Harvard"};
 
 string f(string&& s)
 {
+	cout << "-- f(string&& \"" << s << "\") --\n";
+
 	if (s.size())
 		s[0] = toupper(s[0]);
 	return s ;
@@ -35,6 +37,8 @@ string f(string&& s)
 template<typename T>
 void swap1(T& a, T& b)
 {
+	cout << "-- swap1(T&,T&) --\n";
+
 	T tmp {a};
 	a = b;
 	b = tmp;
@@ -43,6 +47,8 @@ void swap1(T& a, T& b)
 template<typename T>
 void swap2(T& a, T& b)
 {
+	cout << "-- swap2(T&,T&) --\n";
+
 	T tmp {static_cast<T&&>(a)};
 	a = static_cast<T&&>(b);
 	b = static_cast<T&&>(tmp);
@@ -51,6 +57,8 @@ void swap2(T& a, T& b)
 template<typename T>
 void swap3(T& a, T& b)
 {
+	cout << "-- swap3(T&,T&) --\n";
+
 	T tmp {move(a)};
 	a = move(b);
 	b = move(tmp);
@@ -59,6 +67,8 @@ void swap3(T& a, T& b)
 template<typename T>
 void swap3(T&& a, T& b)
 {
+	cout << "-- swap3(T&&,T&) --\n";
+
 	T tmp {move(a)};
 	a = move(b);
 	b = move(tmp);
@@ -67,6 +77,8 @@ void swap3(T&& a, T& b)
 template<typename T>
 void swap3(T& a, T&& b)
 {
+	cout << "-- swap3(T&,T&&) --\n";
+
 	T tmp {move(a)};
 	a = move(b);
 	b = move(tmp);
@@ -74,46 +86,68 @@ void swap3(T& a, T&& b)
 
 void f(vector<int>& v)
 {
+	cout << "-- f(vector<int>&) --\n";
+
 	swap3(v, vector<int>{1,2,3});
 }
 
-void f(string& s, vector<int>& v)
+void f(string& s1, string& s2, vector<int>& v1, vector<int>& v2, vector<int>& v3)
 {
-	s.shrink_to_fit();
-	swap3(s, string{s});
-	v.clear();
-	swap3(v, vector<int>{});
-	v = {};
+	cout << "-- f(string&, string&, vector<int>&, vector<int>&, vector<int>&) --\n";
+
+	cout << "s1.capacity() = " << s1.capacity() << '\n';
+	s1.shrink_to_fit();
+	cout << "s1.capacity() = " << s1.capacity() << '\n';
+
+	cout << "s2.capacity() = " << s2.capacity() << '\n';
+	swap3(s2, string{s2});
+	cout << "s2.capacity() = " << s2.capacity() << '\n';
+
+	cout << "v1.size() = " << v1.size() << '\n';
+	v1.clear();
+	cout << "v1.size() = " << v1.size() << '\n';
+
+	cout << "v2.size() = " << v2.size() << '\n';
+	swap3(v2, vector<int>{});
+	cout << "v2.size() = " << v2.size() << '\n';
+
+	cout << "v3.size() = " << v3.size() << '\n';
+	v3 = {};
+	cout << "v3.size() = " << v3.size() << '\n';
 }
 
 int main()
 {
+	cout << "-- lvalue reference and rvalue reference --\n";
+	cout << "::var = " << ::var << '\n';
+	cout << "::r1 = " << ::r1 << '\n';
+	cout << "::rr1 = " << ::rr1 << '\n';
+	cout << "::rr3 = " << ::rr3 << '\n';
+
 	string val = f("tempolary string");
 	cout << "val = " << val << '\n';
 
-	vector<int> v1(1000), v2(10000);
+	vector<int> vec1(1000), vec2(10000);
 
-	swap1(v1, v2);
-	cout << "v1.size() = " << v1.size() << ", v2.size() = " << v2.size() << '\n';
-	swap2(v1, v2);
-	cout << "v1.size() = " << v1.size() << ", v2.size() = " << v2.size() << '\n';
-	swap3(v1, v2);
-	cout << "v1.size() = " << v1.size() << ", v2.size() = " << v2.size() << '\n';
+	swap1(vec1, vec2);
+	cout << "vec1.size() = " << vec1.size() << ", vec2.size() = " << vec2.size() << '\n';
+	swap2(vec1, vec2);
+	cout << "vec1.size() = " << vec1.size() << ", vec2.size() = " << vec2.size() << '\n';
+	swap3(vec1, vec2);
+	cout << "vec1.size() = " << vec1.size() << ", vec2.size() = " << vec2.size() << '\n';
 
-	f(v1);
-	cout << "v1.size() = " << v1.size() << " after call f(v1)\n";
+	f(vec1);
+	cout << "vec1.size() = " << vec1.size() << " after call f(vec1)\n";
 
-	string s;
-	vector<int> v;
+	string s1, s2;
+	vector<int> v1, v2, v3;
 	for (int i = 0; i < 1000; ++i) {
-		s.push_back('a');
-		v.push_back(i);
+		s1.push_back('a');
+		s2.push_back('a');
+		v1.push_back(i);
+		v2.push_back(i);
+		v3.push_back(i);
 	}
-	cout << "s.capacity() = " << s.capacity() << '\n';
-	cout << "v.size() = " << v.size() << '\n';
-
-	f(s, v);
-	cout << "s.capacity() = " << s.capacity() << '\n';
-	cout << "v.size() = " << v.size() << '\n';
+	f(s1, s2, v1, v2, v3);
 }
 
