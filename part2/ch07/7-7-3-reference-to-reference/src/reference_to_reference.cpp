2084b0af6466cc20232edd5dad0c43afa1e38dac
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-14 20:42:56 +0900

    Linux環境に対応

--- part2/ch07/7-7-3-reference-to-reference/src/reference_to_reference.cpp
+++ part2/ch07/7-7-3-reference-to-reference/src/reference_to_reference.cpp
@@ -5,13 +5,25 @@
  *      Author: sasaki
  */
 
+#include <iostream>
+using namespace std;
+
 using rr_i = int&&;
 using lr_i = int&;
-using rr_rr_i = rr_i&&;
-using lr_rr_i = rr_i&;
-using rr_lr_i = lr_i&&;
-using lr_lr_i = lr_i&;
+using rr_rr_i = rr_i&&;	// int&&
+using lr_rr_i = rr_i&;	// int&
+using rr_lr_i = lr_i&&;	// int&
+using lr_lr_i = lr_i&;	// int&
 
 int i;
 //int && & r = i;
 	// cannot declare reference to 'int&&', which is not a typedef or a template type argument
+
+int main()
+{
+	cout << boolalpha;
+	cout << "is_same<rr_rr_i,rr_i> ? " << is_same<rr_rr_i,rr_i>::value << endl;
+	cout << "is_same<lr_rr_i,lr_i> ? " << is_same<lr_rr_i,lr_i>::value << endl;
+	cout << "is_same<rr_lr_i,lr_i> ? " << is_same<rr_lr_i,lr_i>::value << endl;
+	cout << "is_same<lr_lr_i,lr_i> ? " << is_same<lr_lr_i,lr_i>::value << endl;
+}
