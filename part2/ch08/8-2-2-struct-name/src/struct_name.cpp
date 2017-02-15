e2d33b89b2a21d6760174b95d72f2d1f02358253
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-15 21:00:11 +0900

    Linux環境に対応

--- part2/ch08/8-2-2-struct-name/src/struct_name.cpp
+++ part2/ch08/8-2-2-struct-name/src/struct_name.cpp
@@ -5,6 +5,9 @@
  *      Author: sasaki
  */
 
+#include <iostream>
+using namespace std;
+
 struct Link {
 	Link* previous;
 	Link* successor;
@@ -12,21 +15,30 @@ struct Link {
 
 struct No_good {
 	//No_good member;
-		// field 'member' has incomplete type 'No_good'
+		// error: field 'member' has incomplete type 'No_good'
 };
 
-struct List;
+struct List2;
 
 struct Link2 {
 	Link2* pre;
 	Link2* suc;
-	List* member_of;
+	List2* member_of;
 	int data;
 };
 
-struct List {
-	Link* head;
+
+struct List2 {
+	Link2* head;
 };
+
+void print_list2(List2 *list2)
+{
+	for (Link2 *lk2 = list2->head; lk2; lk2 = lk2->suc) {
+		cout << lk2->data << ' ';
+	}
+	cout << endl;
+}
 
 struct S;
 
@@ -37,20 +49,40 @@ S* h(S*);
 
 void k(S* p)
 {
+	cout << "-- k(S* " << p << ") --\n";
+
 	//S a;
-		// aggregate 'S a' has incomplete type and cannot be defined
+		// error: aggregate 'S a' has incomplete type and cannot be defined
 	//f();
-		// invalid use of incomplete type 'struct S'
+		// error: invalid use of incomplete type 'struct S'
 	//g(a);
-		// invalid use of incomplete type 'struct S'
+		// error: invalid use of incomplete type 'struct S'
 	//p->m = 7;
-		// invalid use of incomplete type 'struct S'
+		// error: invalid use of incomplete type 'struct S'
 	S* q = h(p);
 	//q->m = 7;
-		// invalid use of incomplete type 'struct S'
+		// error: invalid use of incomplete type 'struct S'
 }
 
 S* h(S* p) { return p; }
+struct S { };
 
 struct stat { };
-int stat(char* name, struct stat* buf);
+int stat(char* name, struct stat* buf) { return 3; }
+
+int main()
+{
+	Link *link1 = new Link{nullptr,nullptr};
+	link1->successor = new Link{link1,nullptr};
+
+	List2 list2;
+	Link2 *link2_1 = new Link2{nullptr, nullptr, &list2, 100};
+	list2.head = link2_1;
+	link2_1->suc = new Link2{link2_1, nullptr, &list2, 1000};
+	print_list2(&list2);
+
+	S s;
+	k(&s);
+
+	cout << "stat() = " << stat("", nullptr) << endl;
+}
