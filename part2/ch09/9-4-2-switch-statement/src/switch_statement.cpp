ffbd77339f4ccb07837a5ca982e965211fb0ab94
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-19 08:20:28 +0900

    Linux環境に対応

--- part2/ch09/9-4-2-switch-statement/src/switch_statement.cpp
+++ part2/ch09/9-4-2-switch-statement/src/switch_statement.cpp
@@ -12,12 +12,12 @@ void f(int i)
 {
 	switch (i){
 	//case 2.7:
-		// could not convert '2.7000000000000002e+0' from 'double' to 'int'
+		// error: could not convert '2.7000000000000002e+0' from 'double' to 'int'
 		//break;
 	case 2:
 		break;
 	//case 4-2:
-		// duplicate case value
+		// error: duplicate case value
 		//break;
 	}
 }
@@ -26,7 +26,7 @@ void f() { cout << "f() called\n"; }
 void g() { cout << "g() called\n"; }
 void h() { cout << "h() called\n"; }
 
-void f2(int val)
+void use_switch(int val)
 {
 	switch (val){
 	case 1:
@@ -41,7 +41,7 @@ void f2(int val)
 	}
 }
 
-void f3(int val)
+void use_if_else(int val)
 {
 	if (val == 1)
 		f();
@@ -51,7 +51,7 @@ void f3(int val)
 		h();
 }
 
-void f4(int val)
+void fall_through(int val)
 {
 	switch (val){
 	case 1:
@@ -69,10 +69,10 @@ enum class Vessel { cup, glass, goblet, chalice };
 void problematic(Vessel v)
 {
 	switch (v){
-		// enumeration value 'chalice' not handled in switch [-Wswitch]
-	case Vessel::cup:		break;
-	case Vessel::glass:		break;
-	case Vessel::goblet:	break;
+		// warning: enumeration value 'chalice' not handled in switch [-Wswitch]
+	case Vessel::cup:		cout << "Vessel::cup\n"; break;
+	case Vessel::glass:		cout << "Vessel::glass\n"; break;
+	case Vessel::goblet:	cout << "Vessel::goblet\n"; break;
 	}
 }
 
@@ -80,15 +80,15 @@ int main()
 {
 	f(2);
 
-	cout << "calling f2(1): "; f2(1);
-	cout << "calling f2(3): "; f2(3);
+	cout << "-- calling use_switch(1):\n"; use_switch(1);
+	cout << "-- calling use_switch(3):\n"; use_switch(3);
 
-	cout << "calling f3(1): "; f3(1);
-	cout << "calling f3(3): "; f3(3);
+	cout << "-- calling use_if_else(1):\n"; use_if_else(1);
+	cout << "-- calling use_if_else(2):\n"; use_if_else(2);
 
-	cout << "calling f4(1): "; f4(1);
-	cout << "calling f4(3): "; f4(3);
+	cout << "-- calling fall_through(1):\n"; fall_through(1);
+	cout << "-- calling fall_through(3):\n"; fall_through(3);
 
-	cout << "calling proglematic(Vessel::glass)\n"; problematic(Vessel::glass);
+	cout << "-- calling proglematic(Vessel::glass)\n"; problematic(Vessel::glass);
 }
 
