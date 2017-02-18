e21bd14af98135544fe15391a5c2b7f4c5027ae5
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-18 09:17:29 +0900

    Linux環境に対応

--- part2/ch08/8-2-7-bit-field/src/bit_field.cpp
+++ part2/ch08/8-2-7-bit-field/src/bit_field.cpp
@@ -26,11 +26,39 @@ void part_of_VM_system(PPN* p)
 	}
 }
 
+void init_PPN(PPN *pPPN, unsigned PFN, unsigned CCA, bool nonreachable, bool global)
+{
+	pPPN->PFN = PFN;
+	pPPN->CCA = CCA;
+	pPPN->nonreachable = nonreachable;
+	pPPN->dirty = false;
+	pPPN->valid = true;
+	pPPN->global = global;
+}
+
+void print_PPN(const PPN *pPPN)
+{
+	cout << "PPN: " << pPPN << endl;
+	cout << boolalpha;
+	cout << " PFN: " << pPPN->PFN << endl;
+	cout << " CCA: " << pPPN->CCA << endl;;
+	cout << " nonreachable: " << pPPN->nonreachable << endl;
+	cout << " dirty: " << pPPN->dirty << endl;
+	cout << " valid: " << pPPN->valid << endl;
+	cout << " global: " << pPPN->global << endl;
+	cout << endl;
+}
+
 int main()
 {
 	PPN ppn;
 
+	init_PPN(&ppn, 0x00123456, 0x7, false, false);
+	print_PPN(&ppn);
+	part_of_VM_system(&ppn);
+
 	ppn.dirty = 1;
+	print_PPN(&ppn);
 	part_of_VM_system(&ppn);
 }
 
