9ec06ce7b03a2f953347b4cd06d50b85c5fe8f20
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-19 19:21:33 +0900

    Linux環境に対応

--- part2/ch10/10-3-1-result-of-operator/src/result_of_operator.cpp
+++ part2/ch10/10-3-1-result-of-operator/src/result_of_operator.cpp
@@ -10,24 +10,28 @@ using namespace std;
 
 void f(int x, int y)
 {
+	cout << "-- f(int:" << x << ", int:" << y << ") --\n";
+
 	int j = x = y;
 	int* p = &++x;
 	//int* q = &(x++);
-		// lvalue required as unary '&' operand
+		// error: lvalue required as unary '&' operand
 	int* p2 = &(x>y?x:y);
 	//int& r = (x<y)?x:1;
-		// invalid initialization of non-const reference of type 'int&' from an rvalue of type 'int'
+		// error: invalid initialization of non-const reference of type 'int&' from an rvalue of type 'int'
 
-	cout << "j == " << j << '\n';
-	cout << "p == " << p << '\n';
-	cout << "p2 == " << p2 << '\n';
+	cout << "j = " << j << '\n';
+	cout << "p = " << p << '\n';
+	cout << "p2 = " << p2 << '\n';
 }
 
 void f()
 {
+	cout << "-- f() --\n";
+
 	int i = 1;
 	while (0 < i) ++i;
-	cout << "i has become negative!" << i << '\n';
+	cout << "i has become negative! " << i << '\n';
 }
 
 int main()
