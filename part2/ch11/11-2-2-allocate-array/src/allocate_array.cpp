fd230d907a2f37af6dd472a0b92b6a4219791a94
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-26 17:35:08 +0900

    Linux環境に対応

--- part2/ch11/11-2-2-allocate-array/src/allocate_array.cpp
+++ part2/ch11/11-2-2-allocate-array/src/allocate_array.cpp
@@ -12,6 +12,8 @@ using namespace std;
 
 char* save_string(const char* p)
 {
+	cout << "-- save_string(\"" << p << "\") --\n";
+
 	char* s = new char[strlen(p)+1];
 	strcpy(s, p);
 	return s;
@@ -19,12 +21,16 @@ char* save_string(const char* p)
 
 string save_string2(const char* p)
 {
+	cout << "-- save_string2(\"" << p << "\") --\n";
+
 	return string{p};
 }
 
 void f(int n)
 {
-	vector<int>*p = new vector<int>(n);
+	cout << "-- f(" << n << ") --\n";
+
+	vector<int> *p = new vector<int>(n);
 	int *q = new int[n];
 
 	for (int i = 0; i < n; ++i) {
@@ -41,6 +47,7 @@ struct X {
 };
 void f1()
 {
+	cout << "-- f1() --\n";
 	X *p = new X;
 
 	delete p;
@@ -48,6 +55,7 @@ void f1()
 
 void f2()
 {
+	cout << "-- f1() --\n";
 	X x;
 }
 
