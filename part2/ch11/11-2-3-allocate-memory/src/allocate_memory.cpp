c6522480ee987ed9b80b0e353a0c27d5ba03d449
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-26 19:44:53 +0900

    Linux環境に対応

--- part2/ch11/11-2-3-allocate-memory/src/allocate_memory.cpp
+++ part2/ch11/11-2-3-allocate-memory/src/allocate_memory.cpp
@@ -11,19 +11,22 @@ using namespace std;
 
 void f()
 {
+	cout << "-- f() --\n";
+
 	vector<char*> v;
 	try {
 		// this crash PC!!!
+		// on Ubuntu/g++, terminated at exit value: -1, not crash
 		for (;;) {
 			char* p = new char[10000];
 			v.push_back(p);
 			p[0] = 'x';
 		}
 	}
-	//catch (bad_alloc) {
-	catch (bad_alloc&) {
+	catch (bad_alloc) {
+	//catch (bad_alloc&) {
 		// Catching by reference is recommended 'bad_alloc'
-
+		// Ubuntu/g++ 5.40 does not warn
 		cerr << "Memory exhausted!\n";
 	}
 }
