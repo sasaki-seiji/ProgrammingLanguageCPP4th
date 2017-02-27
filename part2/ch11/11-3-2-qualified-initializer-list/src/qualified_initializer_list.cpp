51f2e1655fe1706d4dd8037b8f9d52ec7d3973ed
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-27 20:10:15 +0900

    Linux環境に対応

--- part2/ch11/11-3-2-qualified-initializer-list/src/qualified_initializer_list.cpp
+++ part2/ch11/11-3-2-qualified-initializer-list/src/qualified_initializer_list.cpp
@@ -13,12 +13,14 @@ struct S { int a, b; };
 
 void f()
 {
+	cout << "-- f() --\n";
+
 	S v {7,8};
 	v = S{7,8};
 	S* p = new S{7,8};
 
-	cout << "v.a: " << v.a << ", vb: " << v.b << '\n';
-	cout << "p->a: " << p->a << ", p->b: " << p->b << '\n';
+	cout << "v.a = " << v.a << ", v.b = " << v.b << '\n';
+	cout << "p->a = " << p->a << ", p->b = " << p->b << '\n';
 }
 
 template<typename T>
@@ -29,13 +31,17 @@ T square(T x)
 
 void f(int i)
 {
+	cout << "-- f(" << i << ") --\n";
+
 	double d = square(double{i});
 		// why? narrowing conversion of 'i' from 'int' to 'double' inside { } [-Wnarrowing]
+		// -> see 11.5.1
 
 	complex<double> z = square(complex<double>{i});
+		// -> see 11.5.1
 
-	cout << "d: " << d << '\n';
-	cout << "z: " << z << '\n';
+	cout << "d = " << d << '\n';
+	cout << "z = " << z << '\n';
 }
 int main()
 {
