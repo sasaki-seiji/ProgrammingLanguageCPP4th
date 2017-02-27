6cbbdd0083cfa853f8bea074c906680df17fc01f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-27 21:24:38 +0900

    Linux環境に対応

--- part2/ch11/11-3-3-nonqualified-initializer-list/src/nonqualified_initializer_list.cpp
+++ part2/ch11/11-3-3-nonqualified-initializer-list/src/nonqualified_initializer_list.cpp
@@ -7,6 +7,7 @@
 
 #include <limits>
 #include <vector>
+#include <typeinfo>
 #include <iostream>
 using namespace std;
 
@@ -19,25 +20,27 @@ private:
 
 int f(double d, Matrix& m)
 {
+	cout << "-- f(double:" << d << ",Matrix&) --\n";
+
 	int v {7};
 	int v2 = {7};
 	int v3 = m[{2,3}];
 
 	v = {8};
 	//v += {88};
-		// invalid operands of types 'int' and '<brace-enclosed initializer list>' to binary 'operator+'
+		// error: invalid operands of types 'int' and '<brace-enclosed initializer list>' to binary 'operator+'
 
 	//{v} = 9;
-		// expected ';' before '}' token
+		// error: expected ';' before '}' token
 
 	//v = 7+{10};
-		// expected primary-expression before '{' token
+		// error: expected primary-expression before '{' token
 
 	if (d != 10.0) f({10.0}, m);
 
-	cout << "v: " << v << '\n';
-	cout << "v2: " << v2 << '\n';
-	cout << "v3: " << v3 << '\n';
+	cout << "v = " << v << '\n';
+	cout << "v2 = " << v2 << '\n';
+	cout << "v3 = " << v3 << '\n';
 
 	return {11};
 }
@@ -54,29 +57,19 @@ int high_value(initializer_list<int> val)
 	return high;
 }
 
+void test_initializer_list()
+{
+	cout << "-- test_initializer_list() --\n";
+
+	int v1 = high_value({1,2,3,4,5,6,7});
+	int v2 = high_value({-1,2,v1,4,-9,20,v1});
+
+	cout << "v1 = " << v1 << '\n';
+	cout << "v2 = " << v2 << '\n';
+}
+
 template<typename T>
 void f(T);
-
-void f()
-{
-	//auto x0 = {};
-		// unable to deduce 'std::initializer_list<_Tp>' from '{1, 2.0e+0}'
-	auto x1 = {1};
-	auto x2 = {1, 2};
-	auto x3 = {1, 2, 3};
-	//auto x4 = {1, 2.0};
-		// unable to deduce 'std::initializer_list<_Tp>' from '<brace-enclosed initializer list>()'	nonqualified_initializer_list.cpp	/11-3-3-nonqualified-initializer-list/src	行 58	C/C++ 問題
-
-	cout << is_same<decltype(x1), initializer_list<int>>::value << '\n';
-	cout << is_same<decltype(x2), initializer_list<int>>::value << '\n';
-	cout << is_same<decltype(x3), initializer_list<int>>::value << '\n';
-
-	//f({});
-	//f({1});
-	//f({1, 2});
-	//f({1, 2, 3});
-		// no matching function for call to 'f(<brace-enclosed initializer list>)'
-}
 
 template<typename T>
 void f2(const vector<T>& v)
@@ -87,6 +80,36 @@ void f2(const vector<T>& v)
 	cout << "]\n";
 }
 
+void deduce_initializer_list()
+{
+	cout << "-- deduce_initializer_list() --\n";
+
+	//auto x0 = {};
+		// error: unable to deduce 'std::initializer_list<_Tp>' from '{1, 2.0e+0}'
+	auto x1 = {1};
+	auto x2 = {1, 2};
+	auto x3 = {1, 2, 3};
+	//auto x4 = {1, 2.0};
+		// error: unable to deduce 'std::initializer_list<_Tp>' from '<brace-enclosed initializer list>()'	nonqualified_initializer_list.cpp	/11-3-3-nonqualified-initializer-list/src	行 58	C/C++ 問題
+
+	cout << "typeid(x1) = " << typeid(x1).name() << endl;
+	cout << "typeid(x2) = " << typeid(x2).name() << endl;
+	cout << "typeid(x3) = " << typeid(x3).name() << endl;
+
+	//f({});
+	//f({1});
+	//f({1, 2});
+	//f({1, 2, 3});
+		// error: no matching function for call to 'f(<brace-enclosed initializer list>)'
+
+	//f2({1, 2, 3});
+	//f2({"Kona", "Sidney"});
+		// error: no matching function for call to 'f2(<brace-enclosed initializer list>)'
+
+	f2(vector<int>{1,2,3});
+	f2(vector<string>{"kona","Sidney"});
+}
+
 int main()
 {
 	Matrix m;
@@ -94,20 +117,8 @@ int main()
 
 	cout << "f(5.1, m) returns " << retval << '\n';
 
-	int v1 = high_value({1,2,3,4,5,6,7});
-	int v2 = high_value({-1,2,v1,4,-9,20,v1});
-
-	cout << "v1: " << v1 << '\n';
-	cout << "v2: " << v2 << '\n';
-
-	f();
-
-	//f2({1, 2, 3});
-	//f2({"Kona", "Sidney"});
-		// no matching function for call to 'f2(<brace-enclosed initializer list>)'
-
-	f2(vector<int>{1,2,3});
-	f2(vector<string>{"kona","Sidney"});
+	test_initializer_list();
+	deduce_initializer_list();
 }
 
 
