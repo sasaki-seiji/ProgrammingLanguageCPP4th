03b783fbc0019b09d57567f2e60f0a14618dab4b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-02-27 19:59:17 +0900

    Linux環境に対応

--- part2/ch11/11-3-1-impl-model-of-ilist/src/impl_model_of_ilist.cpp
+++ part2/ch11/11-3-1-impl-model-of-ilist/src/impl_model_of_ilist.cpp
@@ -11,11 +11,17 @@ using namespace std;
 
 void f()
 {
-	vector<double> v1 = {1, 2, 3.14};
+	cout << "-- f() --\n";
 
-	//const double temp[] = {double{1}, double{2}, 3.14};
+	vector<double> v1 = {1, 2, 3.14};
+	cout << "v1: ";
+	for (size_t i = 0 ; i!=v1.size(); ++i) cout << v1[i] << ' ';
+	cout << endl;
+
+	const double temp[] = {double{1}, double{2}, 3.14};
 	//const initializer_list<double> tmp(temp, sizeof(temp)/sizeof(double));
 		// private constructor!!!
+		// error: ‘constexpr std::initializer_list<_E>::initializer_list(std::initializer_list<_E>::const_iterator, std::initializer_list<_E>::size_type) [with _E = double; std::initializer_list<_E>::const_iterator = const double*; std::initializer_list<_E>::size_type = long unsigned int]’ is private
 	//vector<double> v2(tmp);
 
 	//cout << "v1 == v2 ? " << (v1 == v2) << '\n';
@@ -23,7 +29,7 @@ void f()
 	initializer_list<int> lst {1,2,3};
 	cout << *lst.begin() << '\n';
 	//*lst.begin() = 2;
-		// assignment of read-only location '* lst.std::initializer_list<_E>::begin<int>()'
+		// error: assignment of read-only location '* lst.std::initializer_list<_E>::begin<int>()'
 
 	cout << *lst.begin() << '\n';
 }
