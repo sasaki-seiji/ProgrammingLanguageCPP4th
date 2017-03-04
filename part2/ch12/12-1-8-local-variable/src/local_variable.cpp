51d442ad1b054b9a71ff54ed439e1ff91a320e71
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-05 07:47:47 +0900

    Linux環境に対応

--- part2/ch12/12-1-8-local-variable/src/local_variable.cpp
+++ part2/ch12/12-1-8-local-variable/src/local_variable.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 void f(int a)
 {
+	cout << "-- f(" << a << ") --\n";
+
 	while (a--) {
 		static int n = 0;
 		int x = 0;
@@ -21,11 +23,16 @@ void f(int a)
 
 int fn(int n)
 {
+	cout << "-- fn(" << n << ") --\n";
+
 	static int n1 = n;
 	//static int n2 = fn(n-1)+1;
 		// hang up
+		// in Ubuntu:
+		// terminate called after throwing an instance of '__gnu_cxx::recursive_init_error'
+		//    what():  std::exception
 
-	cout << "n1: " << n1 << '\n';
+	cout << "n1 = " << n1 << '\n';
 	//cout << "n2: " << n2 << '\n';
 	return n;
 }
