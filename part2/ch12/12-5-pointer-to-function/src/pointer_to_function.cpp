269db80144debf2cec338700454a6288e22c8e97
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-07 20:37:39 +0900

    Linux環境に対応

--- part2/ch12/12-5-pointer-to-function/src/pointer_to_function.cpp
+++ part2/ch12/12-5-pointer-to-function/src/pointer_to_function.cpp
@@ -15,6 +15,8 @@ void (*efct)(string);
 
 void f()
 {
+	cout << "-- f() --\n";
+
 	efct = &error;
 	efct("error");
 }
@@ -24,6 +26,8 @@ void (*f2)(string) = error;
 
 void g()
 {
+	cout << "-- g() --\n";
+
 	f1("Vasa");
 	(*f1)("Mary Rose");
 }
@@ -35,18 +39,20 @@ void h3(int*);
 
 void h()
 {
+	cout << "-- h() --\n";
+
 	pf = &h1;
 	//pf = &h2;
-		// invalid conversion from 'int (*)(std::__cxx11::string) {aka int (*)(std::__cxx11::basic_string<char>)}' to 'void (*)(std::__cxx11::string) {aka void (*)(std::__cxx11::basic_string<char>)}' [-fpermissive]
+		// error: invalid conversion from 'int (*)(std::__cxx11::string) {aka int (*)(std::__cxx11::basic_string<char>)}' to 'void (*)(std::__cxx11::string) {aka void (*)(std::__cxx11::basic_string<char>)}' [-fpermissive]
 	//pf = &h3;
-		// invalid conversion from 'void (*)(int*)' to 'void (*)(std::__cxx11::string) {aka void (*)(std::__cxx11::basic_string<char>)}' [-fpermissive]
+		// error: invalid conversion from 'void (*)(int*)' to 'void (*)(std::__cxx11::string) {aka void (*)(std::__cxx11::basic_string<char>)}' [-fpermissive]
 
 	pf("Hera");
 	//pf(1);
-		// could not convert '1' from 'int' to 'std::__cxx11::string {aka std::__cxx11::basic_string<char>}'
+		// error: could not convert '1' from 'int' to 'std::__cxx11::string {aka std::__cxx11::basic_string<char>}'
 
 	//int i = pf("Zeus");
-		// void value not ignored as it ought to be
+		// error: void value not ignored as it ought to be
 }
 
 using P1 = int(*)(int*);
@@ -54,8 +60,10 @@ using P2 = void(*)(void);
 
 void f(P1 pf)
 {
+	cout << "-- f(P1) --\n";
+
 	P2 pf2 = reinterpret_cast<P2>(pf);
-	pf2();
+	pf2();	// terminated: -1
 	P1 pf1 = reinterpret_cast<P1>(pf2);
 	int x = 7;
 	int y = pf1(&x);
