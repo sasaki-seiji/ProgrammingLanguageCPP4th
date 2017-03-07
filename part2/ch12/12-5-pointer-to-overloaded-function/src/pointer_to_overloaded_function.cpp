56f3657f823e419926b31e485585aa63dce1a9d0
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-07 21:53:49 +0900

    Linux環境に対応

--- part2/ch12/12-5-pointer-to-overloaded-function/src/pointer_to_overloaded_function.cpp
+++ part2/ch12/12-5-pointer-to-overloaded-function/src/pointer_to_overloaded_function.cpp
@@ -15,13 +15,28 @@ int f(char);
 void (*pf1)(int) = &f;
 int (*pf2)(char) = &f;
 //void (*pf3)(char) = &f;
-	// no matches converting function 'f' to type 'void (*)(char)'
+	// error: no matches converting function 'f' to type 'void (*)(char)'
+
+void ff(int) noexcept;
+void gg(int);
+
+void (*p1)(int) = ff;
+void (*p2)(int) noexcept = ff;
+void (*p3)(int) noexcept = gg;	// 2017.03.07 エラーとならない
+
+//using Pc = extern "C" void(int);
+	// error: expected type-specifier before ‘extern’
+using Pn = void(int) noexcept;	//　2017.03.07　エラーとならない
 
 int main()
 {
 	pf1(12);
 	int ret = pf2('x');
 	cout << "pf2('x') returns " << ret << '\n';
+
+	p1(123);
+	p2(456);
+	p3(789);
 }
 
 // undefs
@@ -34,3 +49,12 @@ int f(char c)
 	cout << "f(char: " << c << ")\n";
 	return int{c};
 }
+void ff(int i) noexcept
+{
+	cout << "ff(int: " << i << ")\n";
+}
+void gg(int i)
+{
+	cout << "gg(int: " << i << ")\n";
+}
+
