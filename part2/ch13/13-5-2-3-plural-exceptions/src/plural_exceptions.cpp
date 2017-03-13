3038b5fb4b5981a30ed52aacd2b08908a059953d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-13 20:13:31 +0900

    Linux環境に対応

--- part2/ch13/13-5-2-3-plural-exceptions/src/plural_exceptions.cpp
+++ part2/ch13/13-5-2-3-plural-exceptions/src/plural_exceptions.cpp
@@ -17,15 +17,15 @@ void f()
 		// ...
 	}
 	catch (std::ios_base::failure& e) {
-		cerr << e.what() << '\n';
+		cout << "caught in f() as ios_base::failure&: " << e.what() << '\n';
 		// ...
 	}
 	catch (std::exception& e) {
-		cerr << e.what() << '\n';
+		cout << "caught in f() as exception&: " << e.what() << '\n';
 		// ...
 	}
 	catch (...) {
-		cerr << "some excetption\n";
+		cout << "caught in f() as ...\n";
 		// ...
 	}
 }
@@ -39,13 +39,14 @@ void g()
 	//catch (...) {
 	//	// ...
 	//}
-		// '...' handler must be the last handler for its try block [-fpermissive]
+		// error: '...' handler must be the last handler for its try block [-fpermissive]
 	catch (std::exception& e) {
-		cerr << "caught exception\n";
+		cout << "caught in g() as exception&: " << e.what() << endl ;
 		// ...
 	}
-	catch (std::bad_cast) {
-		cerr << "caught bad_cast\n";
+	catch (std::bad_cast& e) {
+		// warning: exception of type ‘std::bad_cast’ will be caught
+		cout << "caught in g() as bad_cast" << e.what() << endl;
 		// ...
 	}
 }
