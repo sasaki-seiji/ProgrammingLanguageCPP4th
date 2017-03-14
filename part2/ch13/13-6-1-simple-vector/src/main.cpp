e2b9431b44687845ec3b1814205e4f7e8a6a0222
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-14 21:35:00 +0900

    要素のコピーコンストラクタ失敗時の振る舞いを確認

--- part2/ch13/13-6-1-simple-vector/src/main.cpp
+++ part2/ch13/13-6-1-simple-vector/src/main.cpp
@@ -9,21 +9,28 @@
 #include <string>
 using namespace std;
 #include "vector.h"
+#include "Test_element.h"
+
+void test_construct()
+{
+	cout << "-- test_construct() --\n";
+
+	Vector<Test_element> v1(5, 1);
+	cout << "Vector<Test_element> v1(5, 1) : success\n";
+
+	Test_element::trigger_copy_construct_exception(3);
+	try {
+		Vector<Test_element> v2(5,2);
+		cout << "Vector<Test_element> v2(5,2) : success\n";
+	}
+	catch (const exception& e) {
+		cout << e.what() << endl;
+	}
+}
 
 int main()
 {
-	const int	sz = 100*1000*1000;
-	string str;
-	for (int i; i < 100; ++i) str += 'z';
-
-	try {
-		vector<int> vi(sz);
-		cout << "success alloc vector<int>(" << sz << ")\n" << flush;
-
-		vector<string> vs(sz, str);
-		cout << "success alloc vector<string>(" << sz << ")\n" << flush;
-	}
-	catch (...) {
-		cerr << "fail alloc\n" << flush;
-	}
+	Test_element::verbose();
+	test_construct();
+	Test_element::display_counters();
 }
