73fcab0763165f5c626ac67d1720f1fdffed219c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-16 20:22:11 +0900

    Linux環境に対応

--- part2/ch13/13-6-2-separate-memory-handle/src/main.cpp
+++ part2/ch13/13-6-2-separate-memory-handle/src/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #include "vector.h"
 
 template<typename T>
-ostream& operator<<(ostream& os, const vector<T>& v)
+ostream& operator<<(ostream& os, const Vector<T>& v)
 {
 	os << '[';
 	for (size_t i = 0 ; i != v.size(); ++i)
@@ -21,28 +21,33 @@ ostream& operator<<(ostream& os, const vector<T>& v)
 }
 
 template<typename T>
-vector<T> make_vector(size_t n, const T& val = T{})
+Vector<T> make_vector(size_t n, const T& val = T{})
 {
-	vector<T> v(n, val);
+	Vector<T> v(n, val);
 	return v;
 }
 
 int main()
 {
-	vector<int> vi(10, 123);
-	vector<string> vs(10, "abc");
+	Vector<int> vi(10, 123);
+	Vector<string> vs(10, "abc");
 	cout << "vi: " << vi << '\n';
 	cout << "vs: " << vs << '\n';
 
 	// copy
-	vector<int> vi2{vi};
-	vector<string> vs2{vs};
+	Vector<int> vi2{vi};
+	Vector<string> vs2{vs};
 	cout << "vi2(copied): " << vi2 << '\n';
 	cout << "vs2(copied): " << vs2 << '\n';
 
 	// move
-	vector<int> vi3 = make_vector(10, 456);
-	vector<string> vs3 = make_vector<string>(10, "xyz");
+	Vector<int> vi3 = make_vector(10, 456);
+	Vector<string> vs3 = make_vector<string>(10, "xyz");
 	cout << "vi3(moved): " << vi3 << '\n';
 	cout << "vs3(moved): " << vs3 << '\n';
+
+	// move construct and move assign
+	swap(vi2, vi3);
+	cout << "vi2(swapped): " << vi2 << endl;
+	cout << "vi3(swapped): " << vi3 << endl;
 }
