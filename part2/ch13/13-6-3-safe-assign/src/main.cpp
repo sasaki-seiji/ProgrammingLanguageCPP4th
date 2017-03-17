ea445b698d868bf3c9f73756968e753c7f8c3673
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-18 08:16:10 +0900

    要素のコピー失敗時の振る舞いを確認

--- part2/ch13/13-6-3-safe-assign/src/main.cpp
+++ part2/ch13/13-6-3-safe-assign/src/main.cpp
@@ -8,7 +8,9 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 #include "vector.h"
+#include "Test_element.h"
 
 template<typename T>
 ostream& operator<<(ostream& os, const Vector<T>& v)
@@ -27,8 +29,10 @@ Vector<T> make_vector(size_t n, const T& val = T{})
 	return v;
 }
 
-int main()
+void test_safe_assign()
 {
+	cout << "-- test_safe_assign() --\n";
+
 	Vector<int> vi(10, 123);
 	Vector<string> vs(10, "abc");
 	cout << "vi: " << vi << '\n'<< flush;
@@ -44,13 +48,54 @@ int main()
 	safe_assign(vs2, vs);
 	cout << "vi2(safe assigned from vi): " << vi2 << '\n'<< flush;
 	cout << "vs2(safe assigned from vs): " << vs2 << '\n'<< flush;
+}
+
+void test_safe_assign_by_value()
+{
+	cout << "-- test_safe_assign_by_value() --\n";
+
+	Vector<int> vi(10, 123);
+	Vector<string> vs(10, "abc");
+	cout << "vi: " << vi << '\n'<< flush;
+	cout << "vs: " << vs << '\n'<< flush;
+
+	Vector<int> vi2(15, 456);
+	Vector<string> vs2(15, "xyz");
+	cout << "vi2: " << vi2 << '\n'<< flush;
+	cout << "vs2: " << vs2 << '\n'<< flush;
 
 	// safe assign by value
-	Vector<int> vi3(5, 789);
-	Vector<string> vs3(5, "uvw");
-	safe_assign_by_value(vi, vi3);
-	safe_assign_by_value(vs, vs3);
-	cout << "vi(safe assigned by value from vi3): " << vi << '\n'<< flush;
-	cout << "vs(safe assigned by value from vs3): " << vs << '\n'<< flush;
+	safe_assign_by_value(vi2, vi);
+	safe_assign_by_value(vs2, vs);
+	cout << "vi2(safe assigned by value from vi): " << vi2 << '\n'<< flush;
+	cout << "vs2(safe assigned by value from vs): " << vs2 << '\n'<< flush;
+}
 
+void test_element_assign_fail()
+{
+	cout << "-- test_element_assign_fail() --\n";
+
+	Vector<Test_element<string>> v1(5, string("abc")), v2(5,string("xyz"));
+	cout << "Vector<Test_element<string>> v1(5, \"abc\"), v2(5, \"xyz\") : success\n";
+
+	Test_element<string>::trigger_copy_construct_exception(3);
+	try {
+		safe_assign(v2,v1);
+		cout << "safe_assign(v2,v1) : success\n";
+	}
+	catch (const exception& e) {
+		cout << e.what() << endl;
+	}
+
+	cout << "v2(after safe_assign): " << v2 << endl;
+}
+
+int main()
+{
+	test_safe_assign();
+	test_safe_assign_by_value();
+
+	Test_element<string>::verbose();
+	test_element_assign_fail();
+	Test_element<string>::display_counters();
 }
