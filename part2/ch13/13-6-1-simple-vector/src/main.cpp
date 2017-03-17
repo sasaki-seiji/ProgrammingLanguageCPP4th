262e52399acde2dae58fa5d8142973fa74103831
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-18 07:49:45 +0900

    Test_elementをテンプレート化

--- part2/ch13/13-6-1-simple-vector/src/main.cpp
+++ part2/ch13/13-6-1-simple-vector/src/main.cpp
@@ -11,17 +11,17 @@ using namespace std;
 #include "vector.h"
 #include "Test_element.h"
 
-void test_construct()
+void test_element_copy_fail()
 {
-	cout << "-- test_construct() --\n";
+	cout << "-- test_element_copy_fail() --\n";
 
-	Vector<Test_element> v1(5, 1);
-	cout << "Vector<Test_element> v1(5, 1) : success\n";
+	Vector<Test_element<string>> v1(5, string("abc"));
+	cout << "Vector<Test_element<string>> v1(5, string(\"abc\")) : success\n";
 
-	Test_element::trigger_copy_construct_exception(3);
+	Test_element<string>::trigger_copy_construct_exception(3);
 	try {
-		Vector<Test_element> v2(5,2);
-		cout << "Vector<Test_element> v2(5,2) : success\n";
+		Vector<Test_element<string>> v2(5,string("xyz"));
+		cout << "Vector<Test_element<string>> v2(5,string(\"xyz\")) : success\n";
 	}
 	catch (const exception& e) {
 		cout << e.what() << endl;
@@ -30,7 +30,7 @@ void test_construct()
 
 int main()
 {
-	Test_element::verbose();
-	test_construct();
-	Test_element::display_counters();
+	Test_element<string>::verbose();
+	test_element_copy_fail();
+	Test_element<string>::display_counters();
 }
