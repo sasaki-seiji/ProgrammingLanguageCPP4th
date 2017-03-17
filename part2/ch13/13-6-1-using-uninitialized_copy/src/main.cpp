06983a2d5bf5e059238e7a95f944a5beeccf14c6
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-18 07:34:22 +0900

    Test_elementをテンプレート化

--- part2/ch13/13-6-1-using-uninitialized_copy/src/main.cpp
+++ part2/ch13/13-6-1-using-uninitialized_copy/src/main.cpp
@@ -11,17 +11,17 @@ using namespace std;
 #include "vector.h"
 #include "Test_element.h"
 
-void test_construct()
+void test_element_copy_fail()
 {
-	cout << "-- test_construct() --\n";
+	cout << "-- test_element_copy_fail() --\n";
 
-	Vector<Test_element> v1(5, 1);
-	cout << "Vector<Test_element> v1(5, 1) : success\n";
+	Vector<Test_element<int>> v1(5, 1);
+	cout << "Vector<Test_element<int>> v1(5, 1) : success\n";
 
-	Test_element::trigger_copy_construct_exception(3);
+	Test_element<int>::trigger_copy_construct_exception(3);
 	try {
-		Vector<Test_element> v2(5,2);
-		cout << "Vector<Test_element> v2(5,2) : success\n";
+		Vector<Test_element<int>> v2(5,2);
+		cout << "Vector<Test_element<int>> v2(5,2) : success\n";
 	}
 	catch (const exception& e) {
 		cout << e.what() << endl;
@@ -30,7 +30,7 @@ void test_construct()
 
 int main()
 {
-	Test_element::verbose();
-	test_construct();
-	Test_element::display_counters();
+	Test_element<int>::verbose();
+	test_element_copy_fail();
+	Test_element<int>::display_counters();
 }
