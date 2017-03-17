55bd5f58651c556b4abf73ce0ed311bb181b4d02
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-18 08:22:06 +0900

    要素のコピー失敗時の振る舞いを確認

--- part2/ch13/13-6-4-1-reserve/src/main.cpp
+++ part2/ch13/13-6-4-1-reserve/src/main.cpp
@@ -8,7 +8,9 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 #include "vector.h"
+#include "Test_element.h"
 
 template<typename T>
 ostream& operator<<(ostream& os, const Vector<T>& v)
@@ -20,15 +22,10 @@ ostream& operator<<(ostream& os, const Vector<T>& v)
 	return os;
 }
 
-template<typename T>
-Vector<T> make_vector(size_t n, const T& val = T{})
+void test_reserve()
 {
-	Vector<T> v(n, val);
-	return v;
-}
+	cout << "-- test_reserve() --\n";
 
-int main()
-{
 	Vector<int> vi(10, 123);
 	Vector<string> vs(10, "abc");
 	cout << "vi(original): " << vi << ", capacity = " << vi.capacity() << '\n'<< flush;
@@ -38,4 +35,33 @@ int main()
 	vs.reserve(100);
 	cout << "vi(reserved): " << vi << ", capacity = " << vi.capacity() << '\n'<< flush;
 	cout << "vs(reserved): " << vs << ", capacity = " << vs.capacity() << '\n'<< flush;
+
+}
+
+void test_element_copy_fail()
+{
+	cout << "-- test_element_copy_fail() --\n";
+
+	Vector<Test_element<string>> v1(5, string("abc"));
+	cout << "Vector<Test_element<string>> v1(5, \"abc\") : success\n";
+
+	Test_element<string>::trigger_copy_construct_exception(3);
+	try {
+		v1.reserve(10);
+		cout << "v1.reserve(10) : success\n";
+	}
+	catch (const exception& e) {
+		cout << e.what() << endl;
+	}
+
+	cout << "v1(after reserve(10)): " << v1 << endl;
+}
+
+int main()
+{
+	test_reserve();
+
+	Test_element<string>::verbose();
+	test_element_copy_fail();
+	Test_element<string>::display_counters();
 }
