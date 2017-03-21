c1d944251749d848c0ae8df97de40fcc193f8615
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-21 21:13:41 +0900

    14-4-9-hello-worldの追加

---
+++ part2/ch14/14-4-9-hello-world/src/hello_world.cpp
@@ -0,0 +1,19 @@
+/*
+ * hello_world.cpp
+ *
+ *  Created on: 2017/03/21
+ *      Author: sasaki
+ */
+
+
+#include <vector>
+//vector<int> v1;
+	// error: ‘vector’ does not name a type
+#include "my_stdio.h"
+vector<int> v2;
+
+int main()
+{
+	printf("Hello, world!\n");
+}
+
---
+++ part2/ch14/14-4-9-hello-world/src/my_stdio.h
@@ -0,0 +1,15 @@
+/*
+ * my_stdio.h
+ *
+ *  Created on: 2017/03/21
+ *      Author: sasaki
+ */
+
+#ifndef MY_STDIO_H_
+#define MY_STDIO_H_
+
+#include "my_cstdio"
+using namespace std;
+
+
+#endif /* MY_STDIO_H_ */
