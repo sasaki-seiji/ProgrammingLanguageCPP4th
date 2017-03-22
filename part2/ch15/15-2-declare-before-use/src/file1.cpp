5d320eac1e7a94167eb69991adbbfb8ffbdc2ec6
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-22 21:22:26 +0900

    Linux環境に対応

--- part2/ch15/15-2-declare-before-use/src/file1.cpp
+++ part2/ch15/15-2-declare-before-use/src/file1.cpp
@@ -6,8 +6,10 @@
  */
 
 
-//int g() { return f()+7; } // undefined reference to `c'
-//int f() { return x; }	// 'x' was not declared in this scope
+//int g() { return f()+7; }
+	// error: ‘f’ was not declared in this scope
+//int f() { return x; }
+	// error: 'x' was not declared in this scope
 int x;
 
 int f() { return x; }	// 'x' was not declared in this scope
