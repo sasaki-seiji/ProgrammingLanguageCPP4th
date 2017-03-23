e24d892dfc90afd5a0720351056b1ae78f7eebc6
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-23 21:28:07 +0900

    Linux環境に対応

--- part2/ch15/15-2-3-one-definition-rule/src/file1.cpp
+++ part2/ch15/15-2-3-one-definition-rule/src/file1.cpp
@@ -5,21 +5,46 @@
  *      Author: sasaki
  */
 
+#include <iostream>
+using namespace std;
+
 
 struct S { int a; char b; };
 void f(S*);
 
 #include "s.h"
-void using_f0(S0 *p) { f0(p); }
 
 struct S1 { int a; char b; };
 //struct S1 { int a; char b; };
 	// error: previous definition of 'struct S1'
 
 struct S2 { int a; char b; };
+void f2(S2*);
 
 typedef int X;
 struct S3 { X a; char b; };
+void f3(S3*);
 
 #define Point int
 #include "sp.h"
+void fp(SP*);
+
+void print_file1()
+{
+	cout << "-- print_file2() --\n";
+
+	S s = { 100, 'a' };
+	f(&s);
+
+	S0 s0 = { 200, 'b' };
+	f0(&s0);
+
+	S2 s2 = { 200, 'c' };
+	f2(&s2);
+
+	S3 s3 = { 80, 'd' };
+	f3(&s3);
+
+	SP sp = { 100, 'x' };
+	fp(&sp);
+}
--- part2/ch15/15-2-3-one-definition-rule/src/file2.cpp
+++ part2/ch15/15-2-3-one-definition-rule/src/file2.cpp
@@ -5,39 +5,58 @@
  *      Author: sasaki
  */
 
-
-struct S { int a; char b; };
-
 #include <iostream>
 using namespace std;
+
+struct S { int a; char b; };
 void f(S* p)
-{ cout << "f({" << p->a << ", '" << p->b << "'}) called\n"; }
+{ cout << "file2.f({" << p->a << ", '" << p->b << "'})\n"; }
 
 #include "s.h"
 void f0(S0* p)
-{ cout << "f0({" << p->a << ", '" << p->b << "'}) called\n"; }
+{ cout << "file2.f0({" << p->a << ", '" << p->b << "'})\n"; }
 
 struct S2 { int a; char bb; };
 	// error, but compiler does not detect
+void f2(S2* p)
+{ cout << "file2.f2({" << p->a << ", '" << p->bb << "'})\n"; }
 
 typedef char X;
 struct S3 { X a; char b; };
 	// error, but compiler does not detect
+void f3(S3* p)
+{ cout << "file2.f3({'" << p->a << "', '" << p->b << "'})\n"; }
 
-class Point { int x; int y; };
+struct Point { int x; int y; };
 #include "sp.h"
 	// error, but compiler does not detect
+void fp(SP* p)
+{ cout << "file2.fp({(" << p->a.x << ", " << p->a.y << "), '" << p->b << "'})\n"; }
 
-
-// add main
-int main()
+void print_file1();
+void print_file2()
 {
+	cout << "-- print_file1() --\n";
+
 	S s = { 100, 'a' };
 	f(&s);
 
 	S0 s0 = { 200, 'b' };
 	f0(&s0);
 
-	void using_f0(S0 *p);
-	using_f0(&s0);
+	S2 s2 = { 200, 'c' };
+	f2(&s2);
+
+	S3 s3 = { 80, 'd' };
+	f3(&s3);
+
+	SP sp = { Point{1,2}, 'x' };
+	fp(&sp);
+}
+
+// add main
+int main()
+{
+	print_file1();
+	print_file2();
 }
