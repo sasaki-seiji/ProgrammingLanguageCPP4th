5fed2a08877dff3cf411e060a64116f59dd70146
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-23 20:25:19 +0900

    Linux環境に対応

--- part2/ch15/15-2-internal-by-default/src/file1.cpp
+++ part2/ch15/15-2-internal-by-default/src/file1.cpp
@@ -10,3 +10,16 @@ const int x = 7;
 constexpr T c2 = x+1;
 
 extern const int a = 77;
+
+// 2017.03.23 add
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+void print_file1()
+{
+	cout << "-- print_file1() --\n";
+
+	cout << "typeid(T) = " << typeid(T).name() << endl;
+	cout << "x = " << x << endl;
+	cout << "c2 = " << c2 << endl;
+}
--- part2/ch15/15-2-internal-by-default/src/file2.cpp
+++ part2/ch15/15-2-internal-by-default/src/file2.cpp
@@ -13,16 +13,30 @@ constexpr T c2 = x+9;
 extern const int a;
 
 #include <iostream>
+#include <typeinfo>
 using namespace std;
 void g()
 {
+	cout << "-- g() --\n";
+
 	cout << a << '\n';
 }
 
 // add main
+void print_file1();
+void print_file2()
+{
+	cout << "-- print_file2() --\n";
+
+	cout << "typeid(T) = " << typeid(T).name() << endl;
+	cout << "x = " << x << '\n';
+	cout << "c2 = " << c2 << '\n';
+}
+
 int main()
 {
-	cout << "x : " << x << '\n';
-	cout << "c2 : " << c2 << '\n';
+	print_file1();
+	print_file2();
+
 	g();
 }
