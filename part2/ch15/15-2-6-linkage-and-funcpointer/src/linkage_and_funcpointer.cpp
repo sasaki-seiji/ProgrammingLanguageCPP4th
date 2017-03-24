bc9f4b275a3c0a36796f4b0a172863eac63140ae
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-24 20:13:09 +0900

    Linux環境に対応

--- part2/ch15/15-2-6-linkage-and-funcpointer/src/linkage_and_funcpointer.cpp
+++ part2/ch15/15-2-6-linkage-and-funcpointer/src/linkage_and_funcpointer.cpp
@@ -11,7 +11,7 @@ typedef int (*FT)(const void*, const void*);
 
 extern "C" {
 	typedef int (*CFT)(const void*, const void*);
-	void qsort(void* p, size_t n, size_t sz, FT cmp);
+	void qsort(void* p, size_t n, size_t sz, CFT cmp);
 }
 
 void isort(void*p, size_t n, size_t sz, FT cmp);
@@ -35,34 +35,34 @@ void f(char* v, int sz)
 using namespace std;
 
 extern "C" {
-	void qsort(void* p, size_t n, size_t sz, FT cmp)
+	void qsort(void* p, size_t n, size_t sz, CFT cmp)
 	{
-		cout << "extern \"C\" qsort()\n";
+		cout << "extern \"C\" qsort(void*,size_t,size_t,CFT)\n";
 		cmp(p, p);
 	}
 	void ysort(void* p, size_t n, size_t sz, FT cmp)
 	{
-		cout << "extern \"C\" ysort()\n";
+		cout << "extern \"C\" ysort(void*,size_t,size_t,FT)\n";
 		cmp(p, p);
 	}
 	int ccmp(const void*, const void*){
-		cout << "extern \"C\" ccmp()\n";
+		cout << "  extern \"C\" ccmp(const void*,const void*)\n";
 	}
 }
 
 void isort(void*p, size_t n, size_t sz, FT cmp)
 {
-	cout << "isort()\n";
+	cout << "isort(void*,size_t,size_t,FT)\n";
 	cmp(p, p);
 }
 void xsort(void* p, size_t n, size_t sz, CFT cmp)
 {
-	cout << "xsort()\n";
+	cout << "xsort(void*,size_t,size_t,CFT)\n";
 	cmp(p, p);
 }
 int compare(const void*, const void*)
 {
-	cout << "compare()\n";
+	cout << "  compare(const void*,const void)\n";
 }
 
 // add main func
