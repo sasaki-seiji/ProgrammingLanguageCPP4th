f0c8cca116ca4ad2caf3960dd80f543f44f8e49a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-03-25 19:03:19 +0900

    Linux環境に対応

--- part3/ch16/16-2-5-constructor/src/user.cpp
+++ part3/ch16/16-2-5-constructor/src/user.cpp
@@ -11,35 +11,35 @@
 #include <iostream>
 using namespace std;
 
-void f()
+void multi_params_ctr_funcall()
 {
-	cout << "( ) form\n";
+	cout << "-- Date1( ) form --\n";
 
 	Date1 today = Date1(23, 6, 1983);
 	Date1 xmas(25, 12, 1990);
 	//Date1 my_birthday;
-		// no matching function for call to 'Date1::Date1()'
+		// error: no matching function for call to 'Date1::Date1()'
 	//Date1 release1_0(10, 12);
-		// no matching function for call to 'Date1::Date1(int, int)'
+		// error: no matching function for call to 'Date1::Date1(int, int)'
 	today.print();
 	xmas.print();
 }
 
-void g()
+void multi_params_ctr_init_list()
 {
-	cout << "{ } form\n";
+	cout << "-- Date1{ } form --\n";
 
 	Date1 today = Date1 {23,6,1983};
 	Date1 xmas {25,12,1990};
 	//Date1 release1_0 {10,12};
-		// no matching function for call to 'Date1::Date1(<brace-enclosed initializer list>)'
+		// error: no matching function for call to 'Date1::Date1(<brace-enclosed initializer list>)'
 	today.print();
 	xmas.print();
 }
 
-void h()
+void overloaded_ctr()
 {
-	cout << "constructor overload\n";
+	cout << "-- constructor overload --\n";
 
 	Date2 today {4};
 	Date2 july4 { "July 4, 1983" };
@@ -54,9 +54,9 @@ void h()
 	start.print();
 }
 
-void i()
+void default_arguments_ctr()
 {
-	cout << "constructor with default arguments\n";
+	cout << "-- constructor with default arguments --\n";
 
 	Date3 today {4};
 	Date3 guy {5,11};
@@ -72,8 +72,8 @@ void i()
 // add main
 int main()
 {
-	f();
-	g();
-	h();
-	i();
+	multi_params_ctr_funcall();
+	multi_params_ctr_init_list();
+	overloaded_ctr();
+	default_arguments_ctr();
 }
