f39e96291b1ae9a4a9fcf185c41ad304230f82ba
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-04-01 07:40:21 +0900

    Linux環境の追加

--- part3/ch17/17-3-4-3-direct-initialization-and-copy-initialization/src/direct_initialization_and_copy_initialization.cpp
+++ part3/ch17/17-3-4-3-direct-initialization-and-copy-initialization/src/direct_initialization_and_copy_initialization.cpp
@@ -9,52 +9,6 @@
 #include <iostream>
 using namespace std;
 
-vector<vector<double>> vs = {
-	{10,11,12,13},
-	{10},
-	//10,
-		// error: could not convert '{{10, 11, 12, 13}, {10}, 10, std::vector<double>(std::initializer_list<double>{((const double*)(& ._41)), 4u}, std::allocator<double>()), std::vector<double>(std::initializer_list<double>{((const double*)(& ._42)), 1u}, std::allocator<double>()), std::vector<double>(10ull, std::allocator<double>())}' from '<brace-enclosed initializer list>' to 'std::vector<std::vector<double> >'
-
-	vector<double>{10,11,12,13},
-	vector<double>{10},
-	vector<double>(10)
-};
-
-vector<double> v1(7);
-//vector<double> v2 = 9;
-	// error: conversion from 'int' to non-scalar type 'std::vector<double>' requested
-
-void f(const vector<double>&);
-
-void g()
-{
-	//v1 = 9;
-		// error: no match for 'operator=' (operand types are 'std::vector<double>' and 'int')
-	//f(9);
-		// error: invalid initialization of reference of type 'const std::vector<double>&' from expression of type 'int'
-}
-
-vector<double> v12 {7};
-vector<double> v22 = {9};
-
-void g2()
-{
-	v12 = {9};
-	f({9});
-}
-
-
-vector<string> v13 {"Anya"};
-vector<string> v23 = {"Courtney"};
-
-void f(const vector<string>&);
-
-void g3()
-{
-	v13 = {"Gavin"};
-	f({"Norah"});
-}
-
 // add utilities
 
 void print(const vector<vector<double>>& vv)
@@ -74,8 +28,23 @@ void print(const vector<T>& v)
 	cout << '\n';
 }
 
-// add undef func
 
+vector<vector<double>> vs = {
+	{10,11,12,13},
+	{10},
+	//10,
+		// error: could not convert '{{10, 11, 12, 13}, {10}, 10, std::vector<double>(std::initializer_list<double>{((const double*)(& ._41)), 4u}, std::allocator<double>()), std::vector<double>(std::initializer_list<double>{((const double*)(& ._42)), 1u}, std::allocator<double>()), std::vector<double>(10ull, std::allocator<double>())}' from '<brace-enclosed initializer list>' to 'std::vector<std::vector<double> >'
+
+	vector<double>{10,11,12,13},
+	vector<double>{10},
+	vector<double>(10)
+};
+
+vector<double> v1(7);
+//vector<double> v2 = 9;
+	// error: conversion from 'int' to non-scalar type 'std::vector<double>' requested
+
+void f(const vector<double>&);
 void f(const vector<double>& v)
 {
 	cout << "f(const vector<double>&:\n";
@@ -84,6 +53,29 @@ void f(const vector<double>& v)
 }
 
 
+void g()
+{
+	//v1 = 9;
+		// error: no match for 'operator=' (operand types are 'std::vector<double>' and 'int')
+	//f(9);
+		// error: invalid initialization of reference of type 'const std::vector<double>&' from expression of type 'int'
+}
+
+vector<double> v12 {7};
+vector<double> v22 = {9};
+
+void g2()
+{
+	cout << "-- g2() --\n";
+	v12 = {9};
+	f({9});
+}
+
+
+vector<string> v13 {"Anya"};
+vector<string> v23 = {"Courtney"};
+
+void f(const vector<string>&);
 void f(const vector<string>& v)
 {
 	cout << "f(const vector<string>&:\n";
@@ -92,37 +84,43 @@ void f(const vector<string>& v)
 }
 
 
+void g3()
+{
+	cout << "-- g3() --\n";
+
+	v13 = {"Gavin"};
+	f({"Norah"});
+}
+
+
+
 // add main
 
 int main()
 {
-	cout << "- print(vs)\n";
+	cout << "- print(vs) -\n";
 	print(vs);
 
-	cout << "- print(v1)\n";
+	cout << "- print(v1) -\n";
 	print(v1);
 
-	cout << "- print(v12)\n";
+	cout << "- print(v12) -\n";
 	print(v12);
 
-	cout << "-print(v22)\n";
+	cout << "-print(v22) -\n";
 	print(v22);
 
-	cout << "- calling g2()\n";
 	g2();
-
-	cout << "- after g2() called, print(v12)\n";
+	cout << "- after g2() called, print(v12) -\n";
 	print(v12);
 
-	cout << "- print(v13)\n";
+	cout << "- print(v13) -\n";
 	print(v13);
 
 	cout << "- print(v23)\n";
 	print(v23);
 
-	cout << "- calling g3()\n";
 	g3();
-
 	cout << "- after g3(), print(v13)\n";
 	print(v13);
 }
