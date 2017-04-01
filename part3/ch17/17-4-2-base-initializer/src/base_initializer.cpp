8dcead08350b3c55ab4707a7a04aecf22c2581b6
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-04-01 10:15:43 +0900

    Linux環境に対応

--- part3/ch17/17-4-2-base-initializer/src/base_initializer.cpp
+++ part3/ch17/17-4-2-base-initializer/src/base_initializer.cpp
@@ -8,33 +8,21 @@
 #include <iostream>
 using namespace std;
 
-struct B1 { B1(); };
-struct B2 { B2(int); };
+struct B1 { B1() { cout << "B1()\n"; } };
+struct B2 { B2(int i) { cout << "B2(" << i << ")\n"; } };
 
 struct D1 : B1, B2 {
-	D1(int i) : B1{}, B2{i} { }
+	D1(int i) : B1{}, B2{i} { cout << "D1(" << i << ")\n"; }
 };
 
 struct D2 : B1, B2 {
-	D2(int i) : B2{i} { }
+	D2(int i) : B2{i} { cout << "D2(" << i << ")\n"; }
 };
 
 struct D3 : B1, B2 {
 	//D3(int i) { }
 		// error: no matching function for call to 'B2::B2()'
 };
-
-// add undef func
-
-B1::B1()
-{
-	cout << "B1::B1()\n";
-}
-
-B2::B2(int i)
-{
-	cout << "B2::B2(" << i << ")\n";
-}
 
 // add main
 
