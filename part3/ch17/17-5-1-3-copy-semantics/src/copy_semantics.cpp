55f861e5327d869980cd882e4ca3d2cb3ff60d80
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-04-03 15:17:42 +0900

    Linux環境に対応

--- part3/ch17/17-5-1-3-copy-semantics/src/copy_semantics.cpp
+++ part3/ch17/17-5-1-3-copy-semantics/src/copy_semantics.cpp
@@ -16,21 +16,24 @@ struct S {
 S x {new int{0}};
 void f()
 {
+	cout << "-- f() --\n";
+
 	S y {x};
 
-	cout << "*x.p: " << *x.p << ", *y.p: " << *y.p << '\n';
+	cout << "initially; *x.p = " << *x.p << ", *y.p = " << *y.p << '\n';
 
 	*y.p = 1;
+	cout << "after *y.p = 1; *x.p = " << *x.p << ", *y.p = " << *y.p << '\n';
+
 	*x.p = 2;
-	cout << "*x.p: " << *x.p << ", *y.p: " << *y.p << '\n';
+	cout << "after *x.p = 2; *x.p = " << *x.p << ", *y.p = " << *y.p << '\n';
 
 	delete y.p;
 	y.p = new int{3};
-	cout << "*y.p: " << *y.p << '\n';
+	cout << "after delete y.p; y.p = new int{3}; *y.p = " << *y.p << '\n';
 
 	*x.p = 4; // terrible
-
-	cout << "*y.p: " << *y.p << '\n';
+	cout << "after *x.p = 4; *y.p = " << *y.p << '\n';
 }
 
 struct S2 {
@@ -43,16 +46,22 @@ S2 x2 {shared_ptr<int>(new int{0})};
 
 void f2()
 {
+	cout << "-- f2() --\n";
+
 	S2 y2 {x2};
-	cout << "*x2.p: " << *x2.p << ", *y2.p: " << *y2.p << '\n';
+	cout << "after S2 y2{x2}; *x2.p: " << *x2.p << ", *y2.p: " << *y2.p << '\n';
 
 	*y2.p = 1;
+	cout << "after *y2.p = 1; *x2.p: " << *x2.p << ", *y2.p: " << *y2.p << '\n';
+
 	*x2.p = 2;
-	cout << "*x2.p: " << *x2.p << ", *y2.p: " << *y2.p << '\n';
+	cout << "after *x2.p = 2; *x2.p: " << *x2.p << ", *y2.p: " << *y2.p << '\n';
 
 	y2.p.reset(new int{3});
+	cout << "after y2.p.reset(new int{3}); *x2.p: " << *x2.p << ", *y2.p: " << *y2.p << '\n';
+
 	*x2.p = 4;
-	cout << "*x2.p: " << *x2.p << ", *y2.p: " << *y2.p << '\n';
+	cout << "after *x2.p = 4; *x2.p: " << *x2.p << ", *y2.p: " << *y2.p << '\n';
 }
 
 using Descriptor = int;
@@ -67,7 +76,7 @@ public:
 	void write_block(Descriptor);
 
 	Image() : rep{new Representation{}}{ }
-	Representation* get_rep() const { return rep.get();}
+	const shared_ptr<Representation>& get_rep() const { return rep;}
 private:
 	Representation* clone();
 	shared_ptr<Representation> rep;
@@ -86,10 +95,26 @@ Representation* Image::clone()
 void Image::write_block(Descriptor d)
 {
 	if (rep.use_count() > 1)
-		//rep = shared_ptr<Representation>{clone()};
-		rep.reset(clone());
+		rep = shared_ptr<Representation>{clone()};
+		//rep.reset(clone());
 
 	rep->canvas[100][200] = d;
+}
+
+void test_copy_on_write()
+{
+	cout << "-- test_copy_on_write() --\n";
+
+	Image im1;
+
+	cout << "Image im1; im1.rep = " << im1.get_rep().use_count() << "," << im1.get_rep().get() << endl;
+
+	Image im2{im1};
+	cout << "Image im2{im1}; im2.rep = " << im2.get_rep().use_count() << "," << im2.get_rep().get() << endl;
+
+	im2.write_block(10);
+	cout << "im2.write_block(); im1.rep = " << im1.get_rep().use_count() << "," << im1.get_rep().get()
+			<< "; im2.rep = " << im2.get_rep().use_count() << "," << im2.get_rep().get() << endl;
 }
 
 // add main
@@ -98,14 +123,6 @@ int main()
 {
 	f();
 	f2();
-
-	Image im1;
-	cout << "im1.get_rep(): " << im1.get_rep() << '\n';
-
-	Image im2{im1};
-	cout << "im2.get_rep(): " << im2.get_rep() << '\n';
-
-	im2.write_block(10);
-	cout << "im2.get_rep(): " << im2.get_rep() << '\n';
+	test_copy_on_write();
 }
 
