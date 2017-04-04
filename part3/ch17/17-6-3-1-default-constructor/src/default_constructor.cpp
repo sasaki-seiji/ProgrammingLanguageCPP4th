04ca5d4391e63b8b2d514dbe092e0886ef69cf47
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-04-04 20:14:19 +0900

    Linux環境に対応

--- part3/ch17/17-6-3-1-default-constructor/src/default_constructor.cpp
+++ part3/ch17/17-6-3-1-default-constructor/src/default_constructor.cpp
@@ -11,6 +11,10 @@ using namespace std;
 struct X {
 	X(int);
 };
+X::X(int i)
+{
+	cout << "X::X(" << i << ")\n";
+}
 
 X a {1};
 //X b {};
@@ -22,28 +26,22 @@ struct Y {
 	Y(const string& s);
 	Y() = default;
 };
-
-// add undef-func
-
-X::X(int i)
-{
-	cout << "X::X(" << i << ")\n";
-}
-
 Y::Y(const string& s)
+	:s{s}, n{10}
 {
 	cout << "Y::Y(" << s << ")\n";
 }
+
 // add main
 
 int main()
 {
 	Y y1 {"abc"};
-	cout << "y1.s: " << y1.s << ", y1.n: " << y1.n << '\n';
+	cout << "Y y1{\"abc\"}; y1.s = " << y1.s << ", y1.n = " << y1.n << '\n';
 
 	Y y2 { };
-	cout << "y2.s: " << y2.s << ", y2.n: " << y2.n << '\n';
+	cout << "Y y2{}; y2.s = " << y2.s << ", y2.n = " << y2.n << '\n';
 
 	Y y3;
-	cout << "y3.s: " << y3.s << ", y3.n: " << y3.n << '\n';
+	cout << "Y y3; y3.s = " << y3.s << ", y3.n = " << y3.n << '\n';
 }
