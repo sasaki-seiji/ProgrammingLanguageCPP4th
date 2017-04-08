f5245f236b2b623b3ffb994815a03d04c03b79be
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-04-08 09:18:41 +0900

    Linux環境に対応

--- part3/ch17/17-6-3-4-partial-invariant/src/partial_invariant.cpp
+++ part3/ch17/17-6-3-4-partial-invariant/src/partial_invariant.cpp
@@ -6,7 +6,16 @@
  */
 
 #include <vector>
+#include <iostream>
 using namespace std;
+
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+	for (auto x : v)
+		os << x << ", ";
+	return os;
+}
 
 class Tic_tac_toe {
 public:
@@ -20,8 +29,12 @@ public:
 	}
 
 	enum State { empty, nought, cross };
+	enum State& operator[](int i) { return pos[i]; }
 private:
 	vector<State> pos;
+
+	friend ostream& operator<<(ostream& os,const Tic_tac_toe& toe)
+	{ return os << toe.pos; }
 };
 
 class Tic_tac_toe2 {
@@ -32,15 +45,22 @@ public:
 	~Tic_tac_toe2() = default;
 
 	enum State { empty, nought, cross };
+	enum State& operator[](int i) { return pos[i]; }
 private:
 	vector<State> pos;
+
+	friend ostream& operator<<(ostream& os,const Tic_tac_toe2& toe)
+	{ return os << toe.pos; }
 };
 
 class Tic_tac_toe3 {
 public:
 	enum State { empty, nought, cross };
+	enum State& operator[](int i) { return pos[i]; }
 private:
 	vector<State> pos {vector<State>(9)};
+	friend ostream& operator<<(ostream& os,const Tic_tac_toe3& toe)
+	{ return os << toe.pos; }
 };
 
 // add main
@@ -48,14 +68,32 @@ private:
 int main()
 {
 	Tic_tac_toe toe11, toe12; // no destructor
+	toe12[7] = Tic_tac_toe::State::cross;
+	cout << "toe11: " << toe11 << ", toe12: " << toe12 << endl;
+
 	toe11 = toe12;
+	cout << "toe11 = toe12;　toe11: " << toe11 << endl;
+
 	Tic_tac_toe toe13{toe11};	// not defined copy-constructor
+	cout << "Tic_tac_toe toe13{toe11}; toe13: " << toe13 << endl;
 
 	Tic_tac_toe2 toe21, toe22;
+	toe22[5] = Tic_tac_toe2::State::nought;
+	cout << "toe21: " << toe21 << ", toe22: " << toe22 << endl;
+
 	toe21 = toe22;
+	cout << "toe21 = toe22;　toe21: " << toe21 << endl;
+
 	Tic_tac_toe2 toe23{toe21};
+	cout << "Tic_tac_toe2 toe23{toe21}; toe23: " << toe23 << endl;
 
 	Tic_tac_toe3 toe31, toe32;
+	toe32[3] = Tic_tac_toe3::State::cross;
+	cout << "toe31: " << toe31 << ", toe32: " << toe32 << endl;
+
 	toe31 = toe32;
+	cout << "toe31 = toe32;　toe31: " << toe31 << endl;
+
 	Tic_tac_toe3 toe33{toe31};
+	cout << "Tic_tac_toe3 toe33{toe31}; toe33: " << toe33 << endl;
 }
