59026227a6ad091194662014e88a9be3c8f32acf
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-04-22 17:49:57 +0900

    Linux環境に対応

--- part3/ch21/21-2-1-implementation-inherit/src/BBwidget.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * BBwidget.cpp
- *
- *  Created on: 2016/08/27
- *      Author: sasaki
- */
-
-#include "BBwidget.h"
-
--- part3/ch21/21-2-1-implementation-inherit/src/Ivalbox.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Ivalbox.cpp
- *
- *  Created on: 2016/08/27
- *      Author: sasaki
- */
-
-#include "Ivalbox.h"
-
