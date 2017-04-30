f97be4997a678875449a9738ad63b7318250736d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-04-30 19:32:56 +0900

    空の実装ファイルを削除

--- part3/ch21/21-2-3-other-implement/src/Ivaldial.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Ivaldial.cpp
- *
- *  Created on: 2016/08/28
- *      Author: sasaki
- */
-
-#include "Ivaldial.h"
-
--- part3/ch21/21-2-3-other-implement/src/Ivalslider.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Ivalslider.cpp
- *
- *  Created on: 2016/08/28
- *      Author: sasaki
- */
-
-#include "Ivalslider.h"
-
