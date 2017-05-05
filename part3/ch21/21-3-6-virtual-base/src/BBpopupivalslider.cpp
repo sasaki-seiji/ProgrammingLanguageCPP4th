e43ebb8fe0d17b950bdea9a1571ce5e387fee68b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-05-05 17:43:19 +0900

    ウインドウマネージャに対応

--- part3/ch21/21-3-6-virtual-base/src/BBpopupivalslider.cpp
+++ part3/ch21/21-3-6-virtual-base/src/BBpopupivalslider.cpp
@@ -1,7 +1,7 @@
 /*
  * BBpopupivalslider.cpp
  *
- *  Created on: 2016/09/02
+ *  Created on: 2016/08/28
  *      Author: sasaki
  */
 
@@ -9,12 +9,84 @@
 #include <iostream>
 using namespace std;
 
+BB_popup_ival_slider::BB_popup_ival_slider(int low, int high, int l, int t, int w, int h)
+	: BBslider{low,high,l,t,w,h,false}, changed{false}
+{
+}
+
+int BB_popup_ival_slider::get_value()
+{
+	int val = value();
+	changed = false;
+	return val;
+}
+
+void BB_popup_ival_slider::set_value(int i)
+{
+	if (low() <= i && i <= high()) {
+		value(i);
+		changed = true;
+	}
+}
+
+void BB_popup_ival_slider::reset_value(int i)
+{
+	if (low() <= i && i <= high()) {
+		value(i);
+		changed = false;
+	}
+}
+
+void BB_popup_ival_slider::prompt()
+{
+	cout << "BB_popup_ival_slider::prompt()\n";
+}
+
+bool BB_popup_ival_slider::was_changed() const
+{
+	return changed;
+}
+
+void BB_popup_ival_slider::incr()
+{
+	if (value() < high()) {
+		value(value()+1);
+		changed = true;
+	}
+}
+
+void BB_popup_ival_slider::decr()
+{
+	if (low() < value()) {
+		value(value()-1);
+		changed = true;
+	}
+}
+
+
+void BB_popup_ival_slider::on_changed(int i)
+{
+	set_value(i);
+}
+
 void BB_popup_ival_slider::popup()
 {
-	cout << "BB_popup_ival_slider::popup()\n";
+	Window_manager* mgr = Window_manager::get_instance();
+	mgr->bring_top(this);
+	show();
 }
 
 void BB_popup_ival_slider::popdown()
 {
-	cout << "BB_popup_ival_slider::popdown()\n";
+	hide();
+}
+
+void BB_popup_ival_slider::display_info() const
+{
+	cout << boolalpha;
+	cout << "BB_popup_ival_slider(left=" << left()
+			<< ",top=" << top() << ",width=" << width() << ",height=" << height()
+			<< ",visible=" << is_visible() << endl;
+	cout << "  value=" << value() << ",changed=" << changed << endl;
+
 }
--- part3/ch21/21-3-6-virtual-base/src/user.cpp
+++ part3/ch21/21-3-6-virtual-base/src/user.cpp
@@ -10,41 +10,101 @@
 #include <iostream>
 using namespace std;
 
-void interact(Ival_slider* pb)
+Popup_ival_slider* popup_slider_factory(int ll, int hh)
 {
-	int old_val = pb->get_value();
+	return new BB_popup_ival_slider(ll, hh);
+}
+
+void interact(Ival_box* pb)
+{
+	cout << "-- interact(Ival_box*) --\n";
+
+	bool changed;
+	int i;
+
 	pb->prompt();
 
-	int i = pb->get_value();
-	if (i != old_val) {
-		cout << "new value: " << i << '\n';
-	}
-	else {
-		cout << "not changed. old value: " << old_val << '\n';
-	}
+	cout << "old value: " << pb->get_value() << endl;
+
+	pb->set_value(5);
+	changed = pb->was_changed();
+	i = pb->get_value();
+	cout << "after set_value(5): " << i << ", changed: " << changed << endl;
+	cout << "after get_value(), changed: " << pb->was_changed() << endl;
+
+	pb->set_value(2);
+	pb->reset_value(7);
+	changed = pb->was_changed();
+	i = pb->get_value();
+	cout << "set_value(2), reset_value(7): " << i << ", changed: " << changed << endl;
 }
 
-void some_fct()
+
+void incr_decr(Ival_slider* ivs)
 {
-	unique_ptr<Ival_slider> p3 { new BB_popup_ival_slider{0,10} };
-	interact(p3.get());
+	cout << "-- incr_decr(Ival_slider*) --\n";
+
+	int v;
+	bool changed;
+
+	cout << boolalpha;
+
+	ivs->prompt();
+	changed = ivs->was_changed(); v = ivs->get_value();
+	cout << "original value: " << v << ", changed: " << changed <<  endl;
+
+	ivs->decr();
+	changed = ivs->was_changed(); v = ivs->get_value();
+	cout << "after decr(), value: " << v << ", changed: " << changed << endl;
+
+	ivs->incr(); ivs->incr();
+	changed = ivs->was_changed(); v = ivs->get_value();
+	cout << "after incr() twice, value: " << v << ", changed: " << changed << endl;
+
+	ivs->decr();
+	changed = ivs->was_changed(); v = ivs->get_value();
+	cout << "after decr(), value: " << v << ", changed: " << changed << endl;
 }
 
-void popup_test(Popup_ival_slider* pivs)
+void popup_down(Popup_ival_slider* pivs)
 {
+	cout << "-- popup_down(Popup_ival_slider*) --\n";
+
+	cout << "- original state -\n";
+	pivs->display_info();
+
+	Window_manager* mgr = Window_manager::get_instance();
+
+	cout << "mouse1hit: (60,60)\n";
+	mgr->mouse1hit(60,60);
+
+	cout << "- after hittest -\n";
+	pivs->display_info();
+
+	cout << "popup(), mouse1hit: (70,70)\n";
 	pivs->popup();
+	mgr->mouse1hit(70,70);
+
+	cout << "- after hittest -\n";
+	pivs->display_info();
+
+	cout << "popdown(), mouse1hit: (80,80)\n";
 	pivs->popdown();
+	mgr->mouse1hit(80,80);
+
+	cout << "- after hittest -\n";
+	pivs->display_info();
 }
 
 // add main
 
 int main()
 {
-	some_fct();
-
-	Popup_ival_slider *pivs = new BB_popup_ival_slider{0,99};
-	popup_test(pivs);
-
 	cout << "sizeof(BB_popup_ival_slider): " << sizeof(BB_popup_ival_slider) << '\n';
+
+	Popup_ival_slider *pivs = popup_slider_factory(0,99);
+	interact(pivs);
+	incr_decr(pivs);
+	popup_down(pivs);
 }
 
