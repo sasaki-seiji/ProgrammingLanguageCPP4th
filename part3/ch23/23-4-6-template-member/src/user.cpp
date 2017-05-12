7cc9ae6aab28fde9eb92327853823b354390f48c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-05-13 08:39:32 +0900

    Linux環境に対応

--- part3/ch23/23-4-6-template-member/src/user.cpp
+++ part3/ch23/23-4-6-template-member/src/user.cpp
@@ -7,6 +7,7 @@
 
 
 #include "complex.h"
+#include "complex2.h"
 #include <iostream>
 using namespace std;
 
@@ -24,11 +25,17 @@ complex<double> cd2 {2.0F, 3.0F};
 
 
 class Quad {
+public:
+	long long ll {0LL};
 };
 
 complex<Quad> cq;
 //complex<int> ci {cq};
 	// error: cannot convert '<brace-enclosed initializer list>' to 'int' in initialization
+
+complex2<float> cf4 {2.1,2.9};
+complex2<double> cd3 {cf4};
+complex2<float> cf5 {cd3};
 
 // add main
 
@@ -40,5 +47,9 @@ int main()
 
 	cout << "cf3: " << cf3 << '\n';
 	cout << "cd2: " << cd2 << '\n';
+
+	cout << "cf4: " << cf4 << endl;
+	cout << "cd3: " << cd3 << endl;
+	cout << "cf5: " << cf5 << endl;
 }
 
