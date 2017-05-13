32f52bc1c9ae5a0f72e88b17598c6b99353aba79
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-05-14 08:16:32 +0900

    Linux環境に対応

--- part3/ch23/23-5-2-function-template-argument-derivation/src/function_template_argument_derivation.cpp
+++ part3/ch23/23-5-2-function-template-argument-derivation/src/function_template_argument_derivation.cpp
@@ -29,15 +29,36 @@ void f(const T*, U(*)(U))
 	const char* t_name = typeid(T).name();
 	const char* u_name = typeid(U).name();
 
-	cout << "f(const " << t_name << "*, " << u_name << "(*)(" << u_name << "))\n";
+	cout << "f(const <" << t_name << ">*, <" << u_name << ">(*)(<" << u_name << ">))\n";
 }
 
 int g(int) { return 1; }
+
+template<typename T>
+void f2(T i, T* p);
+
+void g2(int i)
+{
+	f2(i,&i);
+	//f2(i,"Remember!");
+		// error: no matching function for call to ‘f2(int&, const char [10])’
+}
+
+// undefs
+
+template<typename T>
+void f2(T i, T* p)
+{
+	const char* t_name = typeid(T).name();
+	cout << "f2(<" << t_name << ">: " << i << ", <" << t_name << ">*: " << p << ")\n";
+}
+
 
 // add main
 
 int main()
 {
 	h("string");
+	g2(10);
 }
 
