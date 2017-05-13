7c078d994eac65ba31b84406aa0351ff1756f529
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-05-13 12:07:56 +0900

    Linux環境に対応

--- part3/ch23/23-4-6-3-unnested-type-Link/src/List.h
+++ part3/ch23/23-4-6-3-unnested-type-Link/src/List.h
@@ -21,40 +21,76 @@ class Link {
 	T val;
 	Link* succ;
 	Link* prev;
+
+	Link(const T&v = T{}) : val{v}, succ{this}, prev{this} { }
+	void insert_before(Link* p)
+		{ prev->succ = p; p->prev = prev; p->succ = this; prev = p; }
+	void insert_after(Link* p)
+		{ succ->prev = p; p->succ = succ; p->prev = this; succ = p; }
+	Link* unlink()
+		{ prev->succ = succ; succ->prev = prev; return this; }
 };
 
 template<typename T, typename Allocator>
 class List {
 private:
-	Link<T>* head;
+	Link<T> head;
+	size_t sz;
 	Allocator alloc;
+
 public:
-	List() : head{nullptr}, alloc{Allocator{}} { }
-	void add_head(const T&);
+	List() : sz{0}, alloc{Allocator{}} { }
+	List(const List&) = delete;
+	List& operator=(const List&) = delete;
+	~List() { clear(); }
+
+	bool empty() { return sz == 0; }
+	size_t size() { return sz; }
+	void clear();
+
+	void push_front(const T&);
+	T& front();
+	void pop_front();
 	void print_all(std::ostream&);
 };
 
 
 template<typename T, typename A>
-void List<T,A>::add_head(const T& v)
+void List<T,A>::clear()
+{
+	while(!empty()) pop_front();
+}
+
+template<typename T, typename A>
+void List<T,A>::push_front(const T& v)
 {
 	Link<T> *p = static_cast<Link<T>*>(alloc.allocate(sizeof(Link<T>)));
-	p->val = v;
-	p->prev = p->succ = nullptr;
+	new(p) Link<T>(v);
 
-	if (head) {
-		head->prev = p;
-		p->succ = head;
-	}
+	head.insert_after(p);
+	++sz;
+}
 
-	head = p;
+template<typename T, typename A>
+T& List<T,A>::front()
+{
+	return head.succ->val;
+}
+
+template<typename T, typename A>
+void List<T,A>::pop_front()
+{
+	Link<T> *p = head.succ->unlink();
+	--sz;
+	(p->val).~T();
+	alloc.deallocate(p, sizeof(Link<T>));
 }
 
 template<typename T, typename A>
 void List<T,A>::print_all(std::ostream& os)
 {
-	Link<T> *cur = head;
-	while (cur) {
+	Link<T> *cur = head.succ;
+	while (cur!=&head) {
 		os << cur->val << ' ';
 		cur = cur->succ;
 	}
