2b0b9ddb6e1c17e1caeb63d1781269b6c42ee573
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-05-21 16:25:02 +0900

    共通のHas_not_equal、Enable_ifテンプレートを使用するように変更

--- part3/ch28/28-4-4-has-not-equals/src/my_find.h
+++ part3/ch28/28-4-4-has-not-equals/src/my_find.h
@@ -8,15 +8,20 @@
 #ifndef MY_FIND_H_
 #define MY_FIND_H_
 
-#include "has_not_equals.h"
-#include <type_traits>
-#include <iostream>
+#include "has_not_equal.h"
+#include "type_converter.h" // Enable_if
+using namespace Estd;
+//#include <type_traits>
 
+#if 0
 template<bool B, typename T=void>
 using Enable_if = typename std::enable_if<B,T>::type;
+#endif
+
+#include <iostream>
 
 template<typename Iter, typename Val>
-Enable_if<Has_not_equals<Iter>(),Iter> my_find(Iter first, Iter last, Val v)
+Enable_if<Has_not_equal<Iter>(),Iter> my_find(Iter first, Iter last, Val v)
 {
 	std::cout << "my_find(): use !=\n";
 	while (first!=last && !(*first==v))
@@ -25,7 +30,7 @@ Enable_if<Has_not_equals<Iter>(),Iter> my_find(Iter first, Iter last, Val v)
 }
 
 template<typename Iter, typename Val>
-Enable_if<!Has_not_equals<Iter>(),Iter> my_find(Iter first, Iter last, Val v)
+Enable_if<!Has_not_equal<Iter>(),Iter> my_find(Iter first, Iter last, Val v)
 {
 	std::cout << "my_find(): not use !=\n";
 	while (!(first==last) && !(*first==v))
--- part3/ch28/28-4-4-has-not-equals/src/user.cpp
+++ part3/ch28/28-4-4-has-not-equals/src/user.cpp
@@ -19,7 +19,7 @@ int main()
 {
 	cout << boolalpha;
 
-	cout << "Has_not_equals<Vector_iter<int>>(): " << Has_not_equals<Vector_iter<int>>() << '\n';
+	cout << "Has_not_equal<Vector_iter<int>>(): " << Has_not_equal<Vector_iter<int>>() << '\n';
 	Vector<int> v1 { 2, 3, 10, 5, 7, 8};
 	Vector_iter<int> it11 = my_find(v1.begin(),v1.end(),7);
 	Vector_iter<int> it12 = my_find(v1.begin(),v1.end(),9);
@@ -27,7 +27,7 @@ int main()
 	if (it11!=v1.end()) cout << "found: " << *it11 << '\n';
 	if (it12!=v1.end()) cout << "found: " << *it12 << '\n';
 
-	cout << "Has_not_equals<Vector2_iter<int>>(): " << Has_not_equals<Vector2_iter<int>>() << '\n';
+	cout << "Has_not_equal<Vector2_iter<int>>(): " << Has_not_equal<Vector2_iter<int>>() << '\n';
 	Vector2<int> v2 { 2, 3, 10, 5, 7, 8};
 	Vector2_iter<int> it21 = my_find(v2.begin(),v2.end(),7);
 	Vector2_iter<int> it22 = my_find(v2.begin(),v2.end(),9);
