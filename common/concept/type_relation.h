69eddaad98cc59cb010b7d072bcc907c2b5fac8a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-05-28 20:06:09 +0900

    Is_same 型関連性関数を追加

--- common/concept/type_relation.h
+++ common/concept/type_relation.h
@@ -14,6 +14,12 @@ namespace Estd {
 // type relation
 
 template<typename X, typename Y>
+constexpr bool Is_same()
+{
+	return std::is_same<X,Y>::value;
+}
+
+template<typename X, typename Y>
 constexpr bool Convertible()
 {
 	return std::is_convertible<X,Y>::value;
