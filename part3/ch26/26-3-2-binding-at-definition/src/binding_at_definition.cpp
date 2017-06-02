01f5978fef845d1f120cc3755791ce9b9b81a723
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-06-02 22:38:24 +0900

    Linux環境に対応

--- part3/ch26/26-3-2-binding-at-definition/src/binding_at_definition.cpp
+++ part3/ch26/26-3-2-binding-at-definition/src/binding_at_definition.cpp
@@ -23,10 +23,39 @@ int y;
 
 int z = f(2);
 
+void g(double);
+void g2(double);
+
+template<typename T>
+int ff(T a)
+{
+	g2(2);
+	//g3(2);
+		// error: there are no arguments to ‘g3’ that depend on a template parameter, so a declaration of ‘g3’ must be available [-fpermissive]
+	g(a);
+
+	return 123;
+}
+
+void g(int);
+void g3(int);
+
+int xx = ff(5);
+
+
+// undefs
+
+void g(double d) { cout << "g(double: " << d << ")\n"; }
+void g2(double d) { cout << "g2(double: " << d << ")\n"; }
+void g(int i) { cout << "g(int: " << i << ")\n"; }
+void g3(int i) { cout << "g3(int: " << i << ")\n"; }
+
+
 // add main
 
 int main()
 {
-	cout << "x: " << x << '\n';
-	cout << "z: " << z << '\n';
+	cout << "x: " << x << endl;
+	cout << "z: " << z << endl;
+	cout << "xx: " << xx << endl;
 }
