32d116e32d7cdd57df420dff26886007ca54597b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-06-03 18:00:49 +0900

    Linux環境に対応

--- part3/ch26/26-3-7-explicit-dependency-matrix/src/Matrix.h
+++ part3/ch26/26-3-7-explicit-dependency-matrix/src/Matrix.h
@@ -10,11 +10,10 @@
 
 #include <array>
 #include <initializer_list>
+#include <cassert>
 
 template<typename T>
 class Matrix_base {
-public:
-	int size() const { return sz; }
 protected:
 	int sz;
 	T* elem;
@@ -25,20 +24,29 @@ protected:
 template<typename T, int N>
 class Matrix : public Matrix_base<T> {
 	std::array<int,N> dim;
+
 public:
 	T* data()
 	{
+		//return elem;
+			// error: ‘elem’ was not declared in this scope
 		return this->elem;
+	}
+	int size() const
+	{
+		//return sz;
+			// error: ‘sz’ was not declared in this scope
+		return this->sz;
 	}
 
 	Matrix(std::initializer_list<int> il)
 	{
-		int s = 1;
-		int i = 0;
-		auto it = il.begin();
-		for ( ; i < N && it!=il.end(); ++i, ++it ) {
-			s *= *it;
-			dim[i] = *it;
+		assert(il.size() == N);
+
+		int s = 1, i = 0;
+		for ( auto x : il ) {
+			s *= x;
+			dim[i++] = x;
 		}
 		this->alloc(s);
 	}
