9052e3b4867d3620772a00456a8dd763503e50a4
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-06-11 07:51:06 +0900

    Linux環境に対応

--- part3/ch25/25-3-4-2-not-overload-but-specialized/src/not_overload_but_specialized.cpp
+++ part3/ch25/25-3-4-2-not-overload-but-specialized/src/not_overload_but_specialized.cpp
@@ -5,20 +5,22 @@
  *      Author: sasaki
  */
 
+#include "iterator_traits.h"
+using namespace Estd;
+
 #include <climits>
 #include <iostream>
 #include <vector>
+#include <iterator>
 
 template<typename T> T max_value();
 
 template<> constexpr int max_value<int>() { return INT_MAX; }
 template<> constexpr char max_value<char>() { return CHAR_MAX; }
 
-template<typename T>
-using Value_type = typename T::value_type;
 
 template<typename Iter>
-Iter my_alogo(Iter p)
+Iter my_algo(Iter p)
 {
 	auto x = max_value<Value_type<Iter>>();
 	std::cout << "my_alogo - x :" << (long)x << '\n';
@@ -29,7 +31,7 @@ int max2(int) { return INT_MAX; }
 int max2(char) { return CHAR_MAX; }
 
 template<typename Iter>
-Iter my_alogo2(Iter p)
+Iter my_algo2(Iter p)
 {
 	auto x = max2(Value_type<Iter>{});
 	std::cout << "my_alogo2 - x :" << (long)x << '\n';
@@ -41,15 +43,18 @@ Iter my_alogo2(Iter p)
 int main()
 {
 	std::vector<int> vi(10);
-	my_alogo(vi.begin());
+	my_algo(vi.begin());
 
 	std::vector<char> vc(10);
-	my_alogo(vc.begin());
+	my_algo(vc.begin());
 
 	std::vector<long> vl(10);
 	//my_alogo(vl.begin());
-		// undefined reference to `long max_value<long>()'
+		// `long max_value<long>()' に対する定義されていない参照です
 
-	my_alogo2(vi.begin());
-	my_alogo2(vc.begin());
+	int ai[] = {1,2,3,4};
+	my_algo(std::begin(ai));
+
+	my_algo2(vi.begin());
+	my_algo2(vc.begin());
 }
