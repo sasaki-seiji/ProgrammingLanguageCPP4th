674da8b90a86aadd484021ac00b80cdd08383d2d
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-06-18 19:16:44 +0900

    2つの型のEquality_comparable判定テストを追加

--- part3/ch24/24-4-4-Equality_comparable/src/user.cpp
+++ part3/ch24/24-4-4-Equality_comparable/src/user.cpp
@@ -13,10 +13,13 @@ using namespace Estd;
 #include <vector>
 #include <iostream>
 #include <memory>
+#include <complex>
 using namespace std;
 
-int main()
+void test_single_type()
 {
+	cout << "-- test_single_type() --\n";
+
 	bool b1 = Equality_comparable<int>();
 	bool b2 = Equality_comparable<vector<int>>();
 	bool b3 = Equality_comparable<unique_ptr<int>>();
@@ -27,4 +30,24 @@ int main()
 	cout << "Equality_comparable<vector<int>>(): " << b2 << endl;
 	cout << "Equality_comparable<unique_ptr<int>>(): " << b3 << endl;
 	cout << "Equality_comparable<istream>(): " << b4 << endl;
+}
+
+void test_type_pair()
+{
+	cout << "-- test_type_pair() --\n";
+
+	bool b1 = Equality_comparable<int,double>();
+	bool b2 = Equality_comparable<double,complex<double>>();
+	bool b3 = Equality_comparable<vector<int>,vector<double>>();
+
+	cout << boolalpha;
+	cout << "Equality_comparable<int,double>(): " << b1 << endl;
+	cout << "Equality_comparable<double,complex<double>>(): " << b2 << endl;
+	cout << "Equality_comparable<vector<int>,vector<double>>(): " << b3 << endl;
+}
+
+int main()
+{
+	test_single_type();
+	test_type_pair();
 }
