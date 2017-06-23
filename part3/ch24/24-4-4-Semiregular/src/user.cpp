1532f9c816a7c1d778dd5a683397bf82f7ef0092
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-06-23 20:10:24 +0900

    complex<double>の判定を追加

--- part3/ch24/24-4-4-Semiregular/src/user.cpp
+++ part3/ch24/24-4-4-Semiregular/src/user.cpp
@@ -9,6 +9,7 @@
 using namespace Estd;
 
 #include <vector>
+#include <complex>
 #include <memory>
 #include <iterator>
 #include <iostream>
@@ -18,17 +19,19 @@ int main()
 {
 	bool b1 = Semiregular<int>();
 	bool b2 = Semiregular<vector<int>>();
-	bool b3 = Semiregular<ostream_iterator<char>>();
-	bool b4 = Semiregular<unique_ptr<int>>();
-	bool b5 = Semiregular<istream>();
-	bool b6 = Semiregular<type_info>();
+	bool b3 = Semiregular<complex<double>>();
+	bool b4 = Semiregular<ostream_iterator<char>>();
+	bool b5 = Semiregular<unique_ptr<int>>();
+	bool b6 = Semiregular<istream>();
+	bool b7 = Semiregular<type_info>();
 
 	cout << boolalpha;
 	cout << "Semiregular<int>(): " << b1 << endl;
 	cout << "Semiregular<vector<int>>(): " << b2 << endl;
-	cout << "Semiregular<ostream_iterator<char>>(): " << b3 << endl;
-	cout << "Semiregular<unique_ptr<int>>(): " << b4 << endl;
-	cout << "Semiregular<istream>(): " << b5 << endl;
-	cout << "Semiregular<type_info>(): " << b6 << endl;
+	cout << "Semiregular<complex<double>>(): " << b3 << endl;
+	cout << "Semiregular<ostream_iterator<char>>(): " << b4 << endl;
+	cout << "Semiregular<unique_ptr<int>>(): " << b5 << endl;
+	cout << "Semiregular<istream>(): " << b6 << endl;
+	cout << "Semiregular<type_info>(): " << b7 << endl;
 }
 
