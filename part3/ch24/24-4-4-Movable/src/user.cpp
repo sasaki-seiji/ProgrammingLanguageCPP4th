bd7468117510de8b1df8c05e16f4664dc853738a
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-06-23 20:08:48 +0900

    int,vector<int>,complex<double>,ostream_iterator<char>の判定を追加

--- part3/ch24/24-4-4-Movable/src/user.cpp
+++ part3/ch24/24-4-4-Movable/src/user.cpp
@@ -8,6 +8,9 @@
 #include "movable.h"
 using namespace Estd;
 
+#include <vector>
+#include <complex>
+#include <iterator>
 #include <memory>
 #include <iostream>
 using namespace std;
@@ -15,13 +18,19 @@ using namespace std;
 int main()
 {
 	bool b1 = Movable<int>();
-	bool b2 = Movable<unique_ptr<int>>();
-	bool b3 = Movable<istream>();
-	bool b4 = Movable<type_info>();
+	bool b2 = Movable<vector<int>>();
+	bool b3 = Movable<complex<double>>();
+	bool b4 = Movable<ostream_iterator<char>>();
+	bool b5 = Movable<unique_ptr<int>>();
+	bool b6 = Movable<istream>();
+	bool b7 = Movable<type_info>();
 
 	cout << boolalpha;
 	cout << "Movable<int>(): " << b1 << endl;
-	cout << "Movable<unique_ptr<int>>(): " << b2 << endl;
-	cout << "Movable<istream>(): " << b3 << endl;
-	cout << "Movable<type_info>(): " << b4 << endl;
+	cout << "Movable<vector<int>>(): " << b2 << endl;
+	cout << "Movable<complex<double>>(): " << b3 << endl;
+	cout << "Movable<ostream_iterator<char>>(): " << b4 << endl;
+	cout << "Movable<unique_ptr<int>>(): " << b5 << endl;
+	cout << "Movable<istream>(): " << b6 << endl;
+	cout << "Movable<type_info>(): " << b7 << endl;
 }
