1bf61bbf8b4e659842be152ef3ea62c7b71fb840
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-06-23 20:09:33 +0900

    vector<int>,complex<double>,ostream_iterator<char>の判定を追加

--- part3/ch24/24-4-4-Copyable/src/user.cpp
+++ part3/ch24/24-4-4-Copyable/src/user.cpp
@@ -11,6 +11,8 @@ using namespace Estd;
 
 #include <memory>
 #include <vector>
+#include <complex>
+#include <iterator>
 #include <iostream>
 using namespace std;
 
@@ -18,14 +20,18 @@ int main()
 {
 	bool b1 = Copyable<int>();
 	bool b2 = Copyable<vector<int>>();
-	bool b3 = Copyable<unique_ptr<int>>();
-	bool b4 = Copyable<istream>();
-	bool b5 = Copyable<type_info>();
+	bool b3 = Copyable<complex<double>>();
+	bool b4 = Copyable<ostream_iterator<char>>();
+	bool b5 = Copyable<unique_ptr<int>>();
+	bool b6 = Copyable<istream>();
+	bool b7 = Copyable<type_info>();
 
 	cout << boolalpha;
 	cout << "Copyable<int>(): " << b1 << endl;
 	cout << "Copyable<vector<int>>(): " << b2 << endl;
-	cout << "Copyable<unique_ptr<int>>(): " << b3 << endl;
-	cout << "Copyable<istream>(): " << b4 << endl;
-	cout << "Copyable<type_info>(): " << b5 << endl;
+	cout << "Copyable<complex<double>>(): " << b3 << endl;
+	cout << "Copyable<ostream_iterator<char>>(): " << b4 << endl;
+	cout << "Copyable<unique_ptr<int>>(): " << b5 << endl;
+	cout << "Copyable<istream>(): " << b6 << endl;
+	cout << "Copyable<type_info>(): " << b7 << endl;
 }
