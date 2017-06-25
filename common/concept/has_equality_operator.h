b5d09af6b9756fa23c2f4e6315f3684e5e92ee8b
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-06-25 11:01:34 +0900

    Linux環境に対応
    has_equal、has_not_equalの判定で、引数をconst参照からコピーに変更

--- common/concept/has_equality_operator.h
+++ common/concept/has_equality_operator.h
@@ -19,7 +19,7 @@ template<typename T, typename U>
 struct get_equal_result {
 private:
 	template<typename X, typename Y>
-		static auto check(const X& x, const Y& y) -> decltype(x==y);
+		static auto check(X x, Y y) -> decltype(x==y);
 	static substitution_failure check(...);
 public:
 	using type = decltype(check(std::declval<T>(),std::declval<U>()));
@@ -45,7 +45,7 @@ template<typename T, typename U>
 struct get_not_equal_result {
 private:
 	template<typename X, typename Y>
-		static auto check(const X& x, const Y& y) -> decltype(x!=y);
+		static auto check(X x, Y y) -> decltype(x!=y);
 	static substitution_failure check(...);
 public:
 	using type = decltype(check(std::declval<T>(),std::declval<U>()));
--- part3/ch24/24-4-5-template-definition-check/src/template_difinition_check.cpp
+++ part3/ch24/24-4-5-template-definition-check/src/template_difinition_check.cpp
@@ -5,11 +5,14 @@
  *      Author: sasaki
  */
 
-#include "concept.h"
+#include "input_iterator.h"
+#include "equality_comparable.h"
+#include "iterator_traits.h"
 using namespace Estd;
 
 #include <list>
 #include <vector>
+#include <iostream>
 
 template<typename Iter, typename Val>
 Iter find(Iter b, Iter e, Val x)
@@ -31,6 +34,8 @@ Iter find(Iter b, Iter e, Val x)
 
 void f(std::list<int>& lst, std::vector<std::string>& vs)
 {
+	std::cout << "-- f(std::list<int>&,std::vector<std::string>&) --\n";
+
 	auto p = find(lst.begin(), lst.end(), 1209);
 	auto q = find(vs.begin(), vs.end(), "Cambridge");
 
@@ -45,9 +50,10 @@ struct Forward : public std::iterator<std::forward_iterator_tag, Val> {
 	Forward() = default;
 	Forward(const Forward&) = default;
 	Forward& operator=(const Forward&) = default;
-	bool operator==(const Forward& o) { return true; }
+	bool operator==(const Forward& o) { return true; } // should be const
 	bool operator!=(const Forward& o) { return false; }
 	void operator++() { }
+	void operator++(int) { }
 	Val& operator*() { return v; }
 };
 
@@ -60,6 +66,7 @@ struct Input : public std::iterator<std::input_iterator_tag, Val> {
 	bool operator==(const Input& o) { return true; }
 	bool operator!=(const Input& o) { return false; }
 	void operator++() { }
+	void operator++(int) { }
 	Val& operator*() { return v; }
 };
 
@@ -73,11 +80,40 @@ using Value_type<Forward<Val>> = Val;
 
 void g()
 {
+	std::cout << "-- g() --\n";
+	std::cout << std::boolalpha;
+
 	Forward<int> p = find(Forward<int>{},Forward<int>{},7);
 	std::cout << "not found: " << (p == Forward<int>{}) << '\n';
 
 	Input<int> q = find(Input<int>{},Input<int>{},7);
 	std::cout << "not found: " << (q == Input<int>{}) << '\n';
+}
+
+void test_constraint()
+{
+	std::cout << "-- test_constraint() --\n";
+	std::cout << std::boolalpha;
+
+	bool b1 = Has_iterator_category<Forward<int>>();
+	bool b2 = Convertible<Iterator_category_result<Forward<int>>, std::input_iterator_tag>();
+	bool b3 = Has_dereference<Forward<int>>();
+	bool b4 = Has_iterator_value_type<Forward<int>>();
+	bool b5 = Convertible<Dereference_result<Forward<int>>,Iterator_value_type_result<Forward<int>>>();
+	bool b6 = Has_pre_increment<Forward<int>>();
+	bool b7 = Has_post_increment<Forward<int>>();
+	bool b8 = Has_equal<Forward<int>>();
+	bool b9 = Has_not_equal<Forward<int>>();
+
+	std::cout << "Has_iterator_category<Forward<int>>(): " << b1 << '\n';
+	std::cout << "Convertible<Iterator_category_result<Forward<int>>, std::input_iterator_tag>(): " << b2 << '\n';
+	std::cout << "Has_dereference<Forward<int>>(): " << b3 << '\n';
+	std::cout << "Has_iterator_value_type<Forward<int>>(): " << b4 << '\n';
+	std::cout << "Convertible<Dereference_result<Forward<int>>,Iterator_value_type_result<Forward<int>>>(): " << b5 << '\n';
+	std::cout << "Has_pre_increment<Forward<int>>(): " << b6 << '\n';
+	std::cout << "Has_post_increment<Forward<int>>(): " << b7 << '\n';
+	std::cout << "Has_equal<Forward<int>>(): " << b8 << '\n';
+	std::cout << "Has_not_equal<Forward<int>>(): " << b9 << '\n';
 }
 
 // add main
@@ -90,9 +126,6 @@ int main()
 	f(lst,vs);
 
 	g();
+	test_constraint();
 }
-
-
-
-
 
