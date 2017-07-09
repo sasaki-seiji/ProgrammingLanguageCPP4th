7927a1161b4deaf85b1ade6127c0bf92f18c97e4
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-07-09 17:23:41 +0900

    出力内容の見えを改善

--- part3/ch29/29-1-1-basic-Matrix-usage/src/basic_usage.cpp
+++ part3/ch29/29-1-1-basic-Matrix-usage/src/basic_usage.cpp
@@ -24,9 +24,9 @@ void initialize_from_matrix_initializer()
 {
 	cout << "-- initialize_from_matrix_initializer() --\n";
 
-	cout << "md0{1}: " << md0 << endl;
-	cout << "md1{1,2,3,4}: " << md1 << endl;
-	cout << "md2{...}: " << md2 << endl;
+	cout << "Matrix<double,0> md0{1}: " << md0 << endl;
+	cout << "Matrix<double,1> md1{1,2,3,4}: " << md1 << endl;
+	cout << "Matrix<double,2> md2{...}: " << md2 << endl;
 }
 
 Matrix<double,3> md3(4,7,9);
@@ -36,8 +36,12 @@ void initialize_from_sizes()
 {
 	cout << "-- initialize_from_sizes() --\n";
 
-	cout << "md3(4,7,9).desc: " << md3.descriptor() << endl;
-	cout << "md17.desc: " << md17.descriptor() << endl;
+	cout << "Matrix<double,3> md3(4,7,9);\n";
+	cout << "  md3.desc: " << md3.descriptor() << endl;
+	cout << "  md3: " << md3 << endl;
+	cout << "Matrix<double,17> md17;\n";
+	cout << "  md17.desc: " << md17.descriptor() << endl;
+	cout << "  md17: " << md17 << endl;
 }
 
 Matrix<double,2> md;
@@ -46,8 +50,8 @@ Matrix<string,2> ms;
 void various_element_type()
 {
 	cout << "-- various_element_type() --\n";
-	cout << "typeid(md): " << typeid(md).name() << endl;
-	cout << "typeid(ms): " << typeid(ms).name() << endl;
+	cout << "Matrix<double,2> md; typeid(md): " << typeid(md).name() << endl;
+	cout << "Matrix<string,2> ms; typeid(ms): " << typeid(ms).name() << endl;
 }
 
 Matrix<Matrix<int,2>,2> mm {
@@ -68,8 +72,9 @@ Matrix<Matrix<int,2>,2> mm {
 void matrix_of_matrix()
 {
 	cout << "-- matrix_of_matrix() --\n";
-	cout << "mm.desc: " << mm.descriptor() << endl;
-	cout << "mm: " << mm << endl;
+	cout << "Matrix<Matrix<int,2>,2> mm {...};\n";
+	cout << "  mm.desc: " << mm.descriptor() << endl;
+	cout << "  mm: " << mm << endl;
 }
 
 //Matrix<char,2> mc1(2,3,4);
@@ -89,8 +94,9 @@ void check_dimension()
 {
 	cout << "-- check_dimension() --\n";
 
-	cout << "mc2.desc: " << mc2.descriptor() << endl;
-	cout << "mc2: " << mc2 << endl;
+	cout << "Matrix<char,2> mc2 { {'1', '2', '3'} };\n";
+	cout << "  mc2.desc: " << mc2.descriptor() << endl;
+	cout << "  mc2: " << mc2 << endl;
 }
 
 Matrix<double,1> m1(100);
@@ -112,16 +118,18 @@ void order_and_extent()
 {
 	cout << "-- order_and_extent() --\n";
 
-	cout << "m1.desc: " << m1.descriptor() << endl;
-	cout << "m2.desc: " << m2.descriptor() << endl;
-	cout << "auto d1 = m1.order; " << d1 << endl;
-	cout << "auto d2 = m2.order; " << d2 << endl;
-	cout << "auto e1 = m1.extent(0); " << e1 << endl;
-	cout << "(overflow) auto e2 = m1.extent(1); " << e2 << endl;
-	cout << "auto e3 = m2.extent(0); " << e3 << endl;
-	cout << "auto e4 = m2.extent(1); " << e4 << endl;
-	cout << "auto s1 = m1.size(); " << s1 << endl;
-	cout << "auto s2 = m2.size(); " << s2 << endl;
+	cout << "Matrix<double,1> m1(100);\nMatrix<double,2> m2(50,6000);\n";
+
+	cout << "  m1.desc: " << m1.descriptor() << endl;
+	cout << "  m2.desc: " << m2.descriptor() << endl;
+	cout << "  auto d1 = m1.order; " << d1 << endl;
+	cout << "  auto d2 = m2.order; " << d2 << endl;
+	cout << "  auto e1 = m1.extent(0); " << e1 << endl;
+	cout << "  (overflow) auto e2 = m1.extent(1); " << e2 << endl;
+	cout << "  auto e3 = m2.extent(0); " << e3 << endl;
+	cout << "  auto e4 = m2.extent(1); " << e4 << endl;
+	cout << "  auto s1 = m1.size(); " << s1 << endl;
+	cout << "  auto s2 = m2.size(); " << s2 << endl;
 }
 
 Matrix<double,2> m {
@@ -139,11 +147,11 @@ void index_access()
 {
 	cout << "-- index_access() --\n";
 
-	cout << "m: " << m << endl;
-	cout << "double v1 = m(1,2); " << v1 << endl;
-	cout << "double v2 = m[1][2]; " << v2 << endl;
-	cout << "Matrix<double,1> mr1 = m[1]; " << mr1 << endl;
-	cout << "double v3 = mr1[2]; " << v3 << endl;
+	cout << "Matrix<double,2> m {...}; m: " << m << endl;
+	cout << "  double v1 = m(1,2); " << v1 << endl;
+	cout << "  double v2 = m[1][2]; " << v2 << endl;
+	cout << "  Matrix<double,1> mr1 = m[1]; " << mr1 << endl;
+	cout << "  double v3 = mr1[2]; " << v3 << endl;
 }
 
 void basic_usage_all()
