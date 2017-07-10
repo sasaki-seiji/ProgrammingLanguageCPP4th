1531d66086924be4c959651b950a4b66d106a84c
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-07-10 19:55:33 +0900

    大域変数をローカル変数に変更

--- part3/ch29/29-2-2-indexing-and-slicing/src/test_indexing_slicing.cpp
+++ part3/ch29/29-2-2-indexing-and-slicing/src/test_indexing_slicing.cpp
@@ -8,14 +8,14 @@
 #include "Matrix_impl.h"
 #include "Matrix_ref_impl.h"
 
-Matrix<int,2> m {
-	{01,02,03},
-	{11,12,13}
-};
-
 void access_by_int_index()
 {
 	cout << "-- access_by_int_index() --\n";
+
+	Matrix<int,2> m {
+		{01,02,03},
+		{11,12,13}
+	};
 
 	cout << "m: " << m << endl;
 
@@ -28,17 +28,17 @@ void access_by_int_index()
 	cout << "m(1,2) = 99; m: " << m << endl;
 }
 
-Matrix<int,2> m2 {
-	{01,02,03},
-	{11,12,13},
-	{21,22,23}
-};
-
-auto m22 = m2(slice{1,2},slice{0,3});
-
 void access_by_slice_index()
 {
 	cout << "-- access_by_slice_index() --\n";
+
+	Matrix<int,2> m2 {
+		{01,02,03},
+		{11,12,13},
+		{21,22,23}
+	};
+
+	auto m22 = m2(slice{1,2},slice{0,3});
 
 	cout << "m2: " << m2 << endl;
 	cout << "auto m22 = m2(slice{1,2},slice{0,3}); " << m22 << endl;
@@ -48,28 +48,35 @@ void assign_to_sliced_matrix()
 {
 	cout << "-- assign_to_sliced_matrix() --\n";
 
+	Matrix<int,2> m2 {
+		{01,02,03},
+		{11,12,13},
+		{21,22,23}
+	};
+
 	m2(slice{1,2},slice{0,3}) = {
 			{111,112,113},
 			{121,122,123}
 	};
 
+	cout << "m2: " << m2 << endl;
 	cout << "m2(slice{1,2},slice(0,3}) = {{111,112,113},{121,122,123}};\n";
 	cout << "m2: " << m2 << endl;
 }
 
-Matrix<int,2> m3 {
-	{01,02,03},
-	{11,12,13},
-	{21,22,23}
-};
-
-auto m31 = m3(slice{1,2},1);
-auto m32 = m3(slice{1,2},0);
-auto x = m3(1,2);
-
 void access_by_slice_and_int_index()
 {
 	cout << "-- access_by_slice_and_int_index() --\n";
+
+	Matrix<int,2> m3 {
+		{01,02,03},
+		{11,12,13},
+		{21,22,23}
+	};
+
+	auto m31 = m3(slice{1,2},1);
+	auto m32 = m3(slice{1,2},0);
+	auto x = m3(1,2);
 
 	cout << "m3: " << m3 << endl;
 	cout << "auto m31 = m3(slice{1,2},1); m31: " << m31 << endl;
@@ -79,7 +86,7 @@ void access_by_slice_and_int_index()
 
 void indexing_and_slicing_all()
 {
-	cout << "29.2.2 indexing_and_slicing\n";
+	cout << "@@ 29.2.2 indexing_and_slicing @@\n";
 
 	access_by_int_index();
 	access_by_slice_index();
