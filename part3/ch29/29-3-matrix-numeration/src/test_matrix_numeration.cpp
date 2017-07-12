76e2ce1b1f3d094c3c04667799b437a3b6fd4af6
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-07-12 19:49:36 +0900

    大域のMatrix変数をローカル変数に変更

--- part3/ch29/29-3-matrix-numeration/src/test_matrix_numeration.cpp
+++ part3/ch29/29-3-matrix-numeration/src/test_matrix_numeration.cpp
@@ -8,13 +8,12 @@
 
 #include "Matrix.h"
 
-Matrix<int,2> m1 {{1,2,3},{4,5,6}};
-Matrix<int,2> m2 {m1};
-//Matrix<int,2> m2 (m1);
 
 void test_scalar_multiply()
 {
 	cout << "-- test_scalar_multipy() --\n";
+
+	Matrix<int,2> m1 {{1,2,3},{4,5,6}};
 
 	cout << "Matrix<int,2> m1: " << m1 << endl;
 	m1 *= 2;
@@ -26,17 +25,22 @@ void test_matrix_addition()
 {
 	cout << "-- test_matrix_addition() --\n";
 
+	Matrix<int,2> m1 {{1,2,3},{4,5,6}};
+	Matrix<int,2> m2 {m1};
+
 	cout << "Matrix<int,2> m1: " << m1 << endl;
 	cout << "Matrix<int,2> m2: " << m2 << endl;
 	Matrix<int,2> m3 = m1+m2;
 	cout << "Matrix<int,2> m3 = m1+m2; m3: " << m3 << endl;
 }
 
-Matrix<int,2> m4 {{1,2},{3,4},{5,6}};
 
 void test_matrix_product()
 {
 	cout << "-- test_matrix_product() --\n";
+
+	Matrix<int,2> m1 {{1,2,3},{4,5,6}};
+	Matrix<int,2> m4 {{1,2},{3,4},{5,6}};
 
 	cout << "Matrix<int,2> m1: " << m1 << endl;
 	cout << "Matrix<int,2> m4: " << m4 << endl;
@@ -46,9 +50,10 @@ void test_matrix_product()
 
 void matrix_numeration_all()
 {
+	cout << "@@ 29.3. matrix numeration @@\n";
+
 	test_scalar_multiply();
 	test_matrix_addition();
 	test_matrix_product();
-
 }
 
