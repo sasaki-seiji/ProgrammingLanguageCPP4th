fac83a6bf304e72c544707c6102ccf089fcaad9e
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-07-12 19:27:12 +0900

    29.3.3. matrix product のテストを追加

--- part3/ch29/29-test-all/src/main.cpp
+++ part3/ch29/29-test-all/src/main.cpp
@@ -10,6 +10,8 @@
 #include "test_indexing_slicing.h"
 #include "test_scalar_operator.h"
 #include "test_matrix_addition.h"
+#include "test_matrix_product.h"
+
 
 int main()
 {
@@ -18,5 +20,6 @@ int main()
 	indexing_and_slicing_all();
 	scalar_operator_all();
 	matrix_addition_all();
+	matrix_product_all();
 }
 
---
+++ part3/ch29/29-test-all/src/test_matrix_product.h
@@ -0,0 +1,1 @@
+/home/sasaki/books/ProgrammingLanguageCPP4th/part3/ch29/29-3-3-matrix-product/src/test_matrix_product.h
