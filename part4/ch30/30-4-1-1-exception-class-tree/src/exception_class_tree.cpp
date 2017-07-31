de50f0249e12eaad6e3846f8c9e9603bf921c8df
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2017-07-31 19:09:55 +0900

    例外クラスの階層を確認

--- part4/ch30/30-4-1-1-exception-class-tree/src/exception_class_tree.cpp
+++ part4/ch30/30-4-1-1-exception-class-tree/src/exception_class_tree.cpp
@@ -16,20 +16,38 @@ struct My_error : runtime_error {
 };
 
 
-int main()
+void test_exception(int type)
 try {
-	throw My_error{119};
+	cout << "-- test_exception(" << type << ") --\n";
+	switch(type) {
+	case 1:
+		throw My_error{119};
+	case 2:
+		throw range_error("unexcepted range error occurred");
+	case 3:
+		throw out_of_range("out of range error occurred");
+	default:
+		throw int{110};
+	}
 }
 catch (My_error& me) {
 	cout << "My_error - interesting_value: " << me.interesting_value << ", what(): " << me.what() << endl;
 }
 catch (runtime_error& re) {
-
+	cout << "runtime_error - what(): " << re.what() << endl;
 }
 catch (exception& e)
 {
-
+	cout << "exception - what(): " << e.what() << endl;
 }
 catch (...) {
+	cout << "unknown error occurred" << endl;
+}
 
+int main()
+{
+	test_exception(1);
+	test_exception(2);
+	test_exception(3);
+	test_exception(4);
 }
